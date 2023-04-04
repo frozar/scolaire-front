f33288ed1d9d9752da0036a74632b076b258d6ce
frozar <fabien.rozar@gmail.com>
2023-04-04 09:47:13 +0400

    More work in back, less in front

--- src/BusLines.tsx
+++ src/BusLines.tsx
@@ -15,51 +15,26 @@ export default function BusLines() {
         (
           res: {
             id_bus_line: number;
-            id: number;
-            point_id: number;
-            nature: string;
+            stops: {
+              id: number;
+              point_id: number;
+              nature: string;
+            }[];
           }[]
         ) => {
-          if (res.length === 0) {
-            return [];
-          }
-
-          // Reconstruct the stop with the NatureEnum
-          const piecesOfLine: { id_bus_line: number; stop: PointIdentity }[] =
-            res.map((pieceOfLine) => {
-              const { id_bus_line, id, point_id, nature } = pieceOfLine;
-              const enumNature =
-                nature === "ramassage"
-                  ? NatureEnum.ramassage
-                  : NatureEnum.etablissement;
-
-              return {
-                id_bus_line,
-                stop: {
-                  id,
-                  point_id,
-                  nature: enumNature,
-                } as PointIdentity,
-              };
-            });
-
-          // Aggregate the different stop by id_bus_line
-          let lines: Line[] = [];
-          const firstElt = piecesOfLine[0];
-          let currentLine: Line = {
-            id: firstElt.id_bus_line,
-            stops: [firstElt.stop],
-          };
-          for (const elt of piecesOfLine.slice(1)) {
-            const currentIdBusLine = currentLine.id;
-            if (elt.id_bus_line === currentIdBusLine) {
-              currentLine.stops.push(elt.stop);
-            } else {
-              lines.push(currentLine);
-              currentLine = { id: elt.id_bus_line, stops: [elt.stop] };
-            }
-          }
-          lines.push(currentLine);
+          let lines: Line[] = res.map((line) => {
+            const stopsWithNatureEnum = line.stops.map(
+              (stop) =>
+                ({
+                  ...stop,
+                  nature:
+                    stop["nature"] === "ramassage"
+                      ? NatureEnum.ramassage
+                      : NatureEnum.etablissement,
+                } as PointIdentity)
+            );
+            return { ...line, stops: stopsWithNatureEnum };
+          });
 
           setBusLines(lines);
         }
