3804a191acfba2e21f8d433e8fb7ae83e55fea2b
richard <richard.fontaine@flaxib.re>
2023-04-12 15:05:40 +0400

    Wip

--- src/userInformation/RemoveConfirmation.tsx
+++ src/userInformation/RemoveConfirmation.tsx
@@ -1,5 +1,4 @@
-import { Show } from "solid-js";
-
+import { Show, createSignal, onMount } from "solid-js";
 import {
   addNewUserInformation,
   fetchBusLines,
@@ -80,6 +79,13 @@ export default function RemoveConfirmation() {
     });
   }
 
+  const [buttonRef, setButtonRef] = createSignal(null);
+
+  onMount(() => {
+    if (buttonRef()) {
+      buttonRef()!.focus();
+    }
+  });
   return (
     <Transition
       name="slide-fade"
@@ -99,7 +105,6 @@ export default function RemoveConfirmation() {
         >
           <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-
           {/* Forum github pour les nested transitions
           https://github.com/reactjs/react-transition-group/issues/558 */}
           <Transition
@@ -114,84 +119,85 @@ export default function RemoveConfirmation() {
           >
             <div class="fixed inset-0 z-10 overflow-y-auto">
               <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                  <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                    <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                      <button
-                        type="button"
-                        class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        onClick={() => {
-                          setRemoveConfirmation({
-                            displayed: false,
-                            id_bus_line: null,
-                          });
-                        }}
+                <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                    <button
+                      type="button"
+                      class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => {
+                        setRemoveConfirmation({
+                          displayed: false,
+                          id_bus_line: null,
+                        });
+                      }}
+                    >
+                      <span class="sr-only">Close</span>
+                      <svg
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
                       >
-                        <span class="sr-only">Close</span>
-                        <svg
-                          class="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M6 18L18 6M6 6l12 12"
-                          />
-                        </svg>
-                      </button>
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <div class="sm:flex sm:items-start">
+                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg
+                        class="h-6 w-6 text-red-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                        />
+                      </svg>
                     </div>
-                    <div class="sm:flex sm:items-start">
-                      <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <svg
-                          class="h-6 w-6 text-red-600"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                          />
-                        </svg>
-                      </div>
-                      <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                        <h3
-                          class="text-base font-semibold leading-6 text-gray-900"
-                          id="modal-title"
-                        >
-                          Supprimer une ligne de bus
-                        </h3>
-                        <div class="mt-2">
-                          <p class="text-sm text-gray-500">
-                            Etes-vous sûr de vouloir supprimer la ligne de bus
-                            numéro {getRemoveConfirmation()["id_bus_line"]} ?
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                      <button
-                        type="button"
-                        class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                        onClick={handlerOnClickValider}
+                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3
+                        class="text-base font-semibold leading-6 text-gray-900"
+                        id="modal-title"
                       >
-                        Valider
-                      </button>
-                      <button
-                        type="button"
-                        class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                        onClick={handlerOnClickAnnuler}
-                      >
-                        Annuler
-                      </button>
+                        Supprimer une ligne de bus
+                      </h3>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          Etes-vous sûr de vouloir supprimer la ligne de bus
+                          numéro {getRemoveConfirmation()["id_bus_line"]} ?
+                        </p>
+                      </div>
                     </div>
                   </div>
+                  <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      ref={setButtonRef}
+                      class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      onClick={handlerOnClickValider}
+                    >
+                      Valider
+                    </button>
+                    <button
+                      type="button"
+                      class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={handlerOnClickAnnuler}
+                    >
+                      Annuler
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </Transition>
