9d63a3756f18296af6a60d53a48c5f632746280f
frozar <fabien.rozar@gmail.com>
2023-04-20 00:50:34 +0400

    Try a bench of different map ground.

--- src/l7MapBuilder.ts
+++ src/l7MapBuilder.ts
@@ -35,13 +35,68 @@ export function buildMapL7(div: HTMLDivElement) {
   );
 
   // Manage map ground
-  const readTile = L.tileLayer(
+  const OpenStreetMap_Mapnik = L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }
   );
+  const Stadia_Outdoors = L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png",
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    }
+  );
+  const Esri_WorldTopoMap = L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution:
+        "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
+    }
+  );
+  const CyclOSM = L.tileLayer(
+    "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
+    {
+      maxZoom: 20,
+      attribution: "CyclOSM | Map data: © OpenStreetMap contributors",
+    }
+  );
+  const OpenStreetMap_CH = L.tileLayer(
+    "https://tile.osm.ch/switzerland/{z}/{x}/{y}.png",
+    {
+      maxZoom: 18,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+  const OpenStreetMap_HOT = L.tileLayer(
+    "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    {
+      maxZoom: 18,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+    }
+  );
+  const OpenStreetMap_France = L.tileLayer(
+    "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+
+  // const readTile = OpenStreetMap_Mapnik;
+  const readTile = Stadia_Outdoors;
+  // const readTile = Esri_WorldTopoMap;
+  // const readTile = CyclOSM;
+  // const readTile = OpenStreetMap_CH;
+  // const readTile = OpenStreetMap_HOT;
+  // const readTile = OpenStreetMap_France;
+
   const editTile = L.tileLayer(
     "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
     {
