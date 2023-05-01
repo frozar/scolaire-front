7929f4dc28821697436a8b8bfd3ae63e17f9907a
frozar <fabien.rozar@gmail.com>
2023-05-01 18:11:15 +0400

    Fix the setup for ESLint, default formatter in the vscode settings, setup for prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  parser: "@typescript-eslint/parser",
+  plugins: ["solid", "@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:solid/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
+  ignorePatterns: [".eslintrc.js", "node_modules", "dist"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
-  }
-}
+    "solid/reactivity": "warn",
+    "solid/no-destructure": "warn",
+    "solid/jsx-no-undef": "error",
+    quotes: "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
