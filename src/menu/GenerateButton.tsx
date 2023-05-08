aaebc3e8c57a33e241b0f1ea911cbbf510263f21
frozar <fabien.rozar@gmail.com>
2023-05-08 18:50:18 +0400

    Launch a generation of circuit.

--- src/menu/GenerateButton.tsx
+++ src/menu/GenerateButton.tsx
@@ -6,6 +6,7 @@ import {
   LineType,
   MessageLevelEnum,
   MessageTypeEnum,
+  NatureEnum,
   PointIdentityType,
   PointRamassageType,
 } from "../type";
@@ -75,7 +76,62 @@ export default function () {
         type: MessageTypeEnum.global,
         content: "Aucun Point d'Intéret libre pour la génération de lignes.",
       });
+      return;
     }
+
+    const unplanned_ramassage = unplanned_stops.filter(
+      (elt: PointIdentityType) => elt.nature == NatureEnum.ramassage
+    );
+    const unplanned_etablissement = unplanned_stops.filter(
+      (elt: PointIdentityType) => elt.nature == NatureEnum.etablissement
+    );
+
+    if (unplanned_ramassage.length == 0) {
+      addNewUserInformation({
+        displayed: true,
+        level: MessageLevelEnum.error,
+        type: MessageTypeEnum.global,
+        content: "Aucun arrêt de ramassage pour la génération de lignes.",
+      });
+      return;
+    }
+
+    if (unplanned_etablissement.length == 0) {
+      addNewUserInformation({
+        displayed: true,
+        level: MessageLevelEnum.error,
+        type: MessageTypeEnum.global,
+        content: "Aucun établissement pour la génération de lignes.",
+      });
+      return;
+    }
+    const ramassageIds: number[] = unplanned_ramassage.map(
+      (elt: PointIdentityType) => elt["id"]
+    );
+    const etablissementIds: number[] = unplanned_etablissement.map(
+      (elt: PointIdentityType) => elt["id"]
+    );
+
+    // TODO: enable the spinning wheel
+    //       display result
+    //       add a dialogue box to choice the number of vehicule to use
+    fetch(import.meta.env.VITE_BACK_URL + "/generator/school_bus_circuit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ramassage_ids: ramassageIds,
+        etablissement_ids: etablissementIds,
+        num_vehicles: 1,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: any) => {
+        console.log("result", data);
+      });
   };
 
   return (
