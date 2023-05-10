2fa500235e1074d2185006a7b699f9d1317f739f
lucas <lucas.lvn97439@gmail.com>
2023-05-10 14:40:15 +0400

    Ajout dynamisme au niveau des onglets

--- src/layout/NavLateral.tsx
+++ src/layout/NavLateral.tsx
@@ -14,27 +14,43 @@ import { Show } from "solid-js";
 import { useStateGui } from "../StateGui";
 import { For } from "solid-js";
 
-const [stateGui, { toggleDisplayedMenu, setSelectedTab }] = useStateGui();
+const [stateGui, { toggleDisplayedMenu }] = useStateGui();
 
 function MenuItems(props: any) {
-  const { title, logo } = props;
+  const { title, logo, url } = props;
+  const MenuOnClick = () => {
+    window.location.href = url;
+  };
+
+  let url_basename = location.pathname.split("/");
+  url_basename = url_basename[url_basename.length - 1];
+
+  let _class = "lateral-nav-item";
+  if (url_basename == url) {
+    _class += " active";
+  }
 
   return (
-    <li class="lateral-nav-item">
+    <li class={_class} onclick={MenuOnClick}>
       {logo}
       <Show when={stateGui.displayedMenu == true}>{title}</Show>
+      {/* <Show when={url_basename == url}>
+        <div className="bubble-menu"></div>
+      </Show> */}
     </li>
   );
 }
+
 export default function () {
+  // [ComponentsLogo, title, url]
   const menu_items = [
-    [LateralMenuDashboardLogo, "Dashboard"],
-    [LateralMenuGraphicageLogo, "Graphicage"],
-    [LateralMenuVoirieLogo, "Voirie"],
-    [LateralMenuEtablissementLogo, "Établissements"],
-    [LateralMenuArretsLogo, "Arrêts"],
-    [LateralMenuSettingsLogo, "Paramètres"],
-    [LateralMenuSupportLogo, "Support"],
+    [LateralMenuDashboardLogo, "Dashboard", "dashboard"],
+    [LateralMenuGraphicageLogo, "Graphicage", "graphicage"],
+    [LateralMenuVoirieLogo, "Voirie", "voirie"],
+    [LateralMenuEtablissementLogo, "Établissements", "etablissements"],
+    [LateralMenuArretsLogo, "Arrêts", "arrets"],
+    [LateralMenuSettingsLogo, "Paramètres", "parametres"],
+    [LateralMenuSupportLogo, "Support", "support"],
   ];
 
   return (
@@ -48,7 +64,7 @@ export default function () {
 
       <ul class="lateral-nav-list">
         <For each={menu_items}>
-          {(item) => <MenuItems title={item[1]} logo={item[0]} />}
+          {(item) => <MenuItems title={item[1]} logo={item[0]} url={item[2]} />}
         </For>
       </ul>
 
