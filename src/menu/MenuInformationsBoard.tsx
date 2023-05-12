7971efb3f1dbab7c34877d8c62c1131a639610f7
lucas <lucas.lvn97439@gmail.com>
2023-05-12 14:01:08 +0400

    tooltip integration

--- src/menu/MenuInformationsBoard.tsx
+++ src/menu/MenuInformationsBoard.tsx
@@ -5,7 +5,10 @@ const [, { toggleDisplayedInformationBoard }] = useStateGui();
 
 export default function () {
   return (
-    <div class="menu-btn left-[140px]">
+    <div class="menu-btn left-[140px] group">
+      <span class="tooltip group-hover:scale-100">
+        Montré le panneau d'information des arrêts
+      </span>
       <label class="btn btn-circle" onClick={toggleDisplayedInformationBoard}>
         <IoInformation class="w-full p-0 h-2/3" />
       </label>
