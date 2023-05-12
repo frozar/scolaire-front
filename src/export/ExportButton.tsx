7a3ab36bae4f487d62185fd20616d6f66bc8acaf
lucas <lucas.lvn97439@gmail.com>
2023-05-12 15:14:42 +0400

    rework of export button

--- src/export/ExportButton.tsx
+++ src/export/ExportButton.tsx
@@ -1,19 +1,14 @@
 import { openExportConfirmationBox } from "../signaux";
 import { ControlMapMenuExport } from "./Logos";
+import { CgExport } from "solid-icons/cg";
 
 export default function () {
   return (
-    <>
-      <button
-        class="menu-btn"
-        onClick={() => {
-          openExportConfirmationBox();
-        }}
-      >
-        <label class="btn btn-circle text-white">
-          <ControlMapMenuExport />
-        </label>
-      </button>
-    </>
+    <div class="menu-btn left-[140px] group">
+      <span class="tooltip group-hover:scale-100">Exporter la map</span>
+      <label class="custom-btn btn-circle" onClick={openExportConfirmationBox}>
+        <CgExport class="w-full h-2/3" />
+      </label>
+    </div>
   );
 }
