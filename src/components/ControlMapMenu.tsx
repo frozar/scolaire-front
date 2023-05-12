d8339d8e4dfefb14c80e0171854a74cd66d88b0d
lucas <lucas.lvn97439@gmail.com>
2023-05-12 16:23:21 +0400

    Remove unused import

--- src/components/ControlMapMenu.tsx
+++ src/components/ControlMapMenu.tsx
@@ -1,13 +1,11 @@
 import ExportButton from "../export/ExportButton";
 import MenuClear from "../menu/MenuClear";
-// import MenuInformationsBoard from "../menu/MenuInformationsBoard";
 import MenuDraw from "../menu/MenuDraw";
 import MenuRemoveLine from "../menu/MenuRemoveLine";
 
 export default function () {
   return (
     <div id="control-map-menu">
-      {/* <MenuInformationsBoard /> */}
       <MenuDraw />
       <MenuRemoveLine />
       <MenuClear />
