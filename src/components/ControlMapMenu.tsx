262b0fba762bc002cd68cb9275a7984a6e7c2cd6
lucas <lucas.lvn97439@gmail.com>
2023-05-12 10:18:54 +0400

    Integration of InformationsBoard button

--- src/components/ControlMapMenu.tsx
+++ src/components/ControlMapMenu.tsx
@@ -1,17 +1,17 @@
 import ExportButton from "../export/ExportButton";
 import MenuClear from "../menu/MenuClear";
-import { TogglerInformationBoard } from "../menu/Menu";
+import InformationsBoard from "../menu/InformationsBoard";
 import MenuDraw from "../menu/MenuDraw";
 import MenuRemoveLine from "../menu/MenuRemoveLine";
 
 export default function () {
   return (
     <div id="control-map-menu">
+      <InformationsBoard />
       <MenuDraw />
       <MenuRemoveLine />
       <MenuClear />
       <ExportButton />
-      <TogglerInformationBoard />
     </div>
   );
 }
