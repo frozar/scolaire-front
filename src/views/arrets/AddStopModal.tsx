cb086b7b4a48d380e0be3fbd724d68c551709e38
lucas <lucas.lvn97439@gmail.com>
2023-05-16 15:09:52 +0400

    get props to toggle add stop modal via classList

--- src/views/arrets/AddStopModal.tsx
+++ src/views/arrets/AddStopModal.tsx
@@ -1,3 +1,15 @@
-export default function () {
-  return <div id="stop-modal">test</div>;
+import { createEffect } from "solid-js";
+import { CgCloseO } from "solid-icons/cg";
+
+export default function (props: any) {
+  const toggledModal = () => props.toggledModal();
+  const toggleModal = () => props.toggleModal();
+
+  return (
+    <div id="stop-modal" classList={{ active: toggledModal() == true }}>
+      <button onClick={toggleModal} class="h-[30px] w-[30px] ">
+        <CgCloseO class="w-full h-full fill-green " />
+      </button>
+    </div>
+  );
 }
