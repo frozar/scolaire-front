dfc5e328a926103af635ade73dbc861b567767bb
lucas <lucas.lvn97439@gmail.com>
2023-05-16 16:58:46 +0400

    file renamed, define toggle fn in this file & export them setup signal dataToEdit to know if EditStop panel is in add mode or edition mode

---
+++ src/views/arrets/EditStop.tsx
@@ -0,0 +1,58 @@
+import { CgCloseO } from "solid-icons/cg";
+import { HiSolidLocationMarker } from "solid-icons/hi";
+import { createSignal } from "solid-js";
+import { StopLineItem } from "../../type";
+
+export const [toggledEditStop, setToggledEditStop] = createSignal(false);
+export const toggleEditStop = () => setToggledEditStop(!toggledEditStop());
+export const [dataToEdit, setDataToEdit] = createSignal<StopLineItem>();
+
+export default function (props: any) {
+  console.log(dataToEdit());
+
+  return (
+    <div id="stop-modal" classList={{ active: toggledEditStop() == true }}>
+      <header>
+        <button onClick={toggleEditStop} class="h-[30px] w-[30px] ">
+          <CgCloseO class="w-full h-full fill-green " />
+        </button>
+
+        <h1>
+          {dataToEdit() === undefined ? "Ajouter un arrêt" : "Éditer un arrêt"}
+        </h1>
+      </header>
+
+      <section class="mt-6 flex flex-col gap-6">
+        <div class="group inline-grid w-full">
+          <label for="name">Nom</label>
+          <input type="text" id="name" value={dataToEdit()?.name ?? ""} />
+        </div>
+
+        <h3 class="flex items-center">
+          Arrêt <HiSolidLocationMarker />
+        </h3>
+
+        <div class="group inline-grid w-full">
+          <label for="Latitude">Latitude</label>
+          <input
+            type="text"
+            id="Latitude"
+            value={
+              dataToEdit()?.lat ? String(dataToEdit()?.lat) : "Pas de latitude"
+            }
+          />
+        </div>
+        <div class="group inline-grid w-full">
+          <label for="Longitude">Longitude</label>
+          <input
+            type="text"
+            id="Longitude"
+            value={
+              dataToEdit()?.lat ? String(dataToEdit()?.lon) : "Pas de longitude"
+            }
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
