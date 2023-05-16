e266183b53aad3ec732f49375d342325c4b810db
lucas <lucas.lvn97439@gmail.com>
2023-05-16 11:41:02 +0400

    import rename

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import NavLateral from "./layout/NavLateral";
 import { useStateGui } from "./StateGui";
 import ClearConfirmation from "./userInformation/ClearConfirmation";
 import GeneratorDialogueBox from "./userInformation/GeneratorDialogueBox";
-import Arret from "./views/Arret";
+import Arret from "./views/arrets/Arret";
 
 const [
   ,
