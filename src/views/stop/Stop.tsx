9edbebe6f4940e8d78a45246d35b20b0b63a1d14
richard <richard.fontaine@flaxib.re>
2023-05-16 15:11:36 +0400

    WIP

--- src/views/stop/Stop.tsx
+++ src/views/stop/Stop.tsx
@@ -1,23 +1,12 @@
 import { createStore } from "solid-js/store";
 import { AiOutlineSearch } from "solid-icons/ai";
 import EditStop, { setDataToEdit, toggleEditStop } from "./EditStop";
-import { For, createEffect, createSignal } from "solid-js";
+import { For, createEffect, createSignal, onMount } from "solid-js";
 import StopItems from "./StopItem";
 import { StopItemType } from "../../type";
 
-export const [selected, setSelected] = createSignal<StopItemType[]>([]);
-export const [stop, setStop] = createStore([
-  {
-    id: 1,
-    name: "Arret du stade",
-    quantity: 74,
-    nb_etablissement: 3,
-    nb_line: 3,
-    lon: 23.43,
-    lat: 23.54,
-    selected: false,
-  },
-]);
+export const [selected, setSelected] = createSignal<StopLineItem[]>([]);
+export const [stop, setStop] = createStore([]);
 
 export const addSelected = (item: StopItemType) =>
   setSelected([...selected(), item]);
@@ -29,6 +18,35 @@ export const removeSelected = (item: StopItemType) => {
 
 export const [isChecked, setIsChecked] = createSignal(false);
 
+function StopLineBoard(props: { item: StopLineItem }) {
+  const item = props.item;
+  return (
+    <tr>
+      <td class="flex items-center">
+        <input
+          id="comments"
+          aria-describedby="comments-description"
+          name="comments"
+          type="checkbox"
+          class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
+        />
+        {item.name}
+      </td>
+      <td>{item.quantity}</td>
+      <td>{item.nbEtablissement}</td>
+      <td>{item.nbLine}</td>
+      <td>
+        <a href="#" class="text-[#0CC683] mr-2">
+          Editer
+        </a>
+
+        <a href="#" class="text-[#F44434]">
+          Supprimer
+        </a>
+      </td>
+    </tr>
+  );
+}
 export default function () {
   const [refSelect, setRefSelect] = createSignal<HTMLSelectElement>();
   let refCheckbox!: HTMLInputElement;
@@ -53,99 +71,127 @@ export default function () {
     refCheckbox?.addEventListener("change", () => {
       setIsChecked(!isChecked());
     });
-  });
+    const [stop, setStop] = createStore<StopLineItem[]>([]);
+    onMount(() => {
+      fetch(
+        import.meta.env.VITE_BACK_URL + "/ramassages_associated_bus_lines_info",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then(
+          (
+            res: {
+              id: number;
+              name: string;
+              quantity: number;
+              nb_etablissement: number;
+              nb_line: number;
+            }[]
+          ) => {
+            setStop(
+              res.map((elt) => {
+                return {
+                  id: elt.id,
+                  name: elt.name,
+                  quantity: elt.quantity,
+                  nbLine: elt.nb_line,
+                  nbEtablissement: elt.nb_etablissement,
+                };
+              })
+            );
+          }
+        );
+    });
 
-  return (
-    <div class="flex w-full">
-      <div id="arrets-board">
-        <header>
-          <h1>Gérer les arrêts</h1>
-          <div id="filters">
-            <div class="left">
-              <select ref={setRefSelect}>
-                <option selected value="null">
-                  Action
-                </option>
-                <option class="fill-red" value="delete">
-                  Supprimer
-                </option>
-              </select>
+    return (
+      <div class="flex w-full">
+        <div id="arrets-board">
+          <header>
+            <h1>Gérer les arrêts</h1>
+            <div id="filters">
+              <div class="left">
+                <select ref={setRefSelect}>
+                  <option selected value="null">
+                    Action
+                  </option>
+                  <option class="fill-red" value="delete">
+                    Supprimer
+                  </option>
+                </select>
 
-              <button
-                type="button"
-                class="btn-arret-add"
-                onClick={() => {
-                  setDataToEdit();
-                  toggleEditStop();
-                }}
-              >
-                Ajouter
-              </button>
+                <button
+                  type="button"
+                  class="btn-arret-add"
+                  onClick={() => {
+                    setDataToEdit();
+                    toggleEditStop();
+                  }}
+                >
+                  Ajouter
+                </button>
 
-              <div class="input-field">
-                <div class="input-search-logo">
-                  <AiOutlineSearch />
+                <div class="input-field">
+                  <div class="input-search-logo">
+                    <AiOutlineSearch />
+                  </div>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class=""
+                    placeholder="Recherche"
+                  />
                 </div>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class=""
-                  placeholder="Recherche"
-                />
+              </div>
+
+              <div class="right">
+                <button class="btn-arret-export-import">Exporter</button>
+                <button class="btn-arret-export-import">Importer</button>
               </div>
             </div>
-
-            <div class="right">
-              <button class="btn-arret-export-import">Exporter</button>
-              <button class="btn-arret-export-import">Importer</button>
+          </header>
+          <div class="board-content">
+            <div class="h-[78vh]">
+              <table class="min-w-full">
+                <thead>
+                  <tr>
+                    <th scope="col" class="pl-4 pr-3 sm:pl-0 flex items-center">
+                      <input
+                        id="comments"
+                        aria-describedby="comments-description"
+                        name="comments"
+                        type="checkbox"
+                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
+                        ref={refCheckbox}
+                      />
+                      Nom
+                    </th>
+                    <th scope="col">Nombre d'élèves</th>
+                    <th scope="col">Nombre d’établissements desservis</th>
+                    <th scope="col">Nombre de lignes</th>
+                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                      <span class="">Actions</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <For each={stop}>
+                    {(fields) => <StopLineBoard item={fields} />}
+                  </For>
+                </tbody>
+              </table>
             </div>
           </div>
-        </header>
-        <div class="board-content">
-          <div class="h-[78vh]">
-            <table class="min-w-full">
-              <thead>
-                <tr>
-                  <th scope="col" class="pl-4 pr-3 sm:pl-0 flex items-center">
-                    <input
-                      id="comments"
-                      aria-describedby="comments-description"
-                      name="comments"
-                      type="checkbox"
-                      class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
-                      ref={refCheckbox}
-                    />
-                    Nom
-                  </th>
-                  <th scope="col">Nombre d'élèves</th>
-                  <th scope="col">Nombre d’établissements desservis</th>
-                  <th scope="col">Nombre de lignes</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span class="">Actions</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <For each={stop}>
-                  {(fields) => (
-                    <StopItems
-                      id={fields.id}
-                      name={fields.name}
-                      quantity={fields.quantity}
-                      nbEtablissement={fields.nb_etablissement}
-                      nbLine={fields.nb_line}
-                      lon={fields.lon}
-                      lat={fields.lat}
-                    />
-                  )}
-                </For>
-              </tbody>
-            </table>
-          </div>
         </div>
+        <EditStop />
       </div>
-      <EditStop />
-    </div>
-  );
+    );
+  });
 }
