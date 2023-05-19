023f73b8a8f525594bbe774385114bf8e2974452
richard <richard.fontaine@flaxib.re>
2023-05-19 09:31:38 +0400

    Resolve  rebase conflict

--- src/views/stop/Stop.tsx
+++ src/views/stop/Stop.tsx
@@ -7,9 +7,10 @@ import { StopItemType } from "../../type";
 import { displayDownloadErrorMessage } from "../../userInformation/utils";
 import { getExportDate } from "../graphicage/rightMapMenu/export/export";
 import { download } from "../graphicage/rightMapMenu/export/csvExport";
+import RemoveRamassageConfirmation from "../../userInformation/RemoveRamassageConfirmation";
 
 export const [selected, setSelected] = createSignal<StopLineItem[]>([]);
-export const [stop, setStop] = createStore([]);
+export const [stop, setStop] = createStore<StopLineItem[]>([]);
 
 export const addSelected = (item: StopItemType) =>
   setSelected([...selected(), item]);
@@ -20,6 +21,7 @@ export const removeSelected = (item: StopItemType) => {
 };
 
 export const [isChecked, setIsChecked] = createSignal(false);
+
 export function displayArret() {
   fetch(
     import.meta.env.VITE_BACK_URL + "/ramassages_associated_bus_lines_info",
@@ -113,6 +115,8 @@ export default function () {
               quantity: number;
               nb_etablissement: number;
               nb_line: number;
+              lon: number;
+              lat: number;
             }[]
           ) => {
             setStop(
@@ -123,126 +127,130 @@ export default function () {
                   quantity: elt.quantity,
                   nbLine: elt.nb_line,
                   nbEtablissement: elt.nb_etablissement,
+                  lon: elt.lon,
+                  lat: elt.lat,
                 };
               })
             );
           }
         );
     });
+  });
 
-    return (
-      <>
-        <RemoveRamassageConfirmation />
-        <div class="flex w-full">
-          <div id="arrets-board">
-            <header>
-              <h1>Gérer les arrêts</h1>
-              <div id="filters">
-                <div class="left">
-                  <select>
-                    <option selected>Colone</option>
-                  </select>
+  return (
+    <>
+      <RemoveRamassageConfirmation />
+      <div class="flex w-full">
+        <div id="arrets-board">
+          <header>
+            <h1>Gérer les arrêts</h1>
+            <div id="filters">
+              <div class="left">
+                <select ref={setRefSelect}>
+                  <option selected value="null">
+                    Action
+                  </option>
+                  <option class="fill-red" value="delete">
+                    Supprimer
+                  </option>
+                </select>
 
-                  <button
-                    type="button"
-                    class="btn-arret-add"
-                    onClick={() => {
-                      setDataToEdit();
-                      toggleEditStop();
-                    }}
-                  >
-                    Ajouter
-                  </button>
+                <button
+                  type="button"
+                  class="btn-arret-add"
+                  onClick={() => {
+                    setDataToEdit();
+                    toggleEditStop();
+                  }}
+                >
+                  Ajouter
+                </button>
 
-                  <div class="input-field">
-                    <div class="input-search-logo">
-                      <AiOutlineSearch />
-                    </div>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      class=""
-                      placeholder="Recherche"
-                    />
+                <div class="input-field">
+                  <div class="input-search-logo">
+                    <AiOutlineSearch />
                   </div>
-                </div>
-
-                <div class="right">
-                  <button
-                    class="btn-arret-export-import"
-                    onClick={() => {
-                      fetch(
-                        import.meta.env.VITE_BACK_URL +
-                          "/export/etablissement_input"
-                      )
-                        .then((response) => {
-                          if (!response.ok) {
-                            displayDownloadErrorMessage();
-                          } else {
-                            return response.blob();
-                          }
-                        })
-                        .then((blob: Blob | undefined) => {
-                          if (!blob) {
-                            return;
-                          }
-
-                          const { year, month, day, hour, minute } =
-                            getExportDate();
-                          const fileName = `${year}-${month}-${day}_${hour}-${minute}_etablissements.csv`;
-                          download(fileName, blob);
-                        })
-                        .catch(() => {
-                          displayDownloadErrorMessage();
-                        });
-                    }}
-                  >
-                    Exporter
-                  </button>
-                  <button class="btn-arret-export-import">Importer</button>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class=""
+                    placeholder="Recherche"
+                  />
                 </div>
               </div>
-            </header>
-            <div class="board-content">
-              <div class="h-[78vh]">
-                <table class="min-w-full">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        class="pl-4 pr-3 sm:pl-0 flex items-center"
-                      >
-                        <input
-                          id="comments"
-                          aria-describedby="comments-description"
-                          name="comments"
-                          type="checkbox"
-                          class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
-                          ref={refCheckbox}
-                        />
-                        Nom
-                      </th>
-                      <th scope="col">Nombre d'élèves</th>
-                      <th scope="col">Nombre d’établissements desservis</th>
-                      <th scope="col">Nombre de lignes</th>
-                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                        <span class="">Actions</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <For each={stop}>
-                      {(fields) => <StopItems item={fields} />}
-                    </For>
-                  </tbody>
-                </table>
+
+              <div class="right">
+                <button
+                  class="btn-arret-export-import"
+                  onClick={() => {
+                    fetch(
+                      import.meta.env.VITE_BACK_URL +
+                        "/export/etablissement_input"
+                    )
+                      .then((response) => {
+                        if (!response.ok) {
+                          displayDownloadErrorMessage();
+                        } else {
+                          return response.blob();
+                        }
+                      })
+                      .then((blob: Blob | undefined) => {
+                        if (!blob) {
+                          return;
+                        }
+
+                        const { year, month, day, hour, minute } =
+                          getExportDate();
+                        const fileName = `${year}-${month}-${day}_${hour}-${minute}_etablissements.csv`;
+                        download(fileName, blob);
+                      })
+                      .catch(() => {
+                        displayDownloadErrorMessage();
+                      });
+                  }}
+                >
+                  Exporter
+                </button>
+                <button class="btn-arret-export-import">Importer</button>
               </div>
             </div>
+          </header>
+          <div class="board-content">
+            <div class="h-[78vh]">
+              <table class="min-w-full">
+                <thead>
+                  <tr>
+                    <th scope="col" class="pl-4 pr-3 sm:pl-0 flex items-center">
+                      <input
+                        id="comments"
+                        aria-describedby="comments-description"
+                        name="comments"
+                        type="checkbox"
+                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
+                        ref={refCheckbox}
+                      />
+                      Nom
+                    </th>
+                    <th scope="col">Nombre d'élèves</th>
+                    <th scope="col">Nombre d’établissements desservis</th>
+                    <th scope="col">Nombre de lignes</th>
+                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                      <span class="">Actions</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <For each={stop}>
+                    {(fields) => <StopItems item={fields} />}
+                  </For>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <EditStop />
         </div>
-      </>
-    );
-  });
+        <EditStop />
+      </div>
+    </>
+  );
 }
