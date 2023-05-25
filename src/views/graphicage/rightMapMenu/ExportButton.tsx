21a25f8fddbbbb98d234fa01978258d30a854bb6
lucas <lucas.lvn97439@gmail.com>
2023-05-25 16:15:32 +0400

    add logic to define if ConfirmStopAddLine must be invoked & getout of edition mode

--- src/views/graphicage/rightMapMenu/ExportButton.tsx
+++ src/views/graphicage/rightMapMenu/ExportButton.tsx
@@ -1,13 +1,32 @@
+import { useStateAction } from "../../../StateAction";
 import { openExportConfirmationBox } from "../../../signaux";
 import { CgExport } from "solid-icons/cg";
+import { toggleConfirmStopAddLine } from "../ConfirmStopAddLine";
+
+const [, { isInAddLineMode, getLineUnderConstruction, setModeRead }] =
+  useStateAction();
 
 export default function () {
+  const handleClick = () => {
+    const lineInBuild = getLineUnderConstruction().stops.length > 0;
+
+    if (isInAddLineMode() && lineInBuild) {
+      toggleConfirmStopAddLine();
+    } else if (isInAddLineMode()) {
+      setModeRead();
+      openExportConfirmationBox();
+    } else {
+      openExportConfirmationBox();
+    }
+    //
+  };
+
   return (
     <div class="menu-btn left-[140px] group">
       <span class="tooltip group-hover:scale-100">Exporter</span>
       <label
         class="custom-btn btn-circle hover:bg-[#062F3F] hover:text-[#0cc683]"
-        onClick={openExportConfirmationBox}
+        onClick={handleClick}
       >
         <CgExport class="h-10 w-10 pb-[5px]" />
       </label>
