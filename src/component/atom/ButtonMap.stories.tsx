a5b09c9e16c63702fb566da3bd6ef66630b445eb
lucas <lucas.lvn97439@gmail.com>
2023-05-31 15:44:48 +0400

    Create stories for addLine of map button

--- src/component/atom/ButtonMap.stories.tsx
+++ src/component/atom/ButtonMap.stories.tsx
@@ -1,21 +1,33 @@
 import type { Meta, StoryObj } from "storybook-solidjs";
 
 import ButtonMap from "./ButtonMap";
+import { FaSolidPlus } from "solid-icons/fa";
+const Icon = () => <FaSolidPlus class="w-full p-0 h-2/3" />;
 
 const meta = {
-  title: "Example/Button",
+  title: "Map/Button",
   component: ButtonMap,
   tags: ["autodocs"],
   argTypes: {
     onClick: { action: "clicked tt" },
   },
 } satisfies Meta<typeof ButtonMap>;
+
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const addLineEnable: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    tooltip: "Test",
+    icon: <Icon />,
+    _class: true,
+  },
+};
+
+export const addLineDisable: Story = {
+  args: {
+    tooltip: "Test",
+    icon: <Icon />,
+    _class: false,
   },
 };
