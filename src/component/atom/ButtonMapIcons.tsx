52f5d393a493a67a78743e791a4cf8b590d1ea60
lucas <lucas.lvn97439@gmail.com>
2023-05-31 16:11:47 +0400

    Convert simple assignement to function assignement

--- src/component/atom/ButtonMapIcons.tsx
+++ src/component/atom/ButtonMapIcons.tsx
@@ -5,9 +5,11 @@ import { FiArrowUpCircle } from "solid-icons/fi";
 import { BsInfoCircle } from "solid-icons/bs";
 import { FaSolidMinus } from "solid-icons/fa";
 
-export const AddLineIcon = <FaSolidPlus class="w-full p-0 h-2/3" />;
-export const ClearMapIcon = <FaRegularTrashCan class="w-full p-0 h-2/3" />;
-export const ExportIcon = <CgExport class="w-full p-0 h-2/3" />;
-export const GenerateIcon = <FiArrowUpCircle class="w-full p-0 h-2/3" />;
-export const InformationIcon = <BsInfoCircle class="w-full p-0 h-2/3" />;
-export const RemoveLineIcon = <FaSolidMinus class="w-full p-0 h-2/3" />;
+export const AddLineIcon = () => <FaSolidPlus class="w-full p-0 h-2/3" />;
+export const ClearMapIcon = () => (
+  <FaRegularTrashCan class="w-full p-0 h-2/3" />
+);
+export const ExportIcon = () => <CgExport class="w-full p-0 h-2/3" />;
+export const GenerateIcon = () => <FiArrowUpCircle class="w-full p-0 h-2/3" />;
+export const InformationIcon = () => <BsInfoCircle class="w-full p-0 h-2/3" />;
+export const RemoveLineIcon = () => <FaSolidMinus class="w-full p-0 h-2/3" />;
