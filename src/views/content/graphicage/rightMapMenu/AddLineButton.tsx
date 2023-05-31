29687f67896c0cb870f44027904d04c94b3db84c
lucas <lucas.lvn97439@gmail.com>
2023-05-31 15:19:13 +0400

    use default button instead of default JSX

--- src/views/content/graphicage/rightMapMenu/AddLineButton.tsx
+++ src/views/content/graphicage/rightMapMenu/AddLineButton.tsx
@@ -2,32 +2,27 @@ import { useStateAction } from "../../../../StateAction";
 import { displayAddLineMessage } from "../../../../userInformation/utils";
 import { FaSolidPlus } from "solid-icons/fa";
 
-const [, { setModeAddLine, isInAddLineMode, setModeRead }] = useStateAction();
+import ButtonMap from "../../../../component/atom/ButtonMap";
 
-let refLabelMenu: HTMLLabelElement;
+const [, { setModeAddLine, isInAddLineMode, setModeRead }] = useStateAction();
+const Icon = <FaSolidPlus class="w-full p-0 h-2/3" />;
 
 export default function () {
+  const handleClick = () => {
+    if (isInAddLineMode()) {
+      setModeRead();
+    } else {
+      setModeAddLine();
+      displayAddLineMessage();
+    }
+  };
+
   return (
-    <div class="menu-btn group">
-      <span class="tooltip group-hover:scale-100">Ajouter une ligne</span>
-      <label
-        ref={refLabelMenu}
-        tabIndex={0}
-        class="custom-btn btn-circle hover:bg-[#062F3F] hover:text-[#0cc683]"
-        classList={{
-          "bg-[#062F3F] text-[#0cc683]": isInAddLineMode(),
-        }}
-        onClick={() => {
-          if (isInAddLineMode()) {
-            setModeRead();
-          } else {
-            setModeAddLine();
-            displayAddLineMessage();
-          }
-        }}
-      >
-        <FaSolidPlus class="w-full p-0 h-2/3" />
-      </label>
-    </div>
+    <ButtonMap
+      click={handleClick}
+      tooltip="Ajouter une ligne"
+      _class={isInAddLineMode()}
+      icon={Icon}
+    />
   );
 }
