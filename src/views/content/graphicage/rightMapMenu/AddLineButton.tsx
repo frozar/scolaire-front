5d801cd51ff7abcf7c1d8e8d5583f4737c2745e6
lucas <lucas.lvn97439@gmail.com>
2023-05-31 16:49:24 +0400

    use imported icon directely in props as jsx element

--- src/views/content/graphicage/rightMapMenu/AddLineButton.tsx
+++ src/views/content/graphicage/rightMapMenu/AddLineButton.tsx
@@ -5,7 +5,6 @@ import { FaSolidPlus } from "solid-icons/fa";
 import ButtonMap from "../../../../component/atom/ButtonMap";
 
 const [, { setModeAddLine, isInAddLineMode, setModeRead }] = useStateAction();
-const Icon = <FaSolidPlus class="w-full p-0 h-2/3" />;
 
 export default function () {
   const handleClick = () => {
@@ -22,7 +21,7 @@ export default function () {
       onClick={handleClick}
       tooltip="Ajouter une ligne"
       _class={isInAddLineMode()}
-      icon={Icon}
+      icon={<FaSolidPlus class="w-full p-0 h-2/3" />}
     />
   );
 }
