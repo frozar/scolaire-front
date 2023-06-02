863b7dfe4474058b24e2d3507e606d9160f7b834
lucas <lucas.lvn97439@gmail.com>
2023-06-02 16:42:47 +0400

    Integraiton of button to open menu LayerChoice

--- src/views/content/graphicage/rightMapMenu/RightMapMenu.tsx
+++ src/views/content/graphicage/rightMapMenu/RightMapMenu.tsx
@@ -4,16 +4,24 @@ import ClearButton from "./ClearButton";
 import GenerateButton from "./GenerateButton";
 import ExportButton from "./ExportButton";
 import InformationBoardButton from "./InformationBoardButton";
+import OpenLayerChoice from "./OpenLayerChoice";
+import { useStateGui } from "../../../../StateGui";
+
+const [, { getDisplayedLayerChoiceMenu }] = useStateGui();
 
 export default function () {
   return (
-    <div id="control-map-menu">
+    <div
+      id="control-map-menu"
+      classList={{ "active-layer-choice": getDisplayedLayerChoiceMenu() }}
+    >
       <InformationBoardButton />
       <AddLineButton />
       <RemoveLineButton />
       <ClearButton />
       <GenerateButton />
       <ExportButton />
+      <OpenLayerChoice />
     </div>
   );
 }
