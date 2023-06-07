2f3bff27803a6606ae6676ced557ab97502ffdf5
frozar <fabien.rozar@gmail.com>
2023-06-07 09:48:10 +0400

    Wip: url image profile

--- src/component/atom/LoginButton.stories.tsx
+++ src/component/atom/LoginButton.stories.tsx
@@ -18,7 +18,7 @@ export const NotLoggedButtonIcon: Story = {
 
 export const LoggedInButtonIcon: Story = {
   args: {
-    getProfilePic: () =>
-      "https://s.gravatar.com/avatar/89c51c7ed1dbc64a360ba375b76c6665?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Flu.png",
+    getProfilePic: () => "http://localhost:3000/profile-picture.jpeg",
+    // "https://s.gravatar.com/avatar/89c51c7ed1dbc64a360ba375b76c6665?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Flu.png",
   },
 };
