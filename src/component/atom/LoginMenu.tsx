c003aea7bad3f5b15f4fb0beb5765e72441f6fdf
lucas <lucas.lvn97439@gmail.com>
2023-06-09 11:10:17 +0400

    Remove comments

--- src/component/atom/LoginMenu.tsx
+++ src/component/atom/LoginMenu.tsx
@@ -20,11 +20,6 @@ export default function (props: LoginMenuProps) {
     return "translate-x-[" + String(res) + "rem]";
   };
 
-  // Slow load of story when use authenticated signal, ~ 4 - 5 second
-  // Noticed when use any signal stored in signaux.ts, load of storybook will decrease
-  console.log(authenticated());
-  // console.log(isRamassageReady());
-
   return (
     <button id="login-dialog" class={xOffsetClassName()} {...rest}>
       {local.authenticated ? "Se déconnecter" : "Se connecter"}
