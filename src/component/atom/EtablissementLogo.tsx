654fe2b7d48004cc86a8a340a92483eed64c351a
lucas <lucas.lvn97439@gmail.com>
2023-06-14 16:53:22 +0400

    Add LeftMenu logos inside atom folder

---
+++ src/component/atom/EtablissementLogo.tsx
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <svg
+      width="30"
+      height="25"
+      viewBox="0 0 30 25"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5.45455 13.8818V19.3364L15 24.5455L24.5455 19.3364V13.8818L15 19.0909L5.45455 13.8818ZM15 0L0 8.18182L15 16.3636L27.2727 9.66818V19.0909H30V8.18182L15 0Z" />
+    </svg>
+  );
+}
