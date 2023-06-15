177f2beb01f08a20a71d65d0bfa84a6a7b73c103
lucas <lucas.lvn97439@gmail.com>
2023-06-15 09:09:15 +0400

    setup LeftMenuItemList organism

---
+++ src/component/organism/LeftMenuItemList.tsx
@@ -0,0 +1,52 @@
+import { For, createSignal, onMount } from "solid-js";
+import LeftMenuItem from "../molecule/LeftMenuItem";
+import { useStateGui } from "../../StateGui";
+import MenuItemsFields from "../molecule/MenuItemFields";
+const [
+  ,
+  {
+    setSelectedMenu,
+    getSelectedMenu,
+    // toggleDisplayedLeftMenu,
+    getDisplayedLeftMenu,
+  },
+] = useStateGui();
+
+export default function () {
+  const [waitingToDisplayText, SetWaitingToDisplayText] = createSignal(
+    getDisplayedLeftMenu()
+  );
+
+  let refDivLeftMenu!: HTMLElement;
+
+  onMount(() => {
+    refDivLeftMenu.addEventListener("transitionend", () => {
+      SetWaitingToDisplayText(!waitingToDisplayText());
+    });
+  });
+
+  return (
+    <ul class="lateral-nav-list">
+      <For each={MenuItemsFields(waitingToDisplayText())}>
+        {(menuItemArg) => {
+          const { title, menuItem, Logo } = menuItemArg;
+
+          const isActiveItem = () => getSelectedMenu() === menuItem;
+          const isActiveText = () =>
+            getDisplayedLeftMenu() == true && waitingToDisplayText() == true;
+
+          return (
+            <LeftMenuItem
+              title={title}
+              menuItem={menuItem}
+              logo={Logo}
+              isActiveItem={isActiveItem()}
+              isActiveText={isActiveText()}
+              onClick={() => setSelectedMenu(menuItem)}
+            />
+          );
+        }}
+      </For>
+    </ul>
+  );
+}
