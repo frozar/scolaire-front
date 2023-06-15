579f09a8f742c22c6ea02d3c0d922da1be64cea8
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-15 17:13:20 +0400

    review cleaning: old comment deleted

--- src/views/layout/leftMenu/MenuItemFields.ts
+++ src/views/layout/leftMenu/MenuItemFields.ts
@@ -1,32 +1,16 @@
 import ArretsLogo from "../../../component/atom/ArretsLogo";
-// import DashboardLogo from "./logo/DashboardLogo";
 import EtablissementLogo from "../../../component/atom/EtablissementLogo";
 import GraphicageLogo from "../../../component/atom/GraphicageLogo";
-// import SettingsLogo from "./logo/SettingsLogo";
-// import SupportLogo from "./logo/SupportLogo";
-// import VoirieLogo from "./logo/VoirieLogo";
 import { MenuItemType } from "../../../type";
 
 export default function (displayText: boolean) {
   const menuItems: MenuItemType[] = [
-    // {
-    //   title: "Dashboard",
-    //   menuItem: "dashboard",
-    //   Logo: DashboardLogo,
-    //   displayText: displayText,
-    // },
     {
       label: "Graphicage",
       menuItem: "graphicage",
       Logo: GraphicageLogo,
       displayedLabel: displayText,
     },
-    // {
-    //   title: "Voirie",
-    //   menuItem: "voirie",
-    //   Logo: VoirieLogo,
-    //   displayText: displayText,
-    // },
     {
       label: "Établissements",
       menuItem: "etablissements",
@@ -39,18 +23,6 @@ export default function (displayText: boolean) {
       Logo: ArretsLogo,
       displayedLabel: displayText,
     },
-    // {
-    //   title: "Paramètres",
-    //   menuItem: "parametres",
-    //   Logo: SettingsLogo,
-    //   displayText: displayText,
-    // },
-    // {
-    //   title: "Support",
-    //   menuItem: "support",
-    //   Logo: SupportLogo,
-    //   displayText: displayText,
-    // },
   ];
   return menuItems;
 }
