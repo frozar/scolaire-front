071fed3e7dee3bd1e3b871756d8e3235054cd05c
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-26 14:49:21 +0400

    move EnterpriseLogo from components to views

--- src/views/layout/leftNav/LeftNav.tsx
+++ src/views/layout/leftNav/LeftNav.tsx
@@ -1,6 +1,6 @@
 import { Show, createSignal, mergeProps, onCleanup, onMount } from "solid-js";
 
-import EnterpriseLogo from "../../../component/atom/EnterpriseLogo";
+import EnterpriseLogo from "./component/atom/EnterpriseLogo";
 import OpenPictogram from "./component/atom/OpenPictogram";
 import ClosePictogram from "./component/atom/ClosePictogram";
 import { useStateGui } from "../../../StateGui";
