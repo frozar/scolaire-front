99345397f4defb90811e50a17969c13b06faf2fd
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-26 13:40:41 +0400

    move LeftNav from components to views

---
+++ src/views/layout/leftNav/LeftNav.stories.tsx
@@ -0,0 +1,28 @@
+import { createSignal } from "solid-js";
+import { Meta, StoryObj } from "storybook-solidjs";
+
+import LeftNavComponent from "./LeftNav";
+
+const meta = {
+  title: "LeftNav/LeftNav",
+  component: LeftNavComponent,
+  tags: ["autodocs"],
+} satisfies Meta<typeof LeftNavComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const [fakeGetDisplayedLeftMenu, fakeSetDisplayedLeftMenu] =
+  createSignal(false);
+
+function toggleFakeDisplayedLeftMenu() {
+  fakeSetDisplayedLeftMenu((bool) => !bool);
+}
+
+export const LeftNav: Story = {
+  args: {
+    displayedLabel: true,
+    getDisplayedLeftMenu: fakeGetDisplayedLeftMenu,
+    toggleDisplayedLeftMenu: toggleFakeDisplayedLeftMenu,
+  },
+};
---
+++ src/views/layout/leftNav/LeftNav.tsx
@@ -0,0 +1,94 @@
+import { Show, createSignal, mergeProps, onCleanup, onMount } from "solid-js";
+
+import EnterpriseLogo from "../../../component/atom/EnterpriseLogo";
+import OpenPictogram from "../../../component/atom/OpenPictogram";
+import ClosePictogram from "../../../component/atom/ClosePictogram";
+import { useStateGui } from "../../../StateGui";
+
+import LeftMenuItemList from "./LeftMenuItemList";
+
+import "./LeftNav.css";
+
+const [, { getDisplayedLeftMenu, toggleDisplayedLeftMenu }] = useStateGui();
+
+export interface LeftNavProps {
+  getDisplayedLeftMenu?: () => boolean;
+  toggleDisplayedLeftMenu?: () => void;
+}
+
+export default function (props: LeftNavProps) {
+  const mergedProps = mergeProps(
+    { getDisplayedLeftMenu, toggleDisplayedLeftMenu },
+    props
+  );
+
+  const [displayedLabel, setDisplayedLabel] = createSignal(false);
+
+  let refDivLeftMenu!: HTMLElement;
+
+  function computeIsOpened(width: string) {
+    const widthNumber = parseFloat(width.replace("px", ""));
+    return 72 < widthNumber;
+  }
+
+  function handleTransitionStart() {
+    const isOpened = computeIsOpened(getComputedStyle(refDivLeftMenu).width);
+    if (isOpened) {
+      setDisplayedLabel(false);
+    }
+  }
+
+  function handleTransitionEnd() {
+    const isOpened = computeIsOpened(getComputedStyle(refDivLeftMenu).width);
+    if (isOpened) {
+      setDisplayedLabel(true);
+    }
+  }
+
+  onMount(() => {
+    setDisplayedLabel(mergedProps.getDisplayedLeftMenu());
+
+    if (!refDivLeftMenu) {
+      return;
+    }
+
+    refDivLeftMenu.addEventListener("transitionstart", handleTransitionStart);
+    refDivLeftMenu.addEventListener("transitionend", handleTransitionEnd);
+  });
+
+  onCleanup(() => {
+    refDivLeftMenu.removeEventListener(
+      "transitionstart",
+      handleTransitionStart
+    );
+    refDivLeftMenu.removeEventListener("transitionend", handleTransitionEnd);
+  });
+
+  return (
+    <nav
+      id="lateral-nav"
+      classList={{ active: mergedProps.getDisplayedLeftMenu() }}
+      ref={refDivLeftMenu}
+    >
+      <div id="lateral-nav-header">
+        <EnterpriseLogo
+          displayedLeftMenu={mergedProps.getDisplayedLeftMenu()}
+        />
+      </div>
+
+      <LeftMenuItemList displayedLabel={displayedLabel()} />
+
+      <button
+        id="lateral-close"
+        onClick={() => mergedProps.toggleDisplayedLeftMenu()}
+      >
+        <Show
+          when={mergedProps.getDisplayedLeftMenu()}
+          fallback={<OpenPictogram />}
+        >
+          <ClosePictogram />
+        </Show>
+      </button>
+    </nav>
+  );
+}
