1079f8c0bcc74dba5b6bf8feca64545b4cf76f5b
lucas <lucas.lvn97439@gmail.com>
2023-06-27 14:56:00 +0400

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // watchForFileChanges: true,
   component: {
     devServer: {
       framework: "@lmiller1990/cypress-ct-solid-js",
