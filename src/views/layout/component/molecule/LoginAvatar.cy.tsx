5dfe0c7475820e1f2a7d334a535d632d6def5b2c
lucas <lucas.lvn97439@gmail.com>
2023-06-27 14:11:59 +0400

    Cypress test setup for LoginAvatar.tsx component

---
+++ src/views/layout/component/molecule/LoginAvatar.cy.tsx
@@ -0,0 +1,38 @@
+import LoginAvatar from "./LoginAvatar";
+describe("LoginAvatar component", () => {
+  it("When authenticated", () => {
+    const props = {
+      authenticated: true,
+      profilePicture:
+        "https://raw.githubusercontent.com/frozar/scolaire-front/main/public/profile-picture.jpeg",
+    };
+
+    cy.mount(() => (
+      <LoginAvatar
+        authenticated={props.authenticated}
+        profilePicture={props.profilePicture}
+      />
+    ));
+
+    cy.get("img").should("be.visible");
+    cy.get("svg").should("not.exist");
+  });
+
+  it("When not authenticated", () => {
+    const props = {
+      authenticated: false,
+      profilePicture:
+        "https://raw.githubusercontent.com/frozar/scolaire-front/main/public/profile-picture.jpeg",
+    };
+
+    cy.mount(() => (
+      <LoginAvatar
+        authenticated={props.authenticated}
+        profilePicture={props.profilePicture}
+      />
+    ));
+
+    cy.get("img").should("not.exist");
+    cy.get("svg").should("be.visible");
+  });
+});
