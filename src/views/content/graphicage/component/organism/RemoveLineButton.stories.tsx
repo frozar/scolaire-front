a3c9a92b0855d317a7f400c796bc888289eb3c16
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-29 10:46:13 +0400

    RemoveLineButton story done

---
+++ src/views/content/graphicage/component/organism/RemoveLineButton.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from "storybook-solidjs";
+
+import { createSignal } from "solid-js";
+import RemoveLineButtonComponent from "./RemoveLineButton";
+
+const meta = {
+  component: RemoveLineButtonComponent,
+  tags: ["autodocs"],
+} satisfies Meta<typeof RemoveLineButtonComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const [fakeIsInRemoveLineMode, setFakeIsInRemoveLineMode] = createSignal(false);
+
+function fakeHandleClick() {
+  setFakeIsInRemoveLineMode((bool) => !bool);
+}
+
+export const RemoveLineButton: Story = {
+  args: {
+    handleClick: fakeHandleClick,
+    isInRemoveLineMode: fakeIsInRemoveLineMode,
+    xOffset: "right",
+  },
+};
--- src/views/content/graphicage/component/organism/RemoveLineButton.tsx
+++ src/views/content/graphicage/component/organism/RemoveLineButton.tsx
@@ -5,7 +5,7 @@ import { displayRemoveLineMessage } from "../../../../../userInformation/utils";
 import { deselectAllPoints } from "../../Point";
 import { fetchBusLines } from "../../line/busLinesUtils";
 
-import { mergeProps } from "solid-js";
+import { mergeProps, splitProps } from "solid-js";
 import ButtonGraphicageRightMenu, {
   OffsetType,
 } from "../molecule/ButtonGraphicageRightMenu";
@@ -14,14 +14,12 @@ const [, { setModeRemoveLine, isInRemoveLineMode, setModeRead }] =
   useStateAction();
 
 export interface RemoveLineButtonProps {
+  handleClick?: () => void;
+  isInRemoveLineMode?: () => boolean;
   xOffset?: OffsetType;
 }
 
 export default function (props: RemoveLineButtonProps) {
-  const mergedProps = mergeProps({ xOffset: "left" as OffsetType }, props);
-
-  const xOffset = () => mergedProps.xOffset;
-
   const handleClick = () => {
     deselectAllPoints();
     if (isInRemoveLineMode()) {
@@ -34,13 +32,24 @@ export default function (props: RemoveLineButtonProps) {
     displayRemoveLineMessage();
   };
 
+  const mergedProps = mergeProps(
+    { handleClick, isInRemoveLineMode, xOffset: "left" as OffsetType },
+    props
+  );
+
+  const [local] = splitProps(mergedProps, [
+    "handleClick",
+    "isInRemoveLineMode",
+    "xOffset",
+  ]);
+
   return (
     <ButtonGraphicageRightMenu
-      onClick={handleClick}
+      onClick={local.handleClick}
       tooltip="Supprimer une ligne"
       icon={<FaSolidMinus class="w-full h-2/3" />}
-      isActive={isInRemoveLineMode()}
-      xOffset={xOffset()}
+      isActive={local.isInRemoveLineMode()}
+      xOffset={local.xOffset}
     />
   );
 }
