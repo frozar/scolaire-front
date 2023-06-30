f40bed56860462ebda041831369af310fdeabbbd
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-30 14:12:52 +0400

    cleaning

--- src/views/content/graphicage/component/organism/RightMapMenu.stories.tsx
+++ src/views/content/graphicage/component/organism/RightMapMenu.stories.tsx
@@ -10,71 +10,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// const informationBoardProps = {
-//   icon: (() => BsInfoCircle({ class: "h-10 w-10" })) as Element,
-//   onClick: toggleDisplayedInformationBoard,
-//   tooltip: "Afficher le panneau d'information",
-//   isActive: getDisplayedInformationBoard(),
-//   xOffset: "left" as OffsetType,
-// };
-
-// const [fakeGetDisplayedInformationBoard, setFakeDisplayedInformationBoard] =
-//   createSignal(false);
-
-// function fakeToggleDisplayedInformationBoard() {
-//   console.log("fakeToggleDisplayedInformationBoard");
-
-//   setFakeDisplayedInformationBoard((bool) => !bool);
-// }
-
-// const [fakeIsInAddLineMode, setFakeIsInAddLineMode] = createSignal(false);
-// const [fakeIsInRemoveLineMode, setFakeIsInRemoveLineMode] = createSignal(false);
-
-// function fakeAddLineButtonHandleClick() {
-//   console.log("fakeAddLineHandleClick");
-
-//   setFakeIsInRemoveLineMode((bool) => (bool ? !bool : bool));
-//   setFakeIsInAddLineMode((bool) => !bool);
-// }
-
-// function fakeRemoveLineButtonHandleClick() {
-// console.log("fakeRemoveLineHandleClick");
-
-// setFakeIsInAddLineMode((bool) => (bool ? !bool : bool));
-// setFakeIsInRemoveLineMode((bool) => !bool);
-// }
-
-// function fakeClearButtonHandleClick() {
-//   console.log("fakeClearButtonHandleClick");
-
-//   return;
-// }
-
 export const RightMapMenu: Story = {
   args: {
-    // toggleDisplayedInformationBoard: fakeToggleDisplayedInformationBoard,
-    // getDisplayedInformationBoard: fakeGetDisplayedInformationBoard,
-
-    // addLineButtonHandleClick: fakeAddLineButtonHandleClick,
-    // isInAddLineMode: fakeIsInAddLineMode,
-
-    // removeLineButtonHandleClick: fakeRemoveLineButtonHandleClick,
-    // isInRemoveLineMode: fakeIsInRemoveLineMode,
-
-    // clearButtonHandleClick: fakeClearButtonHandleClick,
-
-    // generateButtonHandleClick: () => {
-    //   console.log("fakeGenerateButtonHandleClick");
-
-    //   return;
-    // },
-
-    // exportButtonHandleClick: () => {
-    //   console.log("fakeExportButtonHandleClick");
-
-    //   return;
-    // },
-
     xOffset: "right",
   },
 };
--- src/views/content/graphicage/component/organism/RightMapMenu.tsx
+++ src/views/content/graphicage/component/organism/RightMapMenu.tsx
@@ -9,105 +9,25 @@ import GenerateButton from "./GenerateButton";
 import InformationBoardButton from "./InformationBoardButton";
 import RemoveLineButton from "./RemoveLineButton";
 
-// import { useStateAction } from "../../../../../StateAction";
-
-// import { exportButtonHandleClick } from "../../utils";
-
 import "./RightMapMenu.css";
 
-// const [, { getDisplayedInformationBoard, toggleDisplayedInformationBoard }] =
-//   useStateGui();
-
-// const [, { isInRemoveLineMode }] = useStateAction();
-
 export interface RightMapMenuProps {
-  // toggleDisplayedInformationBoard?: () => void;
-  // getDisplayedInformationBoard?: () => boolean;
-
-  // addLineButtonHandleClick?: () => void;
-  // isInAddLineMode?: () => boolean;
-
-  // removeLineButtonHandleClick?: () => void;
-  // isInRemoveLineMode?: () => boolean;
-
-  // clearButtonHandleClick?: () => void;
-
-  // generateButtonHandleClick?: () => void;
-
-  // exportButtonHandleClick?: () => void;
-
   xOffset?: OffsetType;
 }
 
 export default function (props: RightMapMenuProps) {
-  const mergedProps = mergeProps(
-    {
-      // toggleDisplayedInformationBoard,
-      // getDisplayedInformationBoard,
+  const mergedProps = mergeProps({ xOffset: "left" as OffsetType }, props);
 
-      // addLineButtonHandleClick,
-      // isInAddLineMode,
-
-      // removeLineButtonHandleClick,
-      // isInRemoveLineMode,
-
-      // clearButtonHandleClick,
-
-      // generateButtonHandleClick,
-
-      // exportButtonHandleClick,
-
-      xOffset: "left" as OffsetType,
-    },
-    props
-  );
-
-  const [local] = splitProps(mergedProps, [
-    // "toggleDisplayedInformationBoard",
-    // "getDisplayedInformationBoard",
-    // "addLineButtonHandleClick",
-    // "isInAddLineMode",
-    // "removeLineButtonHandleClick",
-    // "isInRemoveLineMode",
-    // "clearButtonHandleClick",
-    // "generateButtonHandleClick",
-    // "exportButtonHandleClick",
-    "xOffset",
-  ]);
-
-  // const [localAddLineButton] = splitProps(mergedProps, [
-  //   "addLineButtonHandleClick",
-  // ]);
+  const [local] = splitProps(mergedProps, ["xOffset"]);
 
   return (
-    <div id="control-map-menu">
-      <InformationBoardButton
-        xOffset={local.xOffset}
-        // toggleDisplayedInformationBoard={local.toggleDisplayedInformationBoard}
-        // getDisplayedInformationBoard={local.getDisplayedInformationBoard}
-      />
-      <AddLineButton
-        xOffset={local.xOffset}
-        // handleClick={local.addLineButtonHandleClick}
-        // isInAddLineMode={local.isInAddLineMode}
-      />
-      <RemoveLineButton
-        xOffset={local.xOffset}
-        // handleClick={local.removeLineButtonHandleClick}
-        // isInRemoveLineMode={local.isInRemoveLineMode}
-      />
-      <ClearButton
-        xOffset={local.xOffset}
-        // handleClick={local.clearButtonHandleClick}
-      />
-      <GenerateButton
-        xOffset={local.xOffset}
-        // handleClick={local.generateButtonHandleClick}
-      />
-      <ExportButton
-        xOffset={mergedProps.xOffset}
-        // handleClick={local.exportButtonHandleClick}
-      />
+    <div id="right-map-menu">
+      <InformationBoardButton xOffset={local.xOffset} />
+      <AddLineButton xOffset={local.xOffset} />
+      <RemoveLineButton xOffset={local.xOffset} />
+      <ClearButton xOffset={local.xOffset} />
+      <GenerateButton xOffset={local.xOffset} />
+      <ExportButton xOffset={mergedProps.xOffset} />
     </div>
   );
 }
