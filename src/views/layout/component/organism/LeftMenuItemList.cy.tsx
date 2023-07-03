736a5d40110b5aa5b7e20f19b9d6f4f5147bf376
lucas <lucas.lvn97439@gmail.com>
2023-07-03 15:05:01 +0400

    add snapshot test for selected item graphicage

--- src/views/layout/component/organism/LeftMenuItemList.cy.tsx
+++ src/views/layout/component/organism/LeftMenuItemList.cy.tsx
@@ -1,12 +1,19 @@
+import { SelectedMenuType } from "../../../../type";
 import LeftMenuItemList from "./LeftMenuItemList";
 
 describe("LeftMenuItemList component", () => {
   const props = {
     displayedLabel: true,
+    getSelectedMenu: () => "dashboard" as SelectedMenuType,
   };
 
   it("LeftMenuItemList check snapshot with displayed labels", () => {
-    cy.mount(() => <LeftMenuItemList displayedLabel={props.displayedLabel} />);
+    cy.mount(() => (
+      <LeftMenuItemList
+        displayedLabel={props.displayedLabel}
+        getSelectedMenu={props.getSelectedMenu}
+      />
+    ));
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
@@ -14,10 +21,30 @@ describe("LeftMenuItemList component", () => {
   });
 
   it("LeftMenuItemList check snapshot without displayed labels", () => {
-    cy.mount(() => <LeftMenuItemList displayedLabel={!props.displayedLabel} />);
+    cy.mount(() => (
+      <LeftMenuItemList
+        displayedLabel={!props.displayedLabel}
+        getSelectedMenu={props.getSelectedMenu}
+      />
+    ));
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
     cy.get("ul").compareSnapshot("LeftMenuItemList-no-labels-displayed", 0.01);
+  });
+
+  it("LeftMenuItemList check if only one item is selected, item: graphicage", () => {
+    props.getSelectedMenu = () => "graphicage";
+
+    cy.mount(() => (
+      <LeftMenuItemList
+        displayedLabel={props.displayedLabel}
+        getSelectedMenu={props.getSelectedMenu}
+      />
+    ));
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    cy.get("ul").compareSnapshot("LeftMenuItemList-one-selected", 0.01);
   });
 });
