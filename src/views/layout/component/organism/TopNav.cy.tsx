5284355a9985063d8a0d3e7dfaca46e8a467489d
lucas <lucas.lvn97439@gmail.com>
2023-07-04 17:07:43 +0400

    WIP: watch to snapshot TopNav with LoginDropdownmenu

--- src/views/layout/component/organism/TopNav.cy.tsx
+++ src/views/layout/component/organism/TopNav.cy.tsx
@@ -5,19 +5,30 @@ describe("TopNav component", () => {
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
-    cy.get("button").compareSnapshot("TopNav", 0.01);
+    cy.get("#nav-top").compareSnapshot("TopNav", 0.01);
   });
 
   it("TopNav check login dropdown children", () => {
     cy.mount(() => <TopNav />);
 
-    cy.get("#nav-top").find("button#login-btn").as("loginButton");
-
-    cy.get("@loginButton").click();
+    cy.get("#login-btn").click();
     cy.get("#login-menu-container").should("exist");
 
-    cy.get("@loginButton").click();
-    cy.wait(500);
+    cy.get("#login-btn").click();
+
+    // Todo: Can't get screenshot of login menu in snap of top nav, watch if not possible to translate login menu inside de top nav to get it
+
+    // cy.get("#login-menu-container").then((component) => {
+    //   cy.wrap(component).invoke("css", "transform", "translateX(-10px)");
+    //   cy.wrap(component).invoke("css", "transform", "translateY(-30px)");
+    //   cy.wrap(component).invoke("css", "z-index", "5000");
+
+    //   cy.wait(500);
+    //   cy.task("log", "test snapshot here");
+    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //   //@ts-ignore
+    //   cy.get("#nav-top").compareSnapshot("TopNav-2", 0.01);
+    // });
 
     cy.get("#login-menu-container").should("not.exist");
   });
