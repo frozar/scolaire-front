1983e6d8a639f9b72cff8e969a2b379880c7494b
lucas <lucas.lvn97439@gmail.com>
2023-07-04 15:33:11 +0400

    TopNav test & snapshot

---
+++ src/views/layout/component/organism/TopNav.cy.tsx
@@ -0,0 +1,24 @@
+import TopNav from "./TopNav";
+describe("TopNav component", () => {
+  it("TopNav check snapshot", () => {
+    cy.mount(() => <TopNav />);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    cy.get("button").compareSnapshot("TopNav", 0.01);
+  });
+
+  it("TopNav check children", () => {
+    cy.mount(() => <TopNav />);
+
+    cy.get("#nav-top").find("button#login-btn").as("loginButton");
+
+    cy.get("@loginButton").click();
+    cy.get("#login-menu-container").should("exist");
+
+    cy.get("@loginButton").click();
+    cy.wait(500);
+
+    cy.get("#login-menu-container").should("not.exist");
+  });
+});
