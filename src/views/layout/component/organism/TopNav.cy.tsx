f838cac8f0066299b4145f031eae50971569141a
lucas <lucas.lvn97439@gmail.com>
2023-07-05 10:00:54 +0400

    Adjust test to get login menu in snapshot when it is open

--- src/views/layout/component/organism/TopNav.cy.tsx
+++ src/views/layout/component/organism/TopNav.cy.tsx
@@ -1,4 +1,5 @@
 import TopNav from "./TopNav";
+
 describe("TopNav component", () => {
   it("TopNav check snapshot", () => {
     cy.mount(() => <TopNav />);
@@ -11,25 +12,30 @@ describe("TopNav component", () => {
   it("TopNav check login dropdown children", () => {
     cy.mount(() => <TopNav />);
 
+    cy.get("#login-btn").then((topNav) => {
+      cy.wrap(topNav).invoke("css", "outline", "none");
+    });
+
+    cy.get("#nav-top").then((topNav) => {
+      cy.wrap(topNav).invoke("css", "height", "150px");
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    cy.get("#nav-top").compareSnapshot("TopNav-login-menu-close", 0.01);
+
+    // Open the login menu
     cy.get("#login-btn").click();
-    cy.get("#login-menu-container").should("exist");
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    cy.get("#nav-top").compareSnapshot("TopNav-login-menu-open", 0.01);
+
+    // Close the menu login
     cy.get("#login-btn").click();
 
-    // Todo: Can't get screenshot of login menu in snap of top nav, watch if not possible to translate login menu inside de top nav to get it
-
-    // cy.get("#login-menu-container").then((component) => {
-    //   cy.wrap(component).invoke("css", "transform", "translateX(-10px)");
-    //   cy.wrap(component).invoke("css", "transform", "translateY(-30px)");
-    //   cy.wrap(component).invoke("css", "z-index", "5000");
-
-    //   cy.wait(500);
-    //   cy.task("log", "test snapshot here");
-    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //   //@ts-ignore
-    //   cy.get("#nav-top").compareSnapshot("TopNav-2", 0.01);
-    // });
-
-    cy.get("#login-menu-container").should("not.exist");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    cy.get("#nav-top").compareSnapshot("TopNav-login-menu-close", 0.01);
   });
 });
