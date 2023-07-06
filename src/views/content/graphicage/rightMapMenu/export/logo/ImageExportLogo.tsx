1fd10f5ee77c6c998d374e1584a091168008df13
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-06 14:39:22 +0400

    fix: currentColor of ImageExportLogo and GtfsExportLogo

--- src/views/content/graphicage/rightMapMenu/export/logo/ImageExportLogo.tsx
+++ src/views/content/graphicage/rightMapMenu/export/logo/ImageExportLogo.tsx
@@ -5,7 +5,7 @@ export default function () {
       fill="none"
       viewBox="0 0 24 24"
       stroke-width="1.5"
-      stroke="#A6ADBA"
+      stroke="currentColor"
       class="w-6 h-6"
     >
       <path
