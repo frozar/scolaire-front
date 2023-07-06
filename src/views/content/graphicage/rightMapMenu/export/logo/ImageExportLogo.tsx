cfa7d0e31e641d74b7dbe3df236a27c4c7af7081
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-06 13:20:21 +0400

    wip

--- src/views/content/graphicage/rightMapMenu/export/logo/ImageExportLogo.tsx
+++ src/views/content/graphicage/rightMapMenu/export/logo/ImageExportLogo.tsx
@@ -5,7 +5,7 @@ export default function () {
       fill="none"
       viewBox="0 0 24 24"
       stroke-width="1.5"
-      stroke="currentColor"
+      stroke="#A6ADBA"
       class="w-6 h-6"
     >
       <path
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: "media",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
@@ -10,14 +11,23 @@ module.exports = {
       layoutLeft: "1401",
     },
     // colors: {
-    //   "green-base": "0cc683",
-    //   "red-base": "f44434",
-    //   "dark-teal": "062f3f",
-    //   "dark-shade-teal": "062f3fcc",
-    //   "gray-base": "aeb8b4",
-    //   "gray-shade-base": "ccd6d2",
+    //   "green-base": "#0cc683",
+    //   "red-base": "#f44434",
+    //   "dark-teal": "#062f3f",
+    //   "dark-shade-teal": "#062f3fcc",
+    //   "gray-base": "#aeb8b4",
+    //   "gray-shade-base": "#ccd6d2",
     // },
-    extend: {},
+    extend: {
+      colors: {
+        "green-base": "#0cc683",
+        "red-base": "#f44434",
+        "dark-teal": "#062f3f",
+        "dark-shade-teal": "#062f3fcc",
+        "gray-base": "#aeb8b4",
+        "gray-shade-base": "#ccd6d2",
+      },
+    },
   },
   purge: {
     options: {
