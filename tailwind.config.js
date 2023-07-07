f7afddff829cc5a4c1d05a56aef52a88049ff7fc
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-07 10:29:29 +0400

    cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,11 @@ module.exports = {
   darkMode: "media",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 
-  /* Rebuild cypress docker when this file get update */
   theme: {
     zIndex: {
       layoutTop: "1400",
       layoutLeft: "1401",
     },
-    // colors: {
-    //   "green-base": "#0cc683",
-    //   "red-base": "#f44434",
-    //   "dark-teal": "#062f3f",
-    //   "dark-shade-teal": "#062f3fcc",
-    //   "gray-base": "#aeb8b4",
-    //   "gray-shade-base": "#ccd6d2",
-    // },
     extend: {
       colors: {
         "green-base": "#0cc683",
