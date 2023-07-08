682d4f3cb0b6039b83d79ab1b8ab8091fc636045
lucas <lucas.lvn97439@gmail.com>
2023-07-08 19:00:02 +0400

    Create TableColumn component & css

---
+++ src/views/content/etablissement/component/atom/TableColumn.tsx
@@ -0,0 +1,34 @@
+import { JSXElement, Show, children, mergeProps } from "solid-js";
+import "./TableColumn.css";
+
+interface TableBldyRowColumnProps {
+  children: JSXElement;
+  class?: string;
+  classVariant?: "table-head-col" | "table-head-col-checkbox";
+}
+
+export default function (props: TableBldyRowColumnProps) {
+  const child = children(() => props.children);
+
+  const mergedProps = mergeProps(
+    {
+      // eslint-disable-next-line solid/reactivity
+      class: `${props.class} + " " + ${props.classVariant}`,
+    },
+    props
+  );
+
+  return (
+    <Show
+      when={
+        mergedProps.classVariant == "table-head-col" ||
+        mergedProps.classVariant == "table-head-col-checkbox"
+      }
+      fallback={<td>{child()}</td>}
+    >
+      <th scope="col" class={mergedProps.class}>
+        {child()}
+      </th>
+    </Show>
+  );
+}
