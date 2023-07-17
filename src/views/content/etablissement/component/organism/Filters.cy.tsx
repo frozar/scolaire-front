e0f61330c5575e6b8d2f5b56d3b32bfdcb820d69
lucas <lucas.lvn97439@gmail.com>
2023-07-17 14:28:45 +0400

    add Filters component, test & snapshot

---
+++ src/views/content/etablissement/component/organism/Filters.cy.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import Filters from "./Filters";
+
+describe("Filters component", () => {
+  beforeEach(() => {
+    cy.mount(Filters);
+  });
+
+  it("Filters check elements", () => {
+    cy.get("#action-selector").should("exist");
+    cy.get("button").eq(0).contains("Ajouter");
+    cy.get("button").eq(1).contains("Exporter");
+    cy.get("button").eq(2).contains("Importer");
+    cy.get(".input-field").find(".input-search-logo").should("exist");
+    cy.get(".input-field")
+      .find(".input")
+      .should("have.attr", "placeholder", "Recherche");
+  });
+
+  it("Filters snapshot", () => {
+    // TODO Check why only 500px width image is generated and not more
+    // https://github.com/uktrade/cypress-image-diff/issues/100
+    cy.get("#filters").compareSnapshot("filters", 0.01);
+  });
+});
