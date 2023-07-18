5a5ee2a4f3f71424ce1a9b51acfe6e1b910eddfb
lucas <lucas.lvn97439@gmail.com>
2023-07-18 15:06:38 +0400

    update test & snapshot of TableBody

--- src/views/content/etablissement/component/organism/TableBody.cy.tsx
+++ src/views/content/etablissement/component/organism/TableBody.cy.tsx
@@ -1,28 +1,46 @@
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 import EtablissementItem from "../../EtablissementItem";
 import TableBody from "./TableBody";
 
-describe("TableBody component", () => {
-  const [items, setItems] = createSignal({
-    id: 1,
-    name: "Ecole DE BEAUMONT",
-    quantity: 149,
-    nbLine: 0,
-    lon: 1,
-    lat: 1,
-    selected: false,
-  });
+describe("TableBody component with etablissement item row", () => {
+  const [items, setItems] = createSignal([
+    {
+      id: 1,
+      name: "cell 1",
+      quantity: 149,
+      nbLine: 0,
+      lon: 1,
+      lat: 1,
+      selected: false,
+    },
+    {
+      id: 2,
+      name: "cell 2",
+      quantity: 149,
+      nbLine: 0,
+      lon: 1,
+      lat: 1,
+      selected: false,
+    },
+  ]);
 
   beforeEach(() => {
     cy.mount(() => (
       <TableBody>
-        <EtablissementItem item={items()} setEtablissements={setItems} />
+        <For each={items()}>
+          {(fields) => {
+            return (
+              <EtablissementItem item={fields} setEtablissements={setItems} />
+            );
+          }}
+        </For>
       </TableBody>
     ));
   });
 
-  it("TableBody check elements", () => {
-    cy.get("tbody").find("tr").find("td").should("exist");
+  it("TableBody check elements & snapshot", () => {
+    cy.get("tbody").find("tr").find("td").eq(0).should("exist");
+    cy.get("tbody").find("tr").find("td").eq(1).should("exist");
     cy.get("tbody").compareSnapshot("body", 0.01);
   });
 });
