90660ad98ab1f6e33ea0fc79e1a646a26b375e09
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-18 15:26:29 +0400

    wip

--- src/views/layout/component/organism/TopNav.cy.tsx
+++ src/views/layout/component/organism/TopNav.cy.tsx
@@ -1,6 +1,10 @@
 import TopNav from "./TopNav";
 
 describe("TopNav component", () => {
+  beforeEach(() => {
+    cy.viewport(1000, 1000);
+  });
+
   it("TopNav check snapshot", () => {
     cy.mount(() => <TopNav />);
 
@@ -8,24 +12,30 @@ describe("TopNav component", () => {
   });
 
   it("TopNav check login dropdown children", () => {
-    cy.mount(() => <TopNav />);
+    // cy.mount(() => <TopNav />);
+    cy.mount(() => (
+      <div class="h-28" id="to-screenshot">
+        <TopNav />
+      </div>
+    )); // h-max ; h-28
 
     cy.get("#login-btn").then((topNav) => {
       cy.wrap(topNav).invoke("css", "outline", "none");
     });
 
-    cy.get("#top-nav").then((topNav) => {
-      cy.wrap(topNav).invoke("css", "height", "150px");
-    });
+    // cy.get("#top-nav").then((topNav) => {
+    //   cy.wrap(topNav).invoke("css", "height", "150px");
+    // });
 
-    cy.get("#top-nav").compareSnapshot("TopNav-login-menu-close", 0.01);
+    // cy.get("#top-nav").compareSnapshot("TopNav-login-menu-close", 0.01);
 
     // Open the login menu
     cy.get("#login-btn").click();
-    cy.get("#top-nav").compareSnapshot("TopNav-login-menu-open", 0.01);
+    // cy.get("#top-nav").compareSnapshot("TopNav-login-menu-open", 0.01);
+    cy.get("#to-screenshot").compareSnapshot("TopNav-login-menu-open", 0.01);
 
     // Close the menu login
-    cy.get("#login-btn").click();
-    cy.get("#top-nav").compareSnapshot("TopNav-login-menu-close", 0.01);
+    // cy.get("#login-btn").click();
+    // cy.get("#top-nav").compareSnapshot("TopNav-login-menu-close", 0.01);
   });
 });
