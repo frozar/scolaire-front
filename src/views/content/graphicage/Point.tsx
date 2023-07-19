3590e8508845f2de2862322ffdb0d4099b841ea5
lucas <lucas.lvn97439@gmail.com>
2023-07-19 11:38:15 +0400

    use externalised functions

--- src/views/content/graphicage/Point.tsx
+++ src/views/content/graphicage/Point.tsx
@@ -1,23 +1,18 @@
-import L, { LeafletMouseEvent } from "leaflet";
+import L from "leaflet";
 import { createEffect, on, onCleanup, onMount } from "solid-js";
 import {
   NatureEnum,
   PointEtablissementType,
-  PointIdentityType,
   PointRamassageType,
 } from "../../../type";
 
 import { useStateAction } from "../../../StateAction";
 import {
   getLeafletMap,
-  points,
   setIsEtablissementReady,
   setIsRamassageReady,
 } from "../../../signaux";
-import { renderAnimation } from "./animation";
-import { deselectAllBusLines } from "./line/busLinesUtils";
-
-export const linkMap = new Map<number, L.CircleMarker>();
+import { buildCircleEvent, linkMap } from "./pointUtils";
 
 const [
   ,
@@ -28,25 +23,27 @@ const [
   },
 ] = useStateAction();
 
-function selectPointById(targerIdPoint: number) {
-  points().map((point) => point.setSelected(targerIdPoint == point.idPoint));
-}
+// function selectPointById(targerIdPoint: number) {
+//   points().map((point) => point.setSelected(targerIdPoint == point.idPoint));
+// }
 
-export function deselectAllPoints() {
-  points().map((point) => point.setSelected(false));
-}
+// export function deselectAllPoints() {
+//   points().map((point) => point.setSelected(false));
+// }
 
 const minSizeValue = 5;
 const maxSizeValue = 10;
 const range = maxSizeValue - minSizeValue;
 
-export default function (props: {
+interface PointProps {
   point: PointRamassageType | PointEtablissementType;
   isLast: boolean;
   nature: NatureEnum;
   minQuantity: number;
   maxQuantity: number;
-}) {
+}
+
+export default function (props: PointProps) {
   const point = () => props.point;
   const isLast = () => props.isLast;
   const nature = () => props.nature;
@@ -56,37 +53,17 @@ export default function (props: {
   let circle: L.CircleMarker;
 
   function buildCircle(point: PointEtablissementType): L.CircleMarker {
-    // console.log("point", point);
-    // const location = point.location;
-    // const lonlat = location.split("(")[1].split(")")[0];
-    // const lon = Number(lonlat.split(" ")[0]);
-    // const lat = Number(lonlat.split(" ")[1]);
     const lon = point.lon;
     const lat = point.lat;
-
-    // console.log("location", location);
-    // console.log("lonlat", lonlat);
-    // console.log("lon", lon);
-    // console.log("lat", lat);
-
-    // console.log("minQuantity", minQuantity());
-    // console.log("maxQuantity", maxQuantity());
-
-    // console.log("point.quantity", point.quantity);
-    // console.log("minQuantity", minQuantity());
-    // console.log("maxQuantity", maxQuantity());
 
     const coef =
       minQuantity() == maxQuantity()
         ? 0
         : (point.quantity - minQuantity()) / (maxQuantity() - minQuantity());
-    // console.log("coef", coef);
 
     const radiusValue = coef * range + minSizeValue;
-    // console.log("radiusValue", radiusValue);
 
     const { nature } = point;
-    // console.log("nature", nature);
 
     const [color, fillColor, radius, weight] =
       nature === NatureEnum.ramassage
@@ -94,13 +71,7 @@ export default function (props: {
         : nature === NatureEnum.etablissement
         ? ["green", "white", 12, 4]
         : ["white", "#000", 18, 4];
-    // console.log("color", color);
-    // console.log("fillColor", fillColor);
-    // console.log("radius", radius);
-    // console.log("weight", weight);
 
-    // console.log("lon", lon, "typeof lon", typeof lon);
-    // console.log("lat", lat);
     return L.circleMarker([lat, lon], {
       color,
       fillColor,
@@ -109,38 +80,8 @@ export default function (props: {
       weight,
       pane: "markerPane",
     })
-      .on("click", () => {
-        // Select the current element to display information
-
-        if (!isInAddLineMode()) {
-          deselectAllBusLines();
-          selectPointById(point.idPoint);
-          return;
-        }
-
-        const pointIdentity: PointIdentityType = {
-          id: point.id,
-          idPoint: point.idPoint,
-          nature: point.nature,
-        };
-
-        addPointToLineUnderConstruction(pointIdentity);
-
-        if (!(1 < getLineUnderConstruction().stops.length)) {
-          return;
-        }
-
-        // Highlight point ramassage
-        for (const associatedPoint of point.associatedPoints()) {
-          let element;
-          if ((element = linkMap.get(associatedPoint.idPoint)?.getElement())) {
-            renderAnimation(element);
-          }
-        }
-      })
-      .on("dblclick", (event: LeafletMouseEvent) => {
-        L.DomEvent.stopPropagation(event);
-      })
+      .on("click", () => buildCircleEvent.onClick(point))
+      .on("dblclick", buildCircleEvent.onDBLClick)
       .on("mouseover", () => {
         for (const associatedPoint of point.associatedPoints()) {
           const element = linkMap.get(associatedPoint.idPoint)?.getElement();
@@ -197,8 +138,6 @@ export default function (props: {
     if (!leafletMap) {
       return;
     }
-
-    // console.log("point", point());
 
     circle = buildCircle(point());
     circle.addTo(leafletMap);
