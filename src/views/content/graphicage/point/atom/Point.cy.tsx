2111db7b70e5e887ff0f6ed86cba26f052d595fd
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-20 13:41:36 +0400

    add cypress test & snapshot for point

---
+++ src/views/content/graphicage/point/atom/Point.cy.tsx
@@ -0,0 +1,15 @@
+import Point from "./Point.story";
+
+describe("Checkbox component", () => {
+  it("Etablissement Point", () => {
+    cy.mount(() => (
+      <Point radius={12} weight={4} borderColor="green" fillColor="white" />
+    ));
+    const retryOptions = {
+      limit: 5, // max number of retries
+      delay: 500, // delay before next iteration, ms
+    };
+
+    cy.get("#map-container").compareSnapshot("school", 0.01, retryOptions);
+  });
+});
