6d70dd4110e5f0864a102904dd34f3e868e26583
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-20 17:14:34 +0400

    fake singleton for storybook map

--- .storybook/utils/mapWrapper.ts
+++ .storybook/utils/mapWrapper.ts
@@ -1,8 +1,15 @@
 import L from "leaflet";
 import { layerTilesList } from "../../src/views/content/graphicage/constant";
 
+let map: L.Map;
+
 export const initialiseMap = (idDiv: string) => {
-  const map = L.map(idDiv).setView([-20.9466588303741, 55.5343806753509], 15);
+  if (map != undefined) {
+    map.off();
+    map.remove();
+  }
+
+  map = L.map(idDiv).setView([-20.9466588303741, 55.5343806753509], 15);
   layerTilesList[0].tileContent.addTo(map);
   return map;
 };
