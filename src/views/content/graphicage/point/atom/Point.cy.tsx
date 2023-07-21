c0fdd06e7bf5707e3618e9433004cef33c842882
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-21 10:58:50 +0400

    add cypress tests

--- src/views/content/graphicage/point/atom/Point.cy.tsx
+++ src/views/content/graphicage/point/atom/Point.cy.tsx
@@ -1,6 +1,11 @@
 import Point from "./Point.mapWrapper.story";
 
 describe("Checkbox component", () => {
+  const retryOptions = {
+    limit: 5, // max number of retries
+    delay: 500, // delay before next iteration, ms
+  };
+
   it("Etablissement Point", () => {
     cy.mount(() => (
       <Point
@@ -11,12 +16,35 @@ describe("Checkbox component", () => {
         isBlinking={false}
       />
     ));
-
-    const retryOptions = {
-      limit: 5, // max number of retries
-      delay: 500, // delay before next iteration, ms
-    };
-
+    cy.wait(1000);
     cy.get("#map-container").compareSnapshot("school", 0.01, retryOptions);
+  });
+
+  it("Ramassage Point", () => {
+    cy.mount(() => (
+      <Point
+        radius={8}
+        weight={4}
+        borderColor="red"
+        fillColor="white"
+        isBlinking={false}
+      />
+    ));
+    cy.wait(1000);
+    cy.get("#map-container").compareSnapshot("stop", 0.01, retryOptions);
+  });
+
+  it("Check blinking", () => {
+    cy.mount(() => (
+      <Point
+        radius={12}
+        weight={4}
+        borderColor="green"
+        fillColor="white"
+        isBlinking={true}
+      />
+    ));
+
+    cy.get(".map-point").should("have.class", "circle-animation-green");
   });
 });
