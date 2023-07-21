14bdce04afaefd2df9902fb4151757c5a5e4f02f
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-21 16:37:34 +0400

    wip: working cypress tests on point without fake component

--- .storybook/utils/mapWrapper.ts
+++ .storybook/utils/mapWrapper.ts
@@ -1,15 +1,39 @@
 import L from "leaflet";
-import { layerTilesList } from "../../src/views/content/graphicage/constant";
+import "leaflet/dist/leaflet.css";
 
-let map: L.Map;
+// let map: L.Map;
+let mapTabs: {
+  idDiv: string;
+  map: L.Map;
+}[] = [];
 
-export const initMap = (idDiv: string) => {
-  if (map != undefined) {
-    map.off();
-    map.remove();
+const initMap = (idDiv: string) => {
+  const newMap = L.map(idDiv).setView(
+    [-20.9466588303741, 55.5343806753509],
+    15
+  );
+
+  // Cannot use the same instance of L.tileLayer for multiple maps
+  L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(newMap);
+
+  return newMap;
+};
+
+export const initialiseMap = (idDiv: string) => {
+  let map = mapTabs.filter((m) => m.idDiv === idDiv)[0];
+  const newMap = initMap(idDiv);
+
+  if (map == undefined) {
+    mapTabs.push({ idDiv, map: newMap });
+    return newMap;
   }
 
-  map = L.map(idDiv).setView([-20.9466588303741, 55.5343806753509], 15);
-  layerTilesList[0].tileContent.addTo(map);
-  return map;
+  map.map.off();
+  map.map.remove();
+  map.map = newMap;
+
+  return map.map;
 };
