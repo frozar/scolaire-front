78b0ed42e21a695faace0b0a685004b7573250e9
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-21 15:33:23 +0400

    fakeComponent (Point.mapWrapper.story.tsx) no longer used in point.stories.tsx

--- src/views/content/graphicage/point/atom/Point.stories.tsx
+++ src/views/content/graphicage/point/atom/Point.stories.tsx
@@ -1,30 +1,92 @@
-import { Meta, StoryObj } from "storybook-solidjs";
+// import { Meta, StoryObj } from "storybook-solidjs";
 
-import MapWithPointComponent from "./Point.mapWrapper.story";
+// import MapWithPointComponent from "./Point.mapWrapper.story";
+
+// const meta = {
+//   component: MapWithPointComponent,
+// } satisfies Meta<typeof MapWithPointComponent>;
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const EtablissementPoint: Story = {
+//   args: {
+//     radius: 12,
+//     weight: 4,
+//     borderColor: "green",
+//     fillColor: "white",
+//     isBlinking: false,
+//   },
+// };
+
+// export const RamassagePoint: Story = {
+//   args: {
+//     radius: 5,
+//     weight: 2,
+//     borderColor: "red",
+//     fillColor: "white",
+//     isBlinking: false,
+//   },
+// };
+
+import "leaflet/dist/leaflet.css";
+import { Meta, StoryObj } from "storybook-solidjs";
+import { initialiseMap } from "../../../../../../.storybook/utils/mapWrapper";
+import PointComponent from "./Point";
 
 const meta = {
-  component: MapWithPointComponent,
-} satisfies Meta<typeof MapWithPointComponent>;
+  component: PointComponent,
+  tags: ["autodocs"],
+} satisfies Meta<typeof PointComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const EtablissementPoint: Story = {
-  args: {
-    radius: 12,
-    weight: 4,
-    borderColor: "green",
-    fillColor: "white",
-    isBlinking: false,
-  },
-};
+interface PointProps {
+  idPoint: number;
+  onIsLast: () => void;
+  lat: number;
+  lon: number;
+  map: L.Map;
+  onClick: () => void;
+  borderColor: string;
+  fillColor: string;
+  isLast: boolean;
+  onDBLClick: () => void;
+  onMouseOut: () => void;
+  onMouseOver: () => void;
+  radius: number;
+  weight: number;
+  isBlinking: boolean;
+}
 
-export const RamassagePoint: Story = {
+export const Point: Story = {
+  render: (props: PointProps) => {
+    const div = document.getElementById("map-container");
+    if (div) {
+      div.remove();
+    }
+
+    return (
+      <div id="map-container" style={{ width: "100%", height: "500px" }}>
+        <PointComponent {...props} map={initialiseMap("map-container")} />
+      </div>
+    );
+  },
   args: {
-    radius: 5,
-    weight: 2,
+    idPoint: 1,
+    onIsLast: () => console.log("ok"),
+    lat: -20.9466588303741,
+    lon: 55.5343806753509,
+    onClick: () => console.log("onClick"),
     borderColor: "red",
     fillColor: "white",
+    isLast: false,
+    onDBLClick: () => console.log("onDBLClick"),
+    onMouseOut: () => console.log("onMouseOut"),
+    onMouseOver: () => console.log("onMouseOver"),
+    radius: 8,
+    weight: 4,
     isBlinking: false,
   },
 };
