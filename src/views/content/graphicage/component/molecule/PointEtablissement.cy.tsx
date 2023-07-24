9ad19a06d68c4b50a714a7f7e9fb49bfadbada68
lucas <lucas.lvn97439@gmail.com>
2023-07-24 11:15:20 +0400

    create PointEtablissement stories, test & snapshot

---
+++ src/views/content/graphicage/component/molecule/PointEtablissement.cy.tsx
@@ -0,0 +1,33 @@
+import "leaflet/dist/leaflet.css";
+import { initialiseMap } from "../../../../../../.storybook/utils/mapWrapper";
+import PointEtablissement from "./PointEtablissement";
+
+describe("Checkbox component", () => {
+  const retryOptions = {
+    limit: 5, // max number of retries
+    delay: 500, // delay before next iteration, ms
+  };
+
+  it("Etablissement Point", () => {
+    cy.mount(() => (
+      <div id="map-container" style={{ width: "100%", height: "500px" }}>
+        <PointEtablissement
+          map={initialiseMap("map-container")}
+          lat={-20.9466588303741}
+          lon={55.5343806753509}
+          isBlinking={false}
+          isLast={false}
+          idPoint={1}
+          onIsLast={() => console.log("onIsLast")}
+          onClick={() => console.log("onClick")}
+          onMouseOut={() => console.log("onMouseOut")}
+          onDBLClick={() => console.log("onDBLClick")}
+          onMouseOver={() => console.log("onMouseOver")}
+        />
+      </div>
+    ));
+
+    cy.wait(1000);
+    cy.get("#map-container").compareSnapshot("schoolPoint", 0.01, retryOptions);
+  });
+});
---
+++ src/views/content/graphicage/component/molecule/PointEtablissement.stories.tsx
@@ -0,0 +1,49 @@
+import { LeafletMouseEvent } from "leaflet";
+import { Meta, StoryObj } from "storybook-solidjs";
+import { initialiseMap } from "../../../../../../.storybook/utils/mapWrapper";
+import PointEtablissementComponent, {
+  PointEtablissementProps,
+} from "./PointEtablissement";
+
+const meta = {
+  component: PointEtablissementComponent,
+  tags: ["autodocs"],
+} satisfies Meta<typeof PointEtablissementComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const PointEtablissement: Story = {
+  render: (props: PointEtablissementProps) => {
+    const div = document.getElementById("map-container");
+
+    if (div) {
+      div.remove();
+    }
+
+    return (
+      <div id="map-container" style={{ width: "100%", height: "500px" }}>
+        <PointEtablissementComponent
+          {...props}
+          map={initialiseMap("map-container")}
+        />
+      </div>
+    );
+  },
+  args: {
+    idPoint: 50,
+    lat: -20.9466588303741,
+    lon: 55.5343806753509,
+    isLast: false,
+    isBlinking: false,
+  },
+
+  argTypes: {
+    onIsLast: () => console.log("onIsLast"),
+    onClick: () => console.log("onClick"),
+    onDBLClick: (event: LeafletMouseEvent) =>
+      console.log("onDBLClick, event:", event),
+    onMouseOver: () => console.log("onMouseOver"),
+    onMouseOut: () => console.log("onMouseOut"),
+  },
+};
