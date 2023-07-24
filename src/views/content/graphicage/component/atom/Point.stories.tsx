92973301097fac0c99f05ae9a525f901d651b671
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-24 13:50:27 +0400

    review: point stories added

--- src/views/content/graphicage/component/atom/Point.stories.tsx
+++ src/views/content/graphicage/component/atom/Point.stories.tsx
@@ -1,7 +1,7 @@
 import "leaflet/dist/leaflet.css";
 
 import { Meta, StoryObj } from "storybook-solidjs";
-import { initMap } from "../../../../../../testing/utils/mapWrapper";
+import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
 
 import PointComponent, { PointProps } from "./Point";
 
@@ -13,16 +13,16 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Point: Story = {
+export const PointStories: Story = {
   render: (props: PointProps) => {
-    const div = document.getElementById("map-container");
+    const div = document.getElementById("map-container-1");
     if (div) {
       div.remove();
     }
 
     return (
-      <div id="map-container" style={{ width: "100%", height: "500px" }}>
-        <PointComponent {...props} map={initMap("map-container")} />
+      <div id="map-container-1" style={{ width: "100%", height: "500px" }}>
+        <PointComponent {...props} map={initialiseMap("map-container-1")} />
       </div>
     );
   },
@@ -35,6 +35,37 @@ export const Point: Story = {
     fillColor: "white",
     weight: 4,
     radius: 8,
+    isLast: false,
+    onIsLast: () => console.log("onIsLast"),
+    onClick: () => console.log("onClick"),
+    onDBLClick: () => console.log("onDBLClick"),
+    onMouseOut: () => console.log("onMouseOut"),
+    onMouseOver: () => console.log("onMouseOver"),
+  },
+};
+
+export const PointStories2: Story = {
+  render: (props: PointProps) => {
+    const div = document.getElementById("map-container-2");
+    if (div) {
+      div.remove();
+    }
+
+    return (
+      <div id="map-container-2" style={{ width: "100%", height: "500px" }}>
+        <PointComponent {...props} map={initialiseMap("map-container-2")} />
+      </div>
+    );
+  },
+  args: {
+    idPoint: 1,
+    lat: -20.9466588303741,
+    lon: 55.5343806753509,
+    isBlinking: false,
+    borderColor: "green",
+    fillColor: "white",
+    weight: 4,
+    radius: 12,
     isLast: false,
     onIsLast: () => console.log("onIsLast"),
     onClick: () => console.log("onClick"),
