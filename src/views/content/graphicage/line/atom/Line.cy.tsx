0f94edcacd61374878b6405f0a9bc5d4a6df6cfa
hugobaret <hugo.baret@epitech.eu>
2023-07-24 10:51:54 +0400

    remove tile layer on cypress test

--- src/views/content/graphicage/line/atom/Line.cy.tsx
+++ src/views/content/graphicage/line/atom/Line.cy.tsx
@@ -4,11 +4,6 @@ import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
 import Line from "./Line";
 
 describe("Line component", () => {
-  const retryOptions = {
-    limit: 5, // max number of retries
-    delay: 1000, // delay before next iteration, ms
-  };
-
   beforeEach(() => {
     cy.viewport(1000, 1000);
   });
@@ -23,15 +18,14 @@ describe("Line component", () => {
               L.latLng(-20.9466588303743, 55.54),
               L.latLng(-20.942, 55.535),
             ]}
-            leafletMap={initialiseMap("map-container")}
+            leafletMap={initialiseMap("map-container", false)}
             color={"orange"}
             opacity={1}
           />
         </div>
       );
     });
-    cy.wait(3000);
-    cy.get("#map-container").compareSnapshot("default", 0.01, retryOptions);
+    cy.get("#map-container").compareSnapshot("default", 0.01);
   });
 
   it("Change color", () => {
@@ -44,15 +38,14 @@ describe("Line component", () => {
               L.latLng(-20.9466588303743, 55.54),
               L.latLng(-20.942, 55.535),
             ]}
-            leafletMap={initialiseMap("map-container")}
+            leafletMap={initialiseMap("map-container", false)}
             color={"red"}
             opacity={1}
           />
         </div>
       );
     });
-    cy.wait(3000);
-    cy.get("#map-container").compareSnapshot("color", 0.01, retryOptions);
+    cy.get("#map-container").compareSnapshot("color", 0.01);
   });
 
   it("Change Opacity", () => {
@@ -65,15 +58,14 @@ describe("Line component", () => {
               L.latLng(-20.9466588303743, 55.54),
               L.latLng(-20.942, 55.535),
             ]}
-            leafletMap={initialiseMap("map-container")}
+            leafletMap={initialiseMap("map-container", false)}
             color={"orange"}
             opacity={0.5}
           />
         </div>
       );
     });
-    cy.wait(3000);
-    cy.get("#map-container").compareSnapshot("opacity", 0.01, retryOptions);
+    cy.get("#map-container").compareSnapshot("opacity", 0.01);
   });
 
   it("Change Latlngs", () => {
@@ -85,14 +77,13 @@ describe("Line component", () => {
               L.latLng(-20.9466588303741, 55.5343806753509),
               L.latLng(-20.9466588303743, 55.54),
             ]}
-            leafletMap={initialiseMap("map-container")}
+            leafletMap={initialiseMap("map-container", false)}
             color={"orange"}
             opacity={1}
           />
         </div>
       );
     });
-    cy.wait(3000);
-    cy.get("#map-container").compareSnapshot("latlng", 0.01, retryOptions);
+    cy.get("#map-container").compareSnapshot("latlng", 0.01);
   });
 });
--- testing/utils/mapWrapper.ts
+++ testing/utils/mapWrapper.ts
@@ -7,25 +7,26 @@ const mapTabs: {
   map: L.Map;
 }[] = [];
 
-const initMap = (idDiv: string) => {
+const initMap = (idDiv: string, withTiles: boolean) => {
   const newMap = L.map(idDiv).setView(
     [-20.9466588303741, 55.5343806753509],
     15
   );
-
-  // Cannot use the same instance of L.tileLayer for multiple maps
-  L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-    attribution:
-      // eslint-disable-next-line quotes
-      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(newMap);
+  if (withTiles) {
+    // Cannot use the same instance of L.tileLayer for multiple maps
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        // eslint-disable-next-line quotes
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(newMap);
+  }
 
   return newMap;
 };
 
-export const initialiseMap = (idDiv: string) => {
+export const initialiseMap = (idDiv: string, withTiles = true) => {
   const map = mapTabs.filter((m) => m.idDiv === idDiv)[0];
-  const newMap = initMap(idDiv);
+  const newMap = initMap(idDiv, withTiles);
 
   if (map == undefined) {
     mapTabs.push({ idDiv, map: newMap });
