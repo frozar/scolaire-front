b101bd0f618eb68dbd35efa6ba5ed5822a792e80
lucas <lucas.lvn97439@gmail.com>
2023-07-25 11:04:39 +0400

    add fetch Stop, EleveVersEtablissement & manage connexion error with notification

--- src/views/content/graphicage/point.service.ts
+++ src/views/content/graphicage/point.service.ts
@@ -1,10 +1,39 @@
+import { addNewUserInformation } from "../../../signaux";
+import { MessageLevelEnum, MessageTypeEnum } from "../../../type";
 import { asyncAuthenticateWrap } from "../../layout/authentication";
 
 const config = {
   host: import.meta.env.VITE_BACK_URL,
 };
 
+const connexionError = () => {
+  addNewUserInformation({
+    displayed: true,
+    level: MessageLevelEnum.error,
+    type: MessageTypeEnum.global,
+    content:
+      "Désoler une erreur est survenue lors du chargement des données veuillez essayer ultérieument",
+  });
+};
+
 export async function fetchSchool(mapId: number) {
+  const headers = await asyncAuthenticateWrap();
+
+  try {
+    const response = await fetch(
+      config.host + `/map/${mapId}/dashboard/etablissement`,
+      { headers }
+    );
+
+    const json = await response.json();
+    return json.content;
+  } catch (error) {
+    connexionError();
+    return false;
+  }
+}
+
+export async function fetchStop(mapId: number) {
   const headers = await asyncAuthenticateWrap();
 
   try {
@@ -14,10 +43,26 @@ export async function fetchSchool(mapId: number) {
     );
 
     const json = await response.json();
-    return json;
+    return json.content;
   } catch (error) {
-    // TODO push notification
-    console.log(error);
+    connexionError();
+    return false;
+  }
+}
+
+export async function fetchEleveVersEtablissement(mapId: number) {
+  const headers = await asyncAuthenticateWrap();
+
+  try {
+    const response = await fetch(
+      config.host + `/map/${mapId}/eleve_vers_etablissement`,
+      { headers }
+    );
+
+    const json = await response.json();
+    return json.content;
+  } catch (error) {
+    connexionError();
     return false;
   }
 }
