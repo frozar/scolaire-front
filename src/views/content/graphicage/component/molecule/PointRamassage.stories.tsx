e2f239a86a5148de38b9bf3459518b232a8df0f1
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-25 11:47:58 +0400

    cypress baseline image added

--- src/views/content/graphicage/component/molecule/PointRamassage.stories.tsx
+++ src/views/content/graphicage/component/molecule/PointRamassage.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const PointEtablissement: Story = {
+export const PointRamassageStories: Story = {
   render: (props: PointRamassageProps) => {
     const div = document.getElementById("map-container");
 
