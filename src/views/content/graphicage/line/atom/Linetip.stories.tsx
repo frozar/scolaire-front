86cb612cbd9bf7eed14829a4db35bdf5b14bea1f
hugobaret <hugo.baret@epitech.eu>
2023-07-25 16:48:58 +0400

    Atomize busLineTip and refactor mapWrapper

--- src/views/content/graphicage/line/atom/Linetip.stories.tsx
+++ src/views/content/graphicage/line/atom/Linetip.stories.tsx
@@ -1,31 +1,76 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { Meta, StoryObj } from "storybook-solidjs";
+import { Meta, StoryContext, StoryObj } from "storybook-solidjs";
 import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
-import LineTipComponent from "./Line";
-import LineTip from "./LineTip";
+import LineTipComponent from "./LineTip";
+
+function getDivFullId(options: StoryContext) {
+  const name = options.name.replaceAll(" ", "-").toLowerCase();
+  const mode = options.viewMode.toLowerCase();
+
+  return `${mode}-${name}-map-container`;
+}
 
 const meta = {
   component: LineTipComponent,
   tags: ["autodocs"],
+  argTypes: {
+    latlng: {
+      description: "The latlng of the line tip",
+      control: {
+        type: "object",
+      },
+    },
+    color: {
+      description: "The color of the line tip",
+      control: {
+        type: "color",
+      },
+    },
+    opacity: {
+      description: "The opacity of the line tip",
+      control: {
+        type: "number",
+      },
+    },
+    leafletMap: {
+      description: "The leaflet map",
+      control: {
+        type: "object",
+      },
+    },
+  },
+  decorators: [
+    (Story, options) => {
+      const fullId = getDivFullId(options);
+
+      return (
+        <div id={fullId} style={{ width: "100%", height: "500px" }}>
+          <Story {...options.args} />
+        </div>
+      );
+    },
+  ],
 } satisfies Meta<typeof LineTipComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const LineTipStory: Story = {
-  render: (props: { latlng: L.LatLng; color: string; opacity: number }) => {
-    const div = document.getElementById("map-container");
-    if (div) {
-      div.remove();
-    }
+  render: (
+    props: {
+      latlng: L.LatLng;
+      color: string;
+      opacity: number;
+      leafletMap: L.Map;
+    },
+    options
+  ) => {
+    const fullId = getDivFullId(options);
 
-    return (
-      <div id="map-container" style={{ width: "100%", height: "500px" }}>
-        <LineTip {...props} leafletMap={initialiseMap("map-container")} />
-      </div>
-    );
+    return <LineTipComponent {...props} leafletMap={initialiseMap(fullId)} />;
   },
+
   args: {
     latlng: L.latLng(-20.9466588303741, 55.5343806753509),
     color: "orange",
--- testing/utils/mapWrapper.ts
+++ testing/utils/mapWrapper.ts
@@ -1,7 +1,6 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// let map: L.Map;
 const mapTabs: {
   idDiv: string;
   map: L.Map;
@@ -12,6 +11,7 @@ const initMap = (idDiv: string, withTiles: boolean) => {
     [-20.9466588303741, 55.5343806753509],
     15
   );
+
   if (withTiles) {
     // Cannot use the same instance of L.tileLayer for multiple maps
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
@@ -26,20 +26,26 @@ const initMap = (idDiv: string, withTiles: boolean) => {
 
 export const initialiseMap = (idDiv: string, withTiles = true) => {
   const map = mapTabs.filter((m) => m.idDiv === idDiv)[0];
+
   if (map == undefined) {
+    //If the map is not already initialised create a new one
     const newMap = initMap(idDiv, withTiles);
     mapTabs.push({ idDiv, map: newMap });
-    console.log("newMap1", newMap);
     return newMap;
   }
+
+  const div = document.getElementById(idDiv);
+  const container = map.map.getContainer();
+
+  //If the map is already initialised and we are in the same page return it
+  if (container === div) {
+    return map.map;
+  }
+
+  //If the map is already initialised but we are in another page we need to move the map to the new div
+  const parent = div?.parentElement;
+  div?.remove();
+  parent?.appendChild(container);
+
   return map.map;
-
-  // map.map.off();
-  // map.map.remove();
-  // const newMap = initMap(idDiv, withTiles);
-
-  // console.log("newMap", newMap);
-  // map.map = newMap;
-
-  // return map.map;
 };
