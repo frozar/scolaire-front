4b02d3d939977ac91181b42aa1de90fe2bff0a69
lucas <lucas.lvn97439@gmail.com>
2023-07-25 13:32:39 +0400

    manage status code

--- src/views/content/graphicage/point.service.ts
+++ src/views/content/graphicage/point.service.ts
@@ -16,53 +16,77 @@ const connexionError = () => {
   });
 };
 
+const manageStatusCode = async (response: Response) => {
+  if (response.status !== 200) {
+    const json = await response.json();
+    const message =
+      json.detail ??
+      "Désoler une erreur est survenue lors du chargement des données veuillez essayer ultérieument";
+
+    addNewUserInformation({
+      displayed: true,
+      level: MessageLevelEnum.error,
+      type: MessageTypeEnum.global,
+      content: message,
+    });
+
+    return false;
+  }
+
+  return true;
+};
+
 export async function fetchSchool(mapId: number) {
   const headers = await asyncAuthenticateWrap();
-
+  let response: Response;
   try {
-    const response = await fetch(
+    response = await fetch(
       config.host + `/map/${mapId}/dashboard/etablissement`,
       { headers }
     );
-
-    const json = await response.json();
-    return json.content;
   } catch (error) {
     connexionError();
     return false;
   }
+
+  if (!(await manageStatusCode(response))) return;
+  const json = await response.json();
+  return json.content;
 }
 
 export async function fetchStop(mapId: number) {
   const headers = await asyncAuthenticateWrap();
+  let response: Response;
 
   try {
-    const response = await fetch(
-      config.host + `/map/${mapId}/dashboard/ramassage`,
-      { headers }
-    );
-
-    const json = await response.json();
-    return json.content;
+    response = await fetch(config.host + `/map/${mapId}/dashboard/ramassage`, {
+      headers,
+    });
   } catch (error) {
     connexionError();
     return false;
   }
+
+  if (!(await manageStatusCode(response))) return;
+  const json = await response.json();
+  return json.content;
 }
 
 export async function fetchEleveVersEtablissement(mapId: number) {
   const headers = await asyncAuthenticateWrap();
+  let response: Response;
 
   try {
-    const response = await fetch(
+    response = await fetch(
       config.host + `/map/${mapId}/eleve_vers_etablissement`,
       { headers }
     );
-
-    const json = await response.json();
-    return json.content;
   } catch (error) {
     connexionError();
     return false;
   }
+
+  if (!manageStatusCode(response)) return;
+  const json = await response.json();
+  return json.content;
 }
