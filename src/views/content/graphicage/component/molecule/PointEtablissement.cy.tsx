cffee5ce68640e8e4633e8f7f594d2578b143a88
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-25 10:20:37 +0400

    cypress tests updated

--- src/views/content/graphicage/component/molecule/PointEtablissement.cy.tsx
+++ src/views/content/graphicage/component/molecule/PointEtablissement.cy.tsx
@@ -3,16 +3,11 @@ import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
 import PointEtablissement from "./PointEtablissement";
 
 describe("Checkbox component", () => {
-  const retryOptions = {
-    limit: 5, // max number of retries
-    delay: 500, // delay before next iteration, ms
-  };
-
   it("Etablissement Point snapshot", () => {
     cy.mount(() => (
       <div id="map-container" style={{ width: "100%", height: "500px" }}>
         <PointEtablissement
-          map={initialiseMap("map-container")}
+          map={initialiseMap("map-container", false)}
           lat={-20.9466588303741}
           lon={55.5343806753509}
           isBlinking={false}
@@ -27,7 +22,6 @@ describe("Checkbox component", () => {
       </div>
     ));
 
-    cy.wait(1000);
-    cy.get("#map-container").compareSnapshot("schoolPoint", 0.01, retryOptions);
+    cy.get("#map-container").compareSnapshot("schoolPoint", 0.01);
   });
 });
