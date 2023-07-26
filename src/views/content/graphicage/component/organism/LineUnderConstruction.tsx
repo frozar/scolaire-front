9876dcc1efa86bbeafd47edb9f605b1e7668f593
hugobaret <hugo.baret@epitech.eu>
2023-07-26 11:52:44 +0400

    Create LineUnderConstruction organism

---
+++ src/views/content/graphicage/component/organism/LineUnderConstruction.tsx
@@ -0,0 +1,32 @@
+import { Show } from "solid-js";
+import { useStateAction } from "../../../../../StateAction";
+import { PointIdentityType } from "../../../../../type";
+import { getLatLngs } from "../../line/busLinesUtils";
+import Line from "../atom/Line";
+import LineTip from "../molecule/LineTip";
+const [, { getLineUnderConstruction }] = useStateAction();
+interface LineUnderConstructionProps {
+  leafletMap: L.Map;
+  stops: PointIdentityType[];
+}
+export default function (props: LineUnderConstructionProps) {
+  const color = "blue";
+  const opacity = 1;
+
+  return (
+    <Show when={getLineUnderConstruction().stops.length > 0}>
+      <Line
+        latlngs={getLatLngs(props.stops)}
+        leafletMap={props.leafletMap}
+        color={color}
+        opacity={opacity}
+      />
+      <LineTip
+        latlng={getLatLngs(props.stops).at(-1) as L.LatLng}
+        leafletMap={props.leafletMap}
+        color={color}
+        opacity={opacity}
+      />
+    </Show>
+  );
+}
