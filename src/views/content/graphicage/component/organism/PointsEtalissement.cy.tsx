b29c03c758de95eafe2e9f5cd0126fa353415055
lucas <lucas.lvn97439@gmail.com>
2023-07-26 14:35:23 +0400

    add cypress test & snapshot for PointsEtablissement

---
+++ src/views/content/graphicage/component/organism/PointsEtalissement.cy.tsx
@@ -0,0 +1,24 @@
+import "leaflet/dist/leaflet.css";
+import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
+import PointEtablissement from "./PointsEtalissement";
+
+describe("PointsEtablissement component", () => {
+  beforeEach(() => {
+    cy.intercept("http://localhost:8000/map/2/dashboard/etablissement", {
+      fixture: "getEtablissement.json",
+    }).as("getData");
+  });
+
+  it("EtablissementPoints snapshot", () => {
+    cy.mount(() => (
+      <div id="map-container" style={{ width: "100%", height: "500px" }}>
+        <PointEtablissement
+          mapID={2}
+          map={initialiseMap("map-container", false)}
+        />
+      </div>
+    )).as("component");
+
+    cy.get("#map-container").compareSnapshot("EtablissementPoints", 0.01);
+  });
+});
