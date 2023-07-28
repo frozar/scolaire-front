4b58764d97f31dfbd16614d2420b26dd284a8200
lucas <lucas.lvn97439@gmail.com>
2023-07-28 13:29:26 +0400

    clean

--- src/views/content/graphicage/component/organism/PointsEtalissement.stories.tsx
+++ src/views/content/graphicage/component/organism/PointsEtalissement.stories.tsx
@@ -18,7 +18,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const PointeEtablissement: Story = {
+export const PointEtablissement: Story = {
   render: (props: null, options) => {
     const fullId = getDivFullId(options);
 
