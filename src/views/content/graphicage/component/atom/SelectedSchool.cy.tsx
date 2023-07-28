8aa236d6071c4c1e3eeefd8164285ac2fbc38dc5
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-28 16:48:26 +0400

    review

--- src/views/content/graphicage/component/atom/SelectedSchool.cy.tsx
+++ src/views/content/graphicage/component/atom/SelectedSchool.cy.tsx
@@ -2,7 +2,6 @@ import { PointInformation } from "./Point";
 import SelectedSchool from "./SelectedSchool";
 
 describe("SelectedSchool atom", () => {
-  //TODO : add elements to schoolSelected array
   const schoolSelected: PointInformation[] = [
     {
       id: 0,
@@ -19,8 +18,8 @@ describe("SelectedSchool atom", () => {
     cy.get(".selected-school").compareSnapshot("no school", 0.01);
   });
 
-  it("SelectedSchool: no school", () => {
+  it("SelectedSchool", () => {
     cy.mount(() => <SelectedSchool schoolSelected={schoolSelected} />);
-    cy.get(".selected-school").compareSnapshot("no school", 0.01);
+    cy.get(".selected-school").compareSnapshot("one school", 0.01);
   });
 });
