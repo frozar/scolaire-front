e69a0de16583a449a1358c5d09160d24c82bc609
lucas <lucas.lvn97439@gmail.com>
2023-07-28 13:44:25 +0400

    clean

--- src/views/content/graphicage/component/organism/PointsRamassage.cy.tsx
+++ src/views/content/graphicage/component/organism/PointsRamassage.cy.tsx
@@ -1,24 +1,24 @@
 import "leaflet/dist/leaflet.css";
 import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
-import RamassagePoints from "./PointsRamassage";
+import PointsRamassage from "./PointsRamassage";
 
-describe("RamassagePoints component", () => {
+describe("PointsRamassage component", () => {
   beforeEach(() => {
     cy.intercept("http://localhost:8000/map/2/dashboard/ramassage", {
       fixture: "getRamassages.json",
     }).as("getData");
   });
 
-  it("RamassagePoints snapshot", () => {
+  it("PointsRamassage snapshot", () => {
     cy.mount(() => (
       <div id="map-container" style={{ width: "100%", height: "500px" }}>
-        <RamassagePoints
+        <PointsRamassage
           mapId={2}
           map={initialiseMap("map-container", false)}
         />
       </div>
     ));
 
-    cy.get("#map-container").compareSnapshot("RamassagePoints", 0.01);
+    cy.get("#map-container").compareSnapshot("PointsRamassage", 0.01);
   });
 });
