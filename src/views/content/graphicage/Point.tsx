a8de9f6c334c7db8cba91f5d38fc7f83c180a830
Quentin PAYET <quentinpayet1203@gmail.com>
2023-08-01 16:04:50 +0400

    pointRamassageAndEtablissement export functions no longer used

--- src/views/content/graphicage/Point.tsx
+++ src/views/content/graphicage/Point.tsx
@@ -1,250 +1,230 @@
-import L, { LeafletMouseEvent } from "leaflet";
-import { createEffect, on, onCleanup, onMount } from "solid-js";
-import {
-  NatureEnum,
-  PointEtablissementType,
-  PointIdentityType,
-  PointRamassageType,
-} from "../../../type";
+// import L, { LeafletMouseEvent } from "leaflet";
+// import { createEffect, on, onCleanup, onMount } from "solid-js";
+// import {
+//   NatureEnum,
+//   PointEtablissementType,
+//   PointIdentityType,
+//   PointRamassageType,
+// } from "../../../type";
 
-import { useStateAction } from "../../../StateAction";
-import {
-  getLeafletMap,
-  points,
-  setIsEtablissementReady,
-  setIsRamassageReady,
-} from "../../../signaux";
-import { renderAnimation } from "./animation";
-import { deselectAllBusLines } from "./line/busLinesUtils";
+// import { useStateAction } from "../../../StateAction";
+// import {
+//   getLeafletMap,
+//   points,
+//   setIsEtablissementReady,
+//   setIsRamassageReady,
+// } from "../../../signaux";
+// import { renderAnimation } from "./animation";
+// import { deselectAllBusLines } from "./line/busLinesUtils";
 
-// export const linkMap = new Map<number, L.CircleMarker>();
-const linkMap = new Map<number, L.CircleMarker>();
+// // export const linkMap = new Map<number, L.CircleMarker>();
+// const linkMap = new Map<number, L.CircleMarker>();
 
-const [
-  ,
-  {
-    addPointToLineUnderConstruction,
-    getLineUnderConstruction,
-    isInAddLineMode,
-    setLineUnderConstruction,
-  },
-] = useStateAction();
+// const [
+//   ,
+//   {
+//     addPointToLineUnderConstruction,
+//     getLineUnderConstruction,
+//     isInAddLineMode,
+//   },
+// ] = useStateAction();
 
-function selectPointById(targerIdPoint: number) {
-  points().map((point) => point.setSelected(targerIdPoint == point.idPoint));
-}
-
-// export function deselectAllPoints() {
-//   points().map((point) => point.setSelected(false));
+// function selectPointById(targerIdPoint: number) {
+//   points().map((point) => point.setSelected(targerIdPoint == point.idPoint));
 // }
 
-const minSizeValue = 5;
-const maxSizeValue = 10;
-const range = maxSizeValue - minSizeValue;
-console.log("ancien point");
+// // export function deselectAllPoints() {
+// //   points().map((point) => point.setSelected(false));
+// // }
 
-export default function (props: {
-  point: PointRamassageType | PointEtablissementType;
-  isLast: boolean;
-  nature: NatureEnum;
-  minQuantity: number;
-  maxQuantity: number;
-}) {
-  const point = () => props.point;
-  const isLast = () => props.isLast;
-  const nature = () => props.nature;
-  const minQuantity = () => props.minQuantity;
-  const maxQuantity = () => props.maxQuantity;
+// const minSizeValue = 5;
+// const maxSizeValue = 10;
+// const range = maxSizeValue - minSizeValue;
+// console.log("ancien point");
 
-  let circle: L.CircleMarker;
+// export default function (props: {
+//   point: PointRamassageType | PointEtablissementType;
+//   isLast: boolean;
+//   nature: NatureEnum;
+//   minQuantity: number;
+//   maxQuantity: number;
+// }) {
+//   const point = () => props.point;
+//   const isLast = () => props.isLast;
+//   const nature = () => props.nature;
+//   const minQuantity = () => props.minQuantity;
+//   const maxQuantity = () => props.maxQuantity;
 
-  function buildCircle(point: PointEtablissementType): L.CircleMarker {
-    // console.log("point", point);
-    // const location = point.location;
-    // const lonlat = location.split("(")[1].split(")")[0];
-    // const lon = Number(lonlat.split(" ")[0]);
-    // const lat = Number(lonlat.split(" ")[1]);
-    const lon = point.lon;
-    const lat = point.lat;
+//   let circle: L.CircleMarker;
 
-    // console.log("location", location);
-    // console.log("lonlat", lonlat);
-    // console.log("lon", lon);
-    // console.log("lat", lat);
+//   function buildCircle(point: PointEtablissementType): L.CircleMarker {
+//     // console.log("point", point);
+//     // const location = point.location;
+//     // const lonlat = location.split("(")[1].split(")")[0];
+//     // const lon = Number(lonlat.split(" ")[0]);
+//     // const lat = Number(lonlat.split(" ")[1]);
+//     const lon = point.lon;
+//     const lat = point.lat;
 
-    // console.log("minQuantity", minQuantity());
-    // console.log("maxQuantity", maxQuantity());
+//     // console.log("location", location);
+//     // console.log("lonlat", lonlat);
+//     // console.log("lon", lon);
+//     // console.log("lat", lat);
 
-    // console.log("point.quantity", point.quantity);
-    // console.log("minQuantity", minQuantity());
-    // console.log("maxQuantity", maxQuantity());
+//     // console.log("minQuantity", minQuantity());
+//     // console.log("maxQuantity", maxQuantity());
 
-    const coef =
-      minQuantity() == maxQuantity()
-        ? 0
-        : (point.quantity - minQuantity()) / (maxQuantity() - minQuantity());
-    // console.log("coef", coef);
+//     // console.log("point.quantity", point.quantity);
+//     // console.log("minQuantity", minQuantity());
+//     // console.log("maxQuantity", maxQuantity());
 
-    const radiusValue = coef * range + minSizeValue;
-    // console.log("radiusValue", radiusValue);
+//     const coef =
+//       minQuantity() == maxQuantity()
+//         ? 0
+//         : (point.quantity - minQuantity()) / (maxQuantity() - minQuantity());
+//     // console.log("coef", coef);
 
-    const { nature } = point;
-    // console.log("nature", nature);
+//     const radiusValue = coef * range + minSizeValue;
+//     // console.log("radiusValue", radiusValue);
 
-    const [color, fillColor, radius, weight] =
-      nature === NatureEnum.ramassage
-        ? ["red", "white", radiusValue, 2]
-        : nature === NatureEnum.etablissement
-        ? ["green", "white", 12, 4]
-        : ["white", "#000", 18, 4];
-    // console.log("color", color);
-    // console.log("fillColor", fillColor);
-    // console.log("radius", radius);
-    // console.log("weight", weight);
+//     const { nature } = point;
+//     // console.log("nature", nature);
 
-    // console.log("lon", lon, "typeof lon", typeof lon);
-    // console.log("lat", lat);
-    return L.circleMarker([lat, lon], {
-      color,
-      fillColor,
-      radius,
-      fillOpacity: 1,
-      weight,
-      pane: "markerPane",
-    })
-      .on("click", () => {
-        // Select the current element to display information
+//     const [color, fillColor, radius, weight] =
+//       nature === NatureEnum.ramassage
+//         ? ["red", "white", radiusValue, 2]
+//         : nature === NatureEnum.etablissement
+//         ? ["green", "white", 12, 4]
+//         : ["white", "#000", 18, 4];
+//     // console.log("color", color);
+//     // console.log("fillColor", fillColor);
+//     // console.log("radius", radius);
+//     // console.log("weight", weight);
 
-        if (!isInAddLineMode()) {
-          deselectAllBusLines();
-          selectPointById(point.idPoint);
-          return;
-        }
+//     // console.log("lon", lon, "typeof lon", typeof lon);
+//     // console.log("lat", lat);
+//     return L.circleMarker([lat, lon], {
+//       color,
+//       fillColor,
+//       radius,
+//       fillOpacity: 1,
+//       weight,
+//       pane: "markerPane",
+//     })
+//       .on("click", () => {
+//         // Select the current element to display information
 
-        //TODO : move to PointEtablissement in click handler when used
+//         if (!isInAddLineMode()) {
+//           deselectAllBusLines();
+//           selectPointById(point.idPoint);
+//           return;
+//         }
 
-        const etablissementSelected =
-          getLineUnderConstruction().etablissementSelected;
+//         const pointIdentity: PointIdentityType = {
+//           id: point.id,
+//           idPoint: point.idPoint,
+//           nature: point.nature,
+//         };
 
-        if (!getLineUnderConstruction().confirmSelection) {
-          if (etablissementSelected?.find((p) => p.idPoint === point.idPoint)) {
-            return;
-          }
-          setLineUnderConstruction({
-            ...getLineUnderConstruction(),
-            etablissementSelected: !etablissementSelected
-              ? [point]
-              : etablissementSelected.concat(point),
-          });
+//         addPointToLineUnderConstruction(pointIdentity);
 
-          return;
-        }
+//         if (!(1 < getLineUnderConstruction().stops.length)) {
+//           return;
+//         }
 
-        const pointIdentity: PointIdentityType = {
-          id: point.id,
-          idPoint: point.idPoint,
-          nature: point.nature,
-        };
+//         // Highlight point ramassage
+//         for (const associatedPoint of point.associatedPoints()) {
+//           let element;
+//           if ((element = linkMap.get(associatedPoint.idPoint)?.getElement())) {
+//             renderAnimation(element);
+//           }
+//         }
+//       })
+//       .on("dblclick", (event: LeafletMouseEvent) => {
+//         L.DomEvent.stopPropagation(event);
+//       })
+//       .on("mouseover", () => {
+//         for (const associatedPoint of point.associatedPoints()) {
+//           const element = linkMap.get(associatedPoint.idPoint)?.getElement();
+//           const { nature } = associatedPoint;
+//           const className =
+//             nature === NatureEnum.ramassage
+//               ? "circle-animation-ramassage"
+//               : "circle-animation-etablissement";
+//           if (element) {
+//             element.classList.add(className);
+//           }
+//         }
+//       })
+//       .on("mouseout", () => {
+//         for (const associatedPoint of point.associatedPoints()) {
+//           const element = linkMap.get(associatedPoint.idPoint)?.getElement();
+//           const { nature } = associatedPoint;
+//           const className =
+//             nature === NatureEnum.ramassage
+//               ? "circle-animation-ramassage"
+//               : "circle-animation-etablissement";
 
-        addPointToLineUnderConstruction(pointIdentity);
+//           if (element) {
+//             element.classList.remove(className);
+//           }
+//         }
+//       });
+//   }
 
-        if (!(1 < getLineUnderConstruction().stops.length)) {
-          return;
-        }
+//   // If a line is under construction, show a pencil when the mouse is over a circle
+//   createEffect(
+//     on(isInAddLineMode, (isInAddLineMode) => {
+//       if (circle) {
+//         const element = circle.getElement() as SVGElement;
+//         if (element) {
+//           if (isInAddLineMode) {
+//             if (String(element.style) !== "cursor: url('/pencil.png'), auto;") {
+//               // @ts-expect-error: 'style' field should not be assigned
+//               element.style = "cursor: url('/pencil.png'), auto;";
+//             }
+//           } else {
+//             if (String(element.style) !== "") {
+//               // @ts-expect-error: 'style' field should not be assigned
+//               element.style = "";
+//             }
+//           }
+//         }
+//       }
+//     })
+//   );
 
-        // Highlight point ramassage
-        for (const associatedPoint of point.associatedPoints()) {
-          let element;
-          if ((element = linkMap.get(associatedPoint.idPoint)?.getElement())) {
-            renderAnimation(element);
-          }
-        }
-      })
-      .on("dblclick", (event: LeafletMouseEvent) => {
-        L.DomEvent.stopPropagation(event);
-      })
-      .on("mouseover", () => {
-        for (const associatedPoint of point.associatedPoints()) {
-          const element = linkMap.get(associatedPoint.idPoint)?.getElement();
-          const { nature } = associatedPoint;
-          const className =
-            nature === NatureEnum.ramassage
-              ? "circle-animation-ramassage"
-              : "circle-animation-etablissement";
-          if (element) {
-            element.classList.add(className);
-          }
-        }
-      })
-      .on("mouseout", () => {
-        for (const associatedPoint of point.associatedPoints()) {
-          const element = linkMap.get(associatedPoint.idPoint)?.getElement();
-          const { nature } = associatedPoint;
-          const className =
-            nature === NatureEnum.ramassage
-              ? "circle-animation-ramassage"
-              : "circle-animation-etablissement";
+//   onMount(() => {
+//     const leafletMap = getLeafletMap();
+//     if (!leafletMap) {
+//       return;
+//     }
 
-          if (element) {
-            element.classList.remove(className);
-          }
-        }
-      });
-  }
+//     // console.log("point", point());
 
-  // If a line is under construction, show a pencil when the mouse is over a circle
-  createEffect(
-    on(isInAddLineMode, (isInAddLineMode) => {
-      if (circle) {
-        const element = circle.getElement() as SVGElement;
-        if (element) {
-          if (isInAddLineMode) {
-            if (String(element.style) !== "cursor: url('/pencil.png'), auto;") {
-              // @ts-expect-error: 'style' field should not be assigned
-              element.style = "cursor: url('/pencil.png'), auto;";
-            }
-          } else {
-            if (String(element.style) !== "") {
-              // @ts-expect-error: 'style' field should not be assigned
-              element.style = "";
-            }
-          }
-        }
-      }
-    })
-  );
+//     circle = buildCircle(point());
+//     circle.addTo(leafletMap);
 
-  onMount(() => {
-    const leafletMap = getLeafletMap();
-    if (!leafletMap) {
-      return;
-    }
+//     const element = circle.getElement();
+//     if (element) {
+//       linkMap.set(point().idPoint, circle);
+//     }
 
-    // console.log("point", point());
+//     // Fetch associated points (ramassage or etablissement) and
+//     // store them in the associatedPoints() signal (used is the on'click' event)
+//     if (isLast()) {
+//       if (nature() === NatureEnum.ramassage) {
+//         setIsRamassageReady(true);
+//       } else {
+//         setIsEtablissementReady(true);
+//       }
+//     }
+//   });
 
-    circle = buildCircle(point());
-    circle.addTo(leafletMap);
+//   onCleanup(() => {
+//     linkMap.delete(point().idPoint);
+//     circle.remove();
+//   });
 
-    const element = circle.getElement();
-    if (element) {
-      linkMap.set(point().idPoint, circle);
-    }
-
-    // Fetch associated points (ramassage or etablissement) and
-    // store them in the associatedPoints() signal (used is the on'click' event)
-    if (isLast()) {
-      if (nature() === NatureEnum.ramassage) {
-        setIsRamassageReady(true);
-      } else {
-        setIsEtablissementReady(true);
-      }
-    }
-  });
-
-  onCleanup(() => {
-    linkMap.delete(point().idPoint);
-    circle.remove();
-  });
-
-  return <></>;
-}
+//   return <></>;
+// }
