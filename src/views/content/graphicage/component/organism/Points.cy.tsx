9f049a57c04d3df435393c4586cfeb2f49c37b19
Quentin PAYET <quentinpayet1203@gmail.com>
2023-08-01 10:33:28 +0400

    points.cy.tsx cleaning

--- src/views/content/graphicage/component/organism/Points.cy.tsx
+++ src/views/content/graphicage/component/organism/Points.cy.tsx
@@ -4,18 +4,19 @@ import Points from "./Points";
 
 describe("PointsEtablissement component", () => {
   beforeEach(() => {
-    // Change fixtures to real corresponding data ?
+    // TODO: Fix fixtures, data must be coherent with eleveVersEtablissement.json
+    // to test blinking
     cy.intercept("http://localhost:8000/map/2/dashboard/etablissement", {
       fixture: "getEtablissements.json",
-    }).as("getDataEtablissement");
+    });
 
     cy.intercept("http://localhost:8000/map/2/dashboard/ramassage", {
       fixture: "getRamassages.json",
-    }).as("getDataRamassage");
+    });
 
     cy.intercept("http://localhost:8000/map/2/eleve_vers_etablissement", {
       fixture: "eleveVersEtablissement.json",
-    }).as("eleveVersEtablissementData");
+    });
   });
 
   it("Points snapshot", () => {
@@ -23,9 +24,9 @@ describe("PointsEtablissement component", () => {
       <div id="map-container" style={{ width: "100%", height: "500px" }}>
         <Points mapId={2} map={initialiseMap("map-container", false)} />
       </div>
-    )).as("component"); // ?
+    ));
 
     cy.get("#map-container").compareSnapshot("Points", 0.01);
-    // test isBlinking
+    // TODO: Add blinking test
   });
 });
