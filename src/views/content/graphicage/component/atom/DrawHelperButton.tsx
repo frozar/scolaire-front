d10adeda42ce3654ac33d2d1fbb7bde57dd3fc50
AdChiv <adrien@entropik.digital>
2023-08-03 10:20:19 +0400

    Graphicage Draw Helper : refacto the draw helper button

---
+++ src/views/content/graphicage/component/atom/DrawHelperButton.tsx
@@ -0,0 +1,38 @@
+import { FaSolidWandMagicSparkles } from "solid-icons/fa";
+import { useStateAction } from "../../../../../StateAction";
+import { PointRamassageType } from "../../../../../type";
+import { PointInformation } from "./Point";
+
+import "./DrawHelperButton.css";
+
+interface DrawHelperButtonProps {
+  //TODO Must impose PointRamassageType[] type
+  schools: PointInformation[] | undefined;
+}
+
+const [, { getLineUnderConstruction }] = useStateAction();
+
+export function DrawHelperButton(props: DrawHelperButtonProps) {
+  function onclick() {
+    console.log("Etablissement selectionnés :");
+    for (const school of props.schools as PointRamassageType[]) {
+      console.log(school);
+    }
+
+    // console.log(getLineUnderConstruction().stops);
+
+    console.log("ramassages :");
+    for (const stopName of getLineUnderConstruction().stops) {
+      console.log(stopName);
+    }
+  }
+
+  return (
+    <div class="graphicage-draw-helper-button">
+      <p>Création d'une ligne</p>
+      <button onClick={onclick}>
+        <FaSolidWandMagicSparkles />
+      </button>
+    </div>
+  );
+}
