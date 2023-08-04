2e39a1121745332084f67cab295405953bdf1e5c
richard <richard.fontaine@flaxib.re>
2023-08-04 11:18:49 +0400

    Add files for timeline refactoring

---
+++ src/views/content/graphicage/informationBoard/TimelineAddMode.tsx
@@ -0,0 +1,37 @@
+import { For } from "solid-js";
+import { useStateAction } from "../../../../StateAction";
+import { LineUnderConstructionType } from "../../../../type";
+import { TimelineAddPointButton } from "../component/atom/TimelineAddPointButton";
+import { mapIdentityToResourceType } from "../line/busLinesUtils";
+import TimelineItemAddMode from "./TimelineItemAddMode";
+const [, { isInAddLineMode }] = useStateAction();
+
+export default function (props: {
+  line: () => LineUnderConstructionType;
+  setLine: (line: LineUnderConstructionType) => void;
+}) {
+  return (
+    <div class="timeline">
+      <div
+        class="v-timeline v-timeline--align-start v-timeline--justify-auto v-timeline--side-end v-timeline--vertical"
+        style={{ "--v-timeline-line-thickness": "2px" }}
+      >
+        <For each={mapIdentityToResourceType(props.line()?.stops)}>
+          {(stop, i) => (
+            <>
+              <TimelineAddPointButton {...props} />
+
+              <TimelineItemAddMode
+                pointsResource={stop}
+                indice={i()}
+                setter={props.setLine}
+                getter={props.line}
+                isInAddLineMode={isInAddLineMode()}
+              />
+            </>
+          )}
+        </For>
+      </div>
+    </div>
+  );
+}
---
+++ src/views/content/graphicage/informationBoard/TimelineItemAddMode.tsx
@@ -0,0 +1,38 @@
+import { JSX } from "solid-js";
+import { AbstractLineType, LineUnderConstructionType } from "../../../../type";
+import { PointInterface } from "../component/atom/Point";
+import { TimelineRemovePointButton } from "../component/atom/TimelineRemovePointButton";
+export type TimelineItemType = {
+  pointsResource: PointInterface;
+  indice: number;
+  getter: () => AbstractLineType;
+  setter: (line: LineUnderConstructionType) => void;
+  isInAddLineMode: boolean;
+  removeButton?: JSX.Element;
+};
+
+export default function (props: TimelineItemType) {
+  const removeButton = <TimelineRemovePointButton {...props} />;
+  return (
+    <div class="v-timeline-item">
+      <div class="v-timeline-item__body">
+        <div class="d-flex">
+          <strong>{props.pointsResource.name}</strong>
+        </div>
+      </div>
+
+      <div class="v-timeline-divider">
+        <div class="v-timeline-divider__before" />
+
+        <div class="v-timeline-divider__dot v-timeline-divider__dot--size-small">
+          <div class="v-timeline-divider__inner-dot bg-pink">
+            <i class="" aria-hidden="true" />
+            {removeButton}
+          </div>
+        </div>
+
+        <div class="v-timeline-divider__after" />
+      </div>
+    </div>
+  );
+}
