b09c9b1caaf13c2d0af661e04a2338e9acd4706d
Quentin PAYET <quentinpayet1203@gmail.com>
2023-08-04 12:09:55 +0400

    wip: timelineItemReadMode

--- src/views/content/graphicage/component/atom/TimelineItemReadMode.tsx
+++ src/views/content/graphicage/component/atom/TimelineItemReadMode.tsx
@@ -1,16 +1,28 @@
-import { AbstractLineType } from "../../../../type";
-import { PointInterface } from "../component/atom/Point";
+import { AbstractLineType, NatureEnum } from "../../../../../type";
+import { PointInterface } from "./Point";
+import { TimelineItemAddType } from "./TimelineItemAddMode";
+
 export type TimelineItemReadType = {
   pointsResource: PointInterface;
   quantity: number;
   getter: () => AbstractLineType | undefined;
 };
 
-export default function (props: TimelineItemReadType) {
+export default function (props: TimelineItemAddType) {
+  const timelineCircleClass =
+    "v-timeline-divider__dot v-timeline-divider__dot--size-small";
+  console.log("props.pointsResource", props.pointsResource);
+
   return (
     <div class="v-timeline-item">
       <div class="v-timeline-item__body">
         <div class="d-flex">
+          <div class="me-4">
+            {props.pointsResource.nature == NatureEnum.ramassage
+              ? // "+ " + props.pointsResource.quantity
+                "+ " + props.quantity
+              : "- " + props.pointsResource.quantity}
+          </div>
           <strong>{props.pointsResource.name}</strong>
         </div>
       </div>
@@ -18,8 +30,15 @@ export default function (props: TimelineItemReadType) {
       <div class="v-timeline-divider">
         <div class="v-timeline-divider__before" />
 
-        <div class="v-timeline-divider__dot v-timeline-divider__dot--size-small">
-          <div class="v-timeline-divider__inner-dot bg-pink">
+        {/* <div class="v-timeline-divider__dot v-timeline-divider__dot--size-small"> */}
+        <div
+          class={
+            props.pointsResource.nature == NatureEnum.ramassage
+              ? timelineCircleClass + " !bg-red-500"
+              : timelineCircleClass + " !bg-green-base"
+          }
+        >
+          <div class="v-timeline-divider__inner-dot !bg-white">
             <i class="" aria-hidden="true" />
           </div>
         </div>
