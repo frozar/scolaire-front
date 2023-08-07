635e3b2a4e9e1f2efa13de31f1260ae0145e4a20
richard <richard.fontaine@flaxib.re>
2023-08-07 16:40:19 +0400

    Fix add point

--- src/views/content/graphicage/component/atom/TimelineAddPointButton.tsx
+++ src/views/content/graphicage/component/atom/TimelineAddPointButton.tsx
@@ -1,6 +1,9 @@
 import { FaSolidPlus } from "solid-icons/fa";
+import { useStateAction } from "../../../../../StateAction";
 import { LineUnderConstructionType } from "../../../../../type";
 import "./TimelineAddPointButton.css";
+
+const [, { setLineUnderConstructionNextIndex }] = useStateAction();
 
 // TODO Create stories and cypress
 export function TimelineAddPointButton(props: {
@@ -8,15 +11,6 @@ export function TimelineAddPointButton(props: {
   getter: () => LineUnderConstructionType;
   setter: (line: LineUnderConstructionType) => void;
 }) {
-  const modifyNextIndex = (indice: number) => {
-    const a = [...props.getter()?.stops];
-    a.splice(indice, 1);
-    props.setter({
-      ...props.getter(),
-      nextIndex: indice,
-    });
-  };
-
   return (
     <div class="v-timeline-item">
       <div class="v-timeline-item__body body-add">
@@ -31,7 +25,7 @@ export function TimelineAddPointButton(props: {
         <div class="v-timeline-divider__dot v-timeline-divider__dot--size-very-small timeline-add-point-button">
           <button
             class=""
-            onClick={() => modifyNextIndex(props.indice)}
+            onClick={() => setLineUnderConstructionNextIndex(props.indice)}
             title="Ajouter un point"
           >
             <FaSolidPlus />
