335a27ede5fa5cb026f971e9f64f6e9f24fd0fdd
lucas <lucas.lvn97439@gmail.com>
2023-08-18 15:00:26 +0400

    add test for map.service

---
+++ src/_services/map.service.cy.ts
@@ -0,0 +1,57 @@
+import { useStateGui } from "../StateGui";
+import { ServiceUtils } from "./_utils.service";
+import { MapService } from "./map.service";
+
+const [, { setActiveMapId }] = useStateGui();
+
+describe("SchoolService", () => {
+  setActiveMapId(1);
+
+  /** TODO: check why bus line is not catch.**/
+
+  it("getAll, spy on: generic, buildXanoUrl & get from ServiceUtils, build from MapService", () => {
+    const spyBuildXanoUrl = cy.spy(ServiceUtils, "buildXanoUrl");
+    const spyGeneric = cy.spy(ServiceUtils, "generic");
+    const spyGet = cy.spy(ServiceUtils, "get");
+
+    // const spyMapServiceBuild = cy.spy(MapEntity, "build");
+
+    MapService.getAll();
+
+    expect(spyBuildXanoUrl).to.be.called;
+    expect(spyGeneric).to.be.called;
+    expect(spyGet).to.be.called;
+
+    // expect(spyMapServiceBuild).to.be.called;
+  });
+
+  it("Create, spy on: generic, buildXanoUrl & get from ServiceUtils, build & dbFormat from MapService", () => {
+    const spyBuildXanoUrl = cy.spy(ServiceUtils, "buildXanoUrl");
+    const spyGeneric = cy.spy(ServiceUtils, "generic");
+    const spyGet = cy.spy(ServiceUtils, "post");
+
+    // const spyMapEntityBuild = cy.spy(MapEntity, "build");
+
+    MapService.create({
+      name: "name",
+    });
+
+    expect(spyBuildXanoUrl).to.be.called;
+    expect(spyGeneric).to.be.called;
+    expect(spyGet).to.be.called;
+
+    // expect(spyMapEntityBuild).to.be.called;
+  });
+
+  it("Update, spy on: generic, buildXanoUrl & get from ServiceUtils, build & dbFormat from MapService", () => {
+    const spyBuildXanoUrl = cy.spy(ServiceUtils, "buildXanoUrl");
+    const spyGeneric = cy.spy(ServiceUtils, "generic");
+    const spyGet = cy.spy(ServiceUtils, "delete");
+
+    MapService.delete(1);
+
+    expect(spyBuildXanoUrl).to.be.called;
+    expect(spyGeneric).to.be.called;
+    expect(spyGet).to.be.called;
+  });
+});
