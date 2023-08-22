49dd475b4eccfb8d2765c78431be91826a8b09ef
lucas <lucas.lvn97439@gmail.com>
2023-08-22 10:55:46 +0400

    refacto french to english & use Tables components & Filters

--- src/views/content/stops/StopsBoard.tsx
+++ src/views/content/stops/StopsBoard.tsx
@@ -1,24 +1,22 @@
-import { AiOutlineSearch } from "solid-icons/ai";
 import { For, createEffect, createSignal, onCleanup, onMount } from "solid-js";
-import { useStateGui } from "../../../StateGui";
 import { StopType } from "../../../_entities/stop.entity";
 import { StopService } from "../../../_services/stop.service";
-import ExportCsvButton from "../../../component/ExportCsvButton";
-import ImportCsvButton from "../../../component/ImportCsvButton";
 import ImportCsvCanvas from "../../../component/ImportCsvCanvas";
 import ImportCsvDialogBox from "../../../component/ImportCsvDialogBox";
-import ActionSelector from "../../../component/atom/ActionSelector";
-import Button from "../../../component/atom/Button";
 import PageTitle from "../../../component/atom/PageTitle";
 import RemoveRamassageConfirmation from "../../../userInformation/RemoveRamassageConfirmation";
+import { setSchools } from "../graphicage/component/organism/SchoolPoints";
 import {
   getStops,
   setStops,
 } from "../graphicage/component/organism/StopPoints";
-import EditStop, { setDataToEdit, toggleEditStop } from "./EditStop";
-import StopItems from "./StopItem";
-
-const [, { getActiveMapId }] = useStateGui();
+import SchoolItem from "../schools/SchoolItem";
+import Checkbox from "../schools/component/atom/Checkbox";
+import TableHeaderCell from "../schools/component/molecule/TableHeaderCell";
+import TableBody from "../schools/component/organism/TableBody";
+import TableHeader from "../schools/component/organism/TableHeader";
+import EditStop from "./EditStop";
+import Filters, { searchInputKeyword } from "./component/organism/Filters";
 
 export async function fetchStop() {
   const stops: StopType[] = await StopService.getAll();
@@ -31,21 +29,21 @@ function preventDefaultHandler(e: DragEvent) {
 
 export default function () {
   let stopDiv!: HTMLDivElement;
-  let refCheckbox!: HTMLInputElement;
-
-  const [keyword, setKeyword] = createSignal("");
-
-  const filteredRamassages = () =>
+  // let refCheckbox!: HTMLInputElement;
+  const [refCheckbox, setRefCheckbox] = createSignal<HTMLInputElement>(
+    document.createElement("input")
+  );
+  const filteredStops = () =>
     getStops().filter((e) =>
-      e.name.toLowerCase().includes(keyword().toLowerCase())
+      e.name.toLowerCase().includes(searchInputKeyword().toLowerCase())
     );
 
-  const selectedRamassages = () => getStops().filter((ram) => ram.selected);
+  const selectedStops = () => getStops().filter((ram) => ram.selected);
 
   createEffect(() => {
-    refCheckbox.checked =
-      filteredRamassages().length != 0 &&
-      selectedRamassages().length == filteredRamassages().length;
+    refCheckbox().checked =
+      filteredStops().length != 0 &&
+      selectedStops().length == filteredStops().length;
   });
 
   const [displayImportCsvCanvas, setDisplayImportCsvCanvas] =
@@ -73,6 +71,14 @@ export default function () {
     stopDiv.removeEventListener("dragover", preventDefaultHandler);
   });
 
+  // TODO: check why when checking global checkbox, only one item is selected
+  const globalCheckboxOnChange = () => {
+    getStops().map((stop) => {
+      stop.setSelected(refCheckbox().checked);
+      console.log(stop.selected(), refCheckbox().checked);
+    });
+  };
+
   return (
     <>
       <ImportCsvDialogBox />
@@ -87,84 +93,40 @@ export default function () {
       <div class="flex w-full bg-white" ref={stopDiv}>
         <div id="ramassages-board">
           <header>
-            <div>
-              <PageTitle title="Points de ramassage" />
-            </div>
-            <div id="filters">
-              <div class="left">
-                <ActionSelector isDisabled={true} />
-                <Button
-                  label="Ajouter"
-                  onClick={() => {
-                    setDataToEdit();
-                    toggleEditStop();
-                  }}
-                />
-
-                <div class="input-field">
-                  <div class="input-search-logo">
-                    <AiOutlineSearch />
-                  </div>
-                  <input
-                    class="bg-white"
-                    type="text"
-                    name="search"
-                    placeholder="Recherche"
-                    onInput={(e) => {
-                      setKeyword(e.currentTarget.value);
-                    }}
-                  />
-                </div>
-              </div>
-
-              <div class="right">
-                <ExportCsvButton
-                  exportRoute={`/map/${getActiveMapId()}/export/csv_ramassage`}
-                  filename="ramassage"
-                />
-                <ImportCsvButton />
-              </div>
-            </div>
+            <PageTitle title="Points de ramassage" />
+            <Filters />
           </header>
+
           <div class="board-content">
             <div class="h-[78vh]">
               <table class="min-w-full">
-                <thead>
-                  <tr>
-                    <th scope="col" class="pl-4 pr-3 sm:pl-0 flex items-center">
-                      <input
-                        id="comments"
-                        aria-describedby="comments-description"
-                        name="comments"
-                        type="checkbox"
-                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative right-2"
-                        onChange={(e) => {
-                          setStops((stops) =>
-                            stops.map((eta) => ({
-                              ...eta,
-                              selected: e.target.checked,
-                            }))
-                          );
-                        }}
-                        ref={refCheckbox}
-                      />
-                      Nom
-                    </th>
-                    <th scope="col">Nombre d'élèves</th>
-                    <th scope="col">Nombre d'établissements desservis</th>
-                    <th scope="col">Nombre de lignes</th>
-                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                      <span class="">Actions</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <For each={filteredRamassages()}>
-                    {(fields) => (
-                      <StopItems item={fields} setRamassages={setStops} />
-                    )}
+                <TableHeader>
+                  <TableHeaderCell>
+                    <Checkbox
+                      ariaDescribedby="school-item"
+                      name="school"
+                      ref={setRefCheckbox}
+                      onChange={globalCheckboxOnChange}
+                    />
+                  </TableHeaderCell>
+                  <TableHeaderCell>Nom</TableHeaderCell>
+                  <TableHeaderCell>Nombre de d'élèves</TableHeaderCell>
+                  <TableHeaderCell>Nombre de lignes</TableHeaderCell>
+                  <TableHeaderCell>Actions</TableHeaderCell>
+                </TableHeader>
+
+                <TableBody>
+                  <For each={filteredStops()}>
+                    {(fields) => {
+                      return (
+                        <SchoolItem
+                          item={fields}
+                          setEtablissements={setSchools}
+                        />
+                      );
+                    }}
                   </For>
-                </tbody>
+                </TableBody>
               </table>
             </div>
           </div>
