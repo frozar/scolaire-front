3b4f3fb75162fd4b457ef335747d7617428bf3cf
lucas <lucas.lvn97439@gmail.com>
2023-08-30 14:30:01 +0400

    add cypress test

--- src/views/content/graphicage/informationBoard/components/atom/Label.cy.tsx
+++ src/views/content/graphicage/informationBoard/components/atom/Label.cy.tsx
@@ -1,1 +1,14 @@
-// TODO
+import "leaflet/dist/leaflet.css";
+import Label from "./Label";
+
+describe("PointsEtablissement component", () => {
+  it("Points snapshot", () => {
+    cy.mount(() => {
+      return <Label label="Label text" for="input-id" />;
+    });
+
+    cy.get("label").contains("Label text");
+    cy.get("label").invoke("attr", "for").should("eq", "input-id");
+    cy.get("label").compareSnapshot("Label", 0.01);
+  });
+});
