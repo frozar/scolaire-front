c58096b270fafcd4ea666c6f895bb23191c1e921
Quentin PAYET <quentinpayet1203@gmail.com>
2023-08-31 14:53:12 +0400

    wip: drag polylinedragmarker

--- src/views/content/graphicage/component/atom/PolylineDragMarker.tsx
+++ src/views/content/graphicage/component/atom/PolylineDragMarker.tsx
@@ -1,5 +1,5 @@
 import L from "leaflet";
-import { onCleanup } from "solid-js";
+import { createEffect, onCleanup } from "solid-js";
 
 type PolylineDragMarkersProps = {
   map: L.Map;
@@ -9,8 +9,6 @@ type PolylineDragMarkersProps = {
 };
 const dotIcon =
   "<svg fill='none' stroke-width='2' xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-point-filled' width='1em' height='1em' viewBox='0 0 24 24' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' style='overflow: visible;'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z' stroke-width='0' fill='black'></path></svg>";
-
-// function onMouseDown() {}
 
 export default function (props: PolylineDragMarkersProps) {
   //   const marker = L.marker()
@@ -24,6 +22,15 @@ export default function (props: PolylineDragMarkersProps) {
   //   pane: "overlayPane",
   //   keyboard: false,
   // }).addTo(props.map);
+  function onMouseDown() {
+    props.map.dragging.disable();
+    createEffect(() => {
+      props.map?.on("mousemove", ({ latlng }) => {
+        polylineDragMarker.setLatLng(latlng);
+      });
+    });
+    // document.addEventListener("mouseup", handleMouseUp) // ! wip
+  }
 
   const polylineDragMarker = L.marker(props.latlngs, {
     icon: dragMarkerIcon,
