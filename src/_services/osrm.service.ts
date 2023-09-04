75de1c2d4ad8bb9eac3093d62c5cdf00ca4419a7
richard <richard.fontaine@flaxib.re>
2023-09-04 10:09:11 +0400

    review

--- src/_services/osrm.service.ts
+++ src/_services/osrm.service.ts
@@ -7,36 +7,49 @@ import { ServiceUtils } from "./_utils.service";
 
 const osrm = import.meta.env.VITE_API_OSRM_URL;
 
+type osrmResponseType = { routes: routesType[] };
+
 export class OsrmService {
   static async getRoadPolyline(
     points: BusLinePointType[]
   ): Promise<{ latlngs: L.LatLng[]; metrics: busLineMetricType }> {
-    const response = await ServiceUtils.generic(
+    const response: osrmResponseType = await ServiceUtils.generic(
       osrm +
         "/" +
         this.buildPositionURL(points) +
         "?geometries=geojson&overview=full"
     );
 
-    if (!response) return { latlngs: [], metrics: {} };
-
-    const distanceParcourue = response.routes[0].distance;
-
-    const metrics: busLineMetricType = {
-      distance: distanceParcourue,
-    };
-
-    return { latlngs: this.formatResponse(response.routes), metrics };
+    return this.formatResponse(response);
   }
 
   private static buildPositionURL(points: BusLinePointType[]): string {
     return points.map((point) => point.lon + "," + point.lat).join(";");
   }
 
-  private static formatResponse(routes: routesType[]): L.LatLng[] {
-    if (routes[0] == undefined) return [];
+  private static formatResponse(response: osrmResponseType): {
+    latlngs: L.LatLng[];
+    metrics: busLineMetricType;
+  } {
+    let latlngs: L.LatLng[] = [];
+    let metrics: busLineMetricType = {};
+
+    if (!response || response.routes[0] == undefined)
+      return { latlngs, metrics };
+
+    const routes = response.routes;
+
     const coordinates = routes[0].geometry.coordinates;
-    return coordinates.map((elt: number[]) => L.latLng(elt[1], elt[0]));
+
+    latlngs = coordinates.map((elt: number[]) => L.latLng(elt[1], elt[0]));
+
+    const distance = response.routes[0].distance;
+
+    metrics = {
+      distance,
+    };
+
+    return { latlngs, metrics };
   }
 }
 
