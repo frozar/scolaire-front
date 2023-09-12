95de2731812a7b2c357cd1b0336f0c6f45a1dd2b
lucas <lucas.lvn97439@gmail.com>
2023-09-12 16:57:34 +0400

    use externelized panel buttons

--- src/views/content/schools/component/organism/SchoolDetails.tsx
+++ src/views/content/schools/component/organism/SchoolDetails.tsx
@@ -2,11 +2,13 @@ import { Match, Switch, createSignal, onMount } from "solid-js";
 import { SchoolType } from "../../../../../_entities/school.entity";
 import { changeBoard } from "../../../board/component/template/ContextManager";
 import SchoolDetailsHeader from "../molecule/SchoolDetailsHeader";
+import SchoolDetailsPanelsButton from "../molecule/SchoolDetailsPanelsButton";
+import ClassesList from "./ClassesList";
 
 export const [schoolDetailsItem, setSchoolDetailsItem] =
   createSignal<SchoolType>();
 
-enum Panels {
+export enum Panels {
   classes = "classes",
   lines = "lines",
 }
@@ -25,23 +27,12 @@ export default function () {
       <SchoolDetailsHeader />
 
       <div class="content mt-2">
-        <div class="labels flex gap-2 text-xs text-white">
-          <button
-            class="px-4 py-2 bg-green-base flex justify-center rounded-2xl"
-            onClick={() => setOnPanel(Panels.classes)}
-          >
-            classes
-          </button>
-          <button
-            class="px-4 py-2 bg-green-base flex justify-center rounded-2xl"
-            onClick={() => setOnPanel(Panels.lines)}
-          >
-            lignes: Todo
-          </button>
-        </div>
+        <SchoolDetailsPanelsButton setOnPanel={setOnPanel} />
 
         <Switch>
-          <Match when={onPanel() == Panels.classes}>classes</Match>
+          <Match when={onPanel() == Panels.classes}>
+            <ClassesList />
+          </Match>
           <Match when={onPanel() == Panels.lines}>lines</Match>
         </Switch>
       </div>
