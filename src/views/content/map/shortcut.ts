ae6d90988edfc70467676c6887fd1c17100be421
lucas <lucas.lvn97439@gmail.com>
2023-09-13 11:53:00 +0400

    fix escape shortcut drawmode

--- src/views/content/map/shortcut.ts
+++ src/views/content/map/shortcut.ts
@@ -89,7 +89,7 @@ function escapeHandler({ code }: KeyboardEvent) {
 
   if (code === "Escape") {
     deselectAllPoints();
-    if (onBoard() == "line") {
+    if (onBoard() != "draw-line") {
       deselectAllBusLines();
       setStopPointsColor([], COLOR_STOP_FOCUS);
       setSchoolPointsColor([], COLOR_SCHOOL_FOCUS);
