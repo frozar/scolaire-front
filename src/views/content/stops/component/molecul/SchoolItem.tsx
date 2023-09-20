d601745957031013a092ce85a9cfe84061a00281
lucas <lucas.lvn97439@gmail.com>
2023-09-20 15:11:33 +0400

    add SchoolItem for SchoolList

---
+++ src/views/content/stops/component/molecul/SchoolItem.tsx
@@ -0,0 +1,29 @@
+import { AssociatedPointType } from "../../../../../_entities/_utils.entity";
+import PencilIcon from "../../../../../icons/PencilIcon";
+import TrashIcon from "../../../../../icons/TrashIcon";
+import ButtonIcon from "../../../board/component/molecule/ButtonIcon";
+import "./SchoolItem.css";
+
+export default function (props: { school: AssociatedPointType }) {
+  function onClickDelete() {
+    console.log("delete school");
+  }
+
+  function onClickEdit() {
+    console.log("edit school");
+  }
+
+  return (
+    <div class="school-item">
+      <div class="school-item-content">
+        <p>{props.school.name}</p>
+        <p>{props.school.quantity + " élèves à récuperer"}</p>
+      </div>
+
+      <div class="school-item-actions">
+        <ButtonIcon icon={<TrashIcon />} onClick={onClickDelete} />
+        <ButtonIcon icon={<PencilIcon />} onClick={onClickEdit} />
+      </div>
+    </div>
+  );
+}
