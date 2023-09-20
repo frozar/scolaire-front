4f38d0714bd7424b51f36ad035c1c9d31593f745
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-20 09:32:48 +0400

    circleMarker used instead of marker

--- src/views/content/map/component/atom/WaypointMarker.tsx
+++ src/views/content/map/component/atom/WaypointMarker.tsx
@@ -6,7 +6,7 @@ import {
   updatePolylineWithOsrm,
 } from "../../../../../_entities/bus-line.entity";
 
-import { COLOR_GREEN_BASE } from "../../constant";
+import { COLOR_WAYPOINT } from "../../constant";
 
 const [, { setLineUnderConstruction, getLineUnderConstruction }] =
   useStateAction();
@@ -17,17 +17,7 @@ type PolylineDragMarkersProps = {
   index: number;
 };
 
-const locationDotIcon =
-  "<svg fill=" +
-  COLOR_GREEN_BASE +
-  " stroke='black' stroke-width='1' xmlns='http://www.w3.org/2000/svg' viewBox='100 300 320 512' height='2em' width='2em' style='overflow: visible;'><path d='M16 144a144 144 0 1 1 288 0 144 144 0 1 1-288 0zm144-64c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96 0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zm-32 400V317.1a177.984 177.984 0 0 0 64 0V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z'></path></svg>";
-
 export default function (props: PolylineDragMarkersProps) {
-  const waypointMarkerIcon = L.divIcon({
-    className: "dragMarker",
-    html: locationDotIcon,
-  });
-
   function onRightClick() {
     const waypoints = [
       ...(getLineUnderConstruction().busLine.waypoints as WaypointType[]),
@@ -44,10 +34,12 @@ export default function (props: PolylineDragMarkersProps) {
     updatePolylineWithOsrm(getLineUnderConstruction().busLine);
   }
   // eslint-disable-next-line solid/reactivity
-  const waypointMarker = L.marker(props.latlngs, {
-    icon: waypointMarkerIcon,
+  const waypointMarker = L.circleMarker(props.latlngs, {
+    weight: 0,
+    radius: 6,
+    fillColor: COLOR_WAYPOINT,
+    fillOpacity: 1,
     pane: "markerPane",
-    keyboard: false,
   }).on("contextmenu", onRightClick);
 
   // eslint-disable-next-line solid/reactivity
