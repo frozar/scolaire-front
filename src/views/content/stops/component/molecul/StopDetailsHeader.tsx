dcd8e72ec71ca101b78fe67d107669c6e14f2d1c
lucas <lucas.lvn97439@gmail.com>
2023-09-20 15:09:05 +0400

    add StopDetailsHeader component

---
+++ src/views/content/stops/component/molecul/StopDetailsHeader.tsx
@@ -0,0 +1,25 @@
+import { createSignal } from "solid-js";
+import { StopType } from "../../../../../_entities/stop.entity";
+import PencilIcon from "../../../../../icons/PencilIcon";
+import ButtonIcon from "../../../board/component/molecule/ButtonIcon";
+
+export default function (props: { stop: StopType }) {
+  const [inputRef, setInputRef] = createSignal<HTMLInputElement>();
+  const [editStopName, setEditStopName] = createSignal<boolean>(true);
+  const editName = () => {
+    setEditStopName((bool) => !bool);
+    inputRef()?.focus();
+  };
+
+  return (
+    <header class="stop-details-header">
+      <input
+        type="text"
+        value={props.stop?.name}
+        ref={setInputRef}
+        disabled={editStopName()}
+      />
+      <ButtonIcon icon={<PencilIcon />} onClick={editName} />
+    </header>
+  );
+}
