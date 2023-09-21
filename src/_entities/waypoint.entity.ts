856cfeedf68da23b9e8c10c822974cc3714af0c8
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-21 13:04:19 +0400

    wip: review changes

---
+++ src/_entities/waypoint.entity.ts
@@ -0,0 +1,34 @@
+import { NatureEnum } from "../type";
+import { BusLineType, WaypointType } from "./bus-line.entity";
+
+export namespace WaypointEntity {
+  export function createWaypointsFromPoints(busLine: BusLineType) {
+    const waypoints: WaypointType[] = [];
+
+    for (const point of busLine.points) {
+      if (point.nature == NatureEnum.school) {
+        waypoints.push({
+          idSchool: point.id,
+          lon: point.lon,
+          lat: point.lat,
+        });
+      } else if (point.nature == NatureEnum.stop) {
+        waypoints.push({
+          idStop: point.id,
+          lon: point.lon,
+          lat: point.lat,
+        });
+      }
+    }
+
+    // setLineUnderConstruction({
+    //   ...getLineUnderConstruction(),
+    //   busLine: {
+    //     ...getLineUnderConstruction().busLine,
+    //     waypoints: waypoints,
+    //   },
+    // });
+    // return { ...busLine, waypoints };
+    return waypoints;
+  }
+}
