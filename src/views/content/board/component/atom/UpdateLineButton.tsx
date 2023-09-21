146e05ac8e96997eb7088f3a030030a91d545c81
richard <richard.fontaine@flaxib.re>
2023-09-21 15:08:37 +0400

    Review 1

--- src/views/content/board/component/atom/UpdateLineButton.tsx
+++ src/views/content/board/component/atom/UpdateLineButton.tsx
@@ -3,15 +3,11 @@ import { BusLineType } from "../../../../../_entities/bus-line.entity";
 import { deselectAllBusLines } from "../../../map/component/organism/BusLines";
 
 import { createSignal } from "solid-js";
-import UpdateButton from "../../../../../icons/UpdateButton";
+import UpdateButton from "../../../../../icons/UpdatePen";
 import { deselectAllPoints } from "../../../map/component/organism/Points";
 import { drawModeStep, setCurrentStep } from "../organism/DrawModeBoardContent";
 import { changeBoard, toggleDrawMod } from "../template/ContextManager";
 import "./DrawUpdateButton.css";
-
-// interface DrawHelperButtonProps {
-//   schools: SchoolType[] | undefined;
-// }
 
 const [, { setLineUnderConstruction, setModeAddLine }] = useStateAction();
 
