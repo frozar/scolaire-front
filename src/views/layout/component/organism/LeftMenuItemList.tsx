277eb2f5118bcdb7f92de62678bc5642c7e995f1
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-25 17:03:11 +0400

    wip

--- src/views/layout/component/organism/LeftMenuItemList.tsx
+++ src/views/layout/component/organism/LeftMenuItemList.tsx
@@ -3,7 +3,17 @@ import { For, createEffect, mergeProps } from "solid-js";
 import { useStateGui } from "../../../../StateGui";
 import { SelectedMenuType } from "../../../../type";
 
+import {
+  setSchoolPointsColor,
+  setStopPointsColor,
+} from "../../../../leafletUtils";
 import { onBoard } from "../../../content/board/component/template/ContextManager";
+import { deselectAllBusLines } from "../../../content/map/component/organism/BusLines";
+import { deselectAllPoints } from "../../../content/map/component/organism/Points";
+import {
+  COLOR_SCHOOL_FOCUS,
+  COLOR_STOP_FOCUS,
+} from "../../../content/map/constant";
 import menuItems from "../../menuItemFields";
 import LeftMenuItem from "../molecule/LeftMenuItem";
 
@@ -14,6 +24,13 @@ export interface LeftMenuItemProps {
   setSelectedMenu?: (itemMenu: SelectedMenuType) => void;
 }
 
+function deselectAllPointsAndBusLines() {
+  deselectAllPoints();
+  deselectAllBusLines();
+  setStopPointsColor([], COLOR_STOP_FOCUS);
+  setSchoolPointsColor([], COLOR_SCHOOL_FOCUS);
+}
+
 export default function (props: LeftMenuItemProps) {
   const mergedProps = mergeProps({ getSelectedMenu, setSelectedMenu }, props);
   createEffect(() => {
@@ -22,12 +39,17 @@ export default function (props: LeftMenuItemProps) {
       return;
     }
     if (["line", "line-draw", "line-details"].includes(onBoardMode)) {
+      if (onBoardMode == "line") {
+        deselectAllPointsAndBusLines();
+      }
       setSelectedMenu("graphicage");
     } else if (
       ["schools", "school-details", "school-class"].includes(onBoardMode)
     ) {
+      deselectAllPointsAndBusLines(); // ! cas particulier ou etablissement selectionées à prendre en compte
       setSelectedMenu("schools");
     } else if (["stops", "stop-details"].includes(onBoardMode)) {
+      deselectAllPointsAndBusLines(); // ! cas particulier à prendre en compte
       setSelectedMenu("stops");
     }
   });
