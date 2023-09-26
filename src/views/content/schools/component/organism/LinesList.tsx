39d962c48a706da122ae8c77e21af90e8a875102
lucas <lucas.lvn97439@gmail.com>
2023-09-26 09:30:58 +0400

    style increase

--- src/views/content/schools/component/organism/LinesList.tsx
+++ src/views/content/schools/component/organism/LinesList.tsx
@@ -1,6 +1,7 @@
 import { For } from "solid-js";
 import { BusLineType } from "../../../../../_entities/bus-line.entity";
 import LineItem from "../molecule/LineItem";
+import "./LinesList.css";
 
 export default function (props: { lines: BusLineType[] }) {
   return (
