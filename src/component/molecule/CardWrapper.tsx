1a1166c2320d30b7278b135f54557c1fab3fef93
lucas <lucas.lvn97439@gmail.com>
2023-09-26 10:07:36 +0400

    add CardWrapper component

---
+++ src/component/molecule/CardWrapper.tsx
@@ -0,0 +1,13 @@
+import { JSXElement, children } from "solid-js";
+import "./CardWrapper.css";
+
+interface CardWrapperProps {
+  children: JSXElement;
+  class?: string;
+}
+
+export default function (props: CardWrapperProps) {
+  const childs = children(() => props.children);
+
+  return <section class={"card " + props.class}>{childs()}</section>;
+}
