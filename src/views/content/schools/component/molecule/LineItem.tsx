a14dddfd0d0229a7f9fd970cd465358ee3c6ec23
lucas <lucas.lvn97439@gmail.com>
2023-09-26 16:37:03 +0400

    add trash button to line card item

--- src/views/content/schools/component/molecule/LineItem.tsx
+++ src/views/content/schools/component/molecule/LineItem.tsx
@@ -1,20 +1,40 @@
+import { createSignal } from "solid-js";
 import { BusLineType } from "../../../../../_entities/bus-line.entity";
 import CardTitle from "../../../../../component/atom/CardTitle";
 import Pellet from "../../../../../component/atom/Pellet";
 import CardWrapper from "../../../../../component/molecule/CardWrapper";
 import ArretsLogo from "../../../../../icons/ArretsLogo";
+import TrashIcon from "../../../../../icons/TrashIcon";
+import ButtonIcon from "../../../board/component/molecule/ButtonIcon";
 import { onClickBusLine } from "../../../map/component/molecule/BusLine";
 import ClasseLinkedSchool from "../atom/ClasseLinkedSchool";
 import "./LineItem.css";
 
 export default function (props: { line: BusLineType }) {
+  const [refTrashButton, setRefTrashButton] = createSignal<HTMLButtonElement>();
   const schoolNames = () => props.line.schools.map((school) => school.name);
 
+  function onClickDelete() {
+    console.log("delete line");
+  }
+
   return (
-    <CardWrapper class="line-item" onClick={() => onClickBusLine(props.line)}>
+    <CardWrapper
+      class="line-item"
+      onClick={() => onClickBusLine(props.line)}
+      refClickableButtons={[refTrashButton]}
+    >
       <Pellet color={props.line.color()} />
       <div class="line-content">
-        <CardTitle title={props.line.name ?? "Pas de nom de ligne"} />
+        <div class="flex justify-between">
+          <CardTitle title={props.line.name ?? "Pas de nom de ligne"} />
+
+          <ButtonIcon
+            icon={<TrashIcon />}
+            onClick={onClickDelete}
+            refSetter={setRefTrashButton}
+          />
+        </div>
         <ClasseLinkedSchool schools={schoolNames()} />
 
         <div class="line-stops-count">
