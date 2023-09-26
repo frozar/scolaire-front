97f13e5ea2230298a3b18773faa56408a4f5c9be
richard <richard.fontaine@flaxib.re>
2023-09-26 16:59:23 +0400

    Add File

---
+++ src/views/content/board/component/atom/RemoveLineButton.tsx
@@ -0,0 +1,28 @@
+import { BusLineType } from "../../../../../_entities/bus-line.entity";
+import TrashIcon from "../../../../../icons/TrashIcon";
+import { setRemoveConfirmation } from "../../../../../signaux";
+import { deselectAllPoints } from "../../../map/component/organism/Points";
+
+export interface RemoveLineButtonProps {
+  busLine: BusLineType;
+}
+
+export default function (props: RemoveLineButtonProps) {
+  const onclick = () => {
+    deselectAllPoints();
+    if (props.busLine.id) {
+      setRemoveConfirmation({
+        displayed: true,
+        idBusLine: props.busLine.id,
+      });
+    }
+  };
+
+  return (
+    <div class="graphicage-draw-update-button">
+      <button onClick={onclick}>
+        <TrashIcon />
+      </button>
+    </div>
+  );
+}
