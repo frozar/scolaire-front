8ffe59b4d5c062b076984ce718940c39a75b872d
lucas <lucas.lvn97439@gmail.com>
2023-09-26 13:53:28 +0400

    add logic to disable card onClick function when over ref button

--- src/component/molecule/CardWrapper.tsx
+++ src/component/molecule/CardWrapper.tsx
@@ -1,20 +1,44 @@
-import { JSXElement, children } from "solid-js";
+import {
+  Accessor,
+  JSXElement,
+  children,
+  createEffect,
+  createSignal,
+} from "solid-js";
 import "./CardWrapper.css";
 
 interface CardWrapperProps {
   children: JSXElement;
   class?: string;
   onClick?: () => void;
+  refClickableButton?: Accessor<HTMLButtonElement>;
 }
 
 export default function (props: CardWrapperProps) {
   const childs = children(() => props.children);
+  const [hoverClickable, setHoverClickable] = createSignal(false);
+  const toggleHoverClickable = () => setHoverClickable((bool) => !bool);
 
   function onClick() {
-    if (props.onClick != undefined) {
+    if (props.onClick != undefined && !hoverClickable()) {
       props.onClick();
     }
   }
+
+  createEffect(() => {
+    if (
+      props.refClickableButton != undefined &&
+      props.refClickableButton() != undefined
+    ) {
+      props.refClickableButton()?.addEventListener("mouseenter", () => {
+        toggleHoverClickable();
+      });
+
+      props.refClickableButton()?.addEventListener("mouseleave", () => {
+        toggleHoverClickable();
+      });
+    }
+  });
 
   return (
     <section
