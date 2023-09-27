039ede93e7cf5d9dece6d68eb0d2bf5149b76dab
richard <richard.fontaine@flaxib.re>
2023-09-27 11:52:10 +0400

    Modify drop zone wip

--- src/component/ImportCsvCanvas.tsx
+++ src/component/ImportCsvCanvas.tsx
@@ -72,7 +72,23 @@ export default function (props: {
   return (
     <div ref={mapDragDropDiv} classList={{ highlight: props.display }}>
       <div class="child" classList={{ invisible: !props.display }}>
-        Drop your file here
+        <svg
+          class="h-150 w-300 text-gray-400"
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 48 48"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width={2}
+            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+          />
+        </svg>
+        <span class="mt-2 block text-sm font-semibold text-gray-900">
+          Drop your file here
+        </span>
       </div>
     </div>
   );
