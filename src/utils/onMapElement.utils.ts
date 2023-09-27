5057e681778a56b8aa8752c4c5ff6ec0a28c8cf4
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-27 11:17:57 +0400

    new utils file added

---
+++ src/utils/onMapElement.utils.ts
@@ -0,0 +1,16 @@
+import { setSchoolPointsColor, setStopPointsColor } from "../leafletUtils";
+import { deselectAllBusLines } from "../views/content/map/component/organism/BusLines";
+import { deselectAllPoints } from "../views/content/map/component/organism/Points";
+import {
+  COLOR_SCHOOL_FOCUS,
+  COLOR_STOP_FOCUS,
+} from "../views/content/map/constant";
+
+export namespace OnMapElementUtils {
+  export function deselectAllPointsAndBusLines() {
+    deselectAllPoints();
+    deselectAllBusLines();
+    setStopPointsColor([], COLOR_STOP_FOCUS);
+    setSchoolPointsColor([], COLOR_SCHOOL_FOCUS);
+  }
+}
