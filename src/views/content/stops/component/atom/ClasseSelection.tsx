0207c61eaaf4ef63453e8c4b288a4fbd7eb9df36
lucas <lucas.lvn97439@gmail.com>
2023-09-29 16:35:28 +0400

    remove disable props of ClasseSelection, cause disabled item is managed from ref

--- src/views/content/stops/component/atom/ClasseSelection.tsx
+++ src/views/content/stops/component/atom/ClasseSelection.tsx
@@ -1,16 +1,15 @@
-import { Setter, For } from "solid-js";
+import { For, Setter } from "solid-js";
 import { ClasseType } from "../../../../../_entities/school.entity";
 
 interface ClasseSelectProps {
   refSelectSetter: Setter<HTMLInputElement>;
   onChange: () => void;
   classes: ClasseType[];
-  disabled?: boolean;
 }
 
 export default function (props: ClasseSelectProps) {
   return (
-    <select disabled={props.disabled}>
+    <select onChange={props.onChange} ref={props.refSelectSetter}>
       <option value="default">Selectionner une classe</option>
       <For each={props.classes}>
         {(classe) => <option value={classe.id}>{classe.classe}</option>}
