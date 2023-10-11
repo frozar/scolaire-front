0811af5b5b5625d4b405a1087e7a0a6cac976e02
lucas <lucas.lvn97439@gmail.com>
2023-10-11 16:06:38 +0400

    clean code

--- src/views/content/stops/component/molecul/StudentSchoolClassItem.tsx
+++ src/views/content/stops/component/molecul/StudentSchoolClassItem.tsx
@@ -1,5 +1,8 @@
 import { Show, createSignal } from "solid-js";
-import { AssociatedPointType } from "../../../../../_entities/_utils.entity";
+import {
+  ClassStudentToSchool,
+  ClassToSchoolTypeFormatedWithUsedQuantity,
+} from "../../../../../_entities/student-to-school.entity";
 import { StudentToSchoolService } from "../../../../../_services/student-to-school.service";
 import CardTitle from "../../../../../component/atom/CardTitle";
 import CardWrapper from "../../../../../component/molecule/CardWrapper";
@@ -7,25 +10,24 @@ import PencilIcon from "../../../../../icons/PencilIcon";
 import TrashIcon from "../../../../../icons/TrashIcon";
 import { QuantityUtils } from "../../../../../utils/quantity.utils";
 import ButtonIcon from "../../../board/component/molecule/ButtonIcon";
+import { stopDetailsItem } from "../organism/StopDetails";
 import EditStudentSchoolClassItem from "./EditStudentSchoolClassItem";
 import "./StudentSchoolClassItem.css";
 
 export default function (props: {
-  school: AssociatedPointType;
-  removeClassStudentToSchoolItem: (id: number) => void;
-  updateClassStudentToSchoolOfStop: (classItem: AssociatedPointType) => void;
+  school: ClassToSchoolTypeFormatedWithUsedQuantity;
 }) {
   const [editingMode, setEditingMode] = createSignal(false);
 
   async function onClickDelete() {
-    const response = await StudentToSchoolService.delete(
-      props.school.studentSchoolId
+    const response = await StudentToSchoolService.delete(props.school.id);
+    ClassStudentToSchool.removeFromStop(
+      response,
+      stopDetailsItem()?.id as number
     );
-    props.removeClassStudentToSchoolItem(response);
   }
 
   function onClickEdit() {
-    console.log("edit school");
     setEditingMode(true);
   }
 
@@ -36,16 +38,14 @@ export default function (props: {
         <EditStudentSchoolClassItem
           classStudentToSchool={props.school}
           close={() => setEditingMode(false)}
-          updateClassStudentToSchoolOfStop={
-            props.updateClassStudentToSchoolOfStop
-          }
         />
       }
     >
       <CardWrapper class="school-list-item">
         <div class="school-list-item-content">
-          <CardTitle title={props.school.name} />
+          <CardTitle title={props.school.school.name} />
           <p class="school-list-item-quantity">
+            {/* TODO: Lucas fix remaining with new type */}
             {QuantityUtils.remaining(props.school) +
               " élèves restant sur " +
               props.school.quantity}
