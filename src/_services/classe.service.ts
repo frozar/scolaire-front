ed0508ea0ec5c481483f5b19302dae05df649e33
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-11 11:03:10 +0400

    deletion works

--- src/_services/classe.service.ts
+++ src/_services/classe.service.ts
@@ -5,14 +5,14 @@ import {
 } from "../_entities/classe.entity";
 import { ServiceUtils } from "./_utils.service";
 
-export namespace ClasseService {
-  export async function create(classe: ClasseType): Promise<ClasseType> {
+export class ClasseService {
+  static async create(classe: ClasseType): Promise<ClasseType> {
     const data = ClasseEntity.dbFormat(classe);
     const dbClasse: ClasseDBType = await ServiceUtils.post("/classe", data);
     return ClasseEntity.build(dbClasse);
   }
 
-  export async function update(classe: ClasseType): Promise<ClasseType> {
+  static async update(classe: ClasseType): Promise<ClasseType> {
     const data = ClasseEntity.dbFormat(classe);
     const dbClasse: ClasseDBType = await ServiceUtils.patch(
       "/classe/" + classe.id,
@@ -20,5 +20,9 @@ export namespace ClasseService {
     );
     if (dbClasse == null) return dbClasse;
     return ClasseEntity.build(dbClasse);
+  }
+
+  static async delete(id: number): Promise<boolean> {
+    return await ServiceUtils.delete("/classe/" + id);
   }
 }
--- src/views/content/schools/component/molecule/ClasseItem.tsx
+++ src/views/content/schools/component/molecule/ClasseItem.tsx
@@ -1,10 +1,19 @@
 import { FaRegularTrashCan, FaSolidPen } from "solid-icons/fa";
 import { ClasseType } from "../../../../../_entities/classe.entity";
+import { ClasseService } from "../../../../../_services/classe.service";
 import CardTitle from "../../../../../component/atom/CardTitle";
 import CardWrapper from "../../../../../component/molecule/CardWrapper";
 import ButtonIcon from "../../../board/component/molecule/ButtonIcon";
 import { changeBoard } from "../../../board/component/template/ContextManager";
+import {
+  getSchools,
+  setSchools,
+} from "../../../map/component/organism/SchoolPoints";
 import { setSelectedClasse } from "../organism/ClasseBoard";
+import {
+  schoolDetailsItem,
+  setSchoolDetailsItem,
+} from "../organism/SchoolDetails";
 import "./ClasseItem.css";
 
 interface ClasseItemProps {
@@ -18,8 +27,28 @@ export default function (props: ClasseItemProps) {
     changeBoard("school-class-modify");
   }
 
-  function onClickDelete() {
+  async function onClickDelete() {
     console.log("Delete classe");
+    const isDeleted = await ClasseService.delete(props.classe.id as number);
+    console.log("isDeleted", isDeleted);
+
+    if (isDeleted) {
+      // eslint-disable-next-line solid/reactivity
+      setSchools((prev) => {
+        return [...prev].map((school) => {
+          return {
+            ...school,
+            classes: school.classes.filter(
+              (classe) => classe.id != props.classe.id
+            ),
+          };
+        });
+      });
+    }
+    const schoolDetailsItemId = schoolDetailsItem()?.id as number;
+    setSchoolDetailsItem(
+      getSchools().filter((school) => school.id == schoolDetailsItemId)[0]
+    );
   }
 
   return (
