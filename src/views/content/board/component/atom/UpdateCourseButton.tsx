1dfbd69d6795f7251dd3d2bd782ea92ef9357714
AdChiv <adrien@entropik.digital>
2023-10-12 09:18:56 +0400

    fix after rebase

--- src/views/content/board/component/atom/UpdateCourseButton.tsx
+++ src/views/content/board/component/atom/UpdateCourseButton.tsx
@@ -1,32 +1,28 @@
-import { useStateAction } from "../../../../../StateAction";
 import { CourseType } from "../../../../../_entities/course.entity";
 
 import { createSignal } from "solid-js";
+import { useStateAction } from "../../../../../StateAction";
 import UpdateButton from "../../../../../icons/UpdatePen";
 import { MapElementUtils } from "../../../../../utils/mapElement.utils";
-import { drawModeStep, setCurrentStep } from "../organism/DrawModeBoardContent";
+import { DrawModeStep, setCurrentStep } from "../organism/DrawRaceBoard";
 import { changeBoard, toggleDrawMod } from "../template/ContextManager";
 import "./DrawUpdateButton.css";
 
-const [, { setCourseUnderConstruction, setModeAddCourse }] = useStateAction();
+const [, { setModeDrawRace }] = useStateAction();
 
 export const [unmodifiedBusCourse, setUnmodifiedBusCourse] =
   createSignal<CourseType>();
 
 export default function (props: { course: CourseType }) {
   async function onclick() {
-    setCourseUnderConstruction({
-      course: props.course,
-      nextIndex: props.course.points.length ?? 0,
-    });
     const [color, setColor] = createSignal<string>(props.course.color());
     setUnmodifiedBusCourse({ ...props.course, color, setColor });
 
     MapElementUtils.deselectAllPointsAndBusCourses();
     toggleDrawMod();
-    setCurrentStep(drawModeStep.editCourse);
+    setCurrentStep(DrawModeStep.editCourse);
     changeBoard("race-draw");
-    setModeAddCourse();
+    setModeDrawRace();
   }
 
   return (
