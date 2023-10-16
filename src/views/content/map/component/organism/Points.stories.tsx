a95a6b0c2ae96f34fc99d4a88ab2cc34f42d4291
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-16 14:27:07 +0400

    review fix storybook

--- src/views/content/map/component/organism/Points.stories.tsx
+++ src/views/content/map/component/organism/Points.stories.tsx
@@ -1,52 +1,41 @@
 import "leaflet/dist/leaflet.css";
 import { Meta, StoryObj } from "storybook-solidjs";
-import stops from "../../../../../../cypress/fixtures/stopsFromDB.json";
-import schools from "../../../../../../cypress/fixtures/schoolsFromDB.json";
 
 import {
   getDivFullId,
   mapDecorators,
 } from "../../../../../../testing/utils/TestUtils";
 import { initialiseMap } from "../../../../../../testing/utils/mapWrapper";
+import { SchoolType } from "../../../../../_entities/school.entity";
+import schoolsFixture from "./../../../../../../cypress/fixtures/schools.json";
+import stopsFixture from "./../../../../../../cypress/fixtures/stops.json";
+
+import { StopType } from "../../../../../_entities/stop.entity";
 import { Points } from "./Points";
-import { useStateGui } from "../../../../../StateGui";
-const [, { getActiveMapId }] = useStateGui();
 
 const meta = {
   component: Points,
   tags: ["autodocs"],
   decorators: mapDecorators,
-  parameters: {
-    mockData: [
-      {
-        url:
-          import.meta.env.VITE_XANO_URL +
-          "/map/" +
-          getActiveMapId() +
-          "/school",
-        method: "GET",
-        status: 200,
-        response: schools,
-      },
-      {
-        url:
-          import.meta.env.VITE_XANO_URL + "/map/" + getActiveMapId() + "/stop",
-        method: "GET",
-        status: 200,
-        response: stops,
-      },
-    ],
-  },
 } satisfies Meta<typeof Points>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+const schools: SchoolType[] = schoolsFixture;
+const stops: StopType[] = stopsFixture;
 
 export const PointsStory: Story = {
   render: (props: null, options) => {
     const fullId = getDivFullId(options);
     // TODO WARNING testMapId depends of local database.
     // Mock data to fetch
-    return <Points leafletMap={initialiseMap(fullId)} />;
+    return (
+      <Points
+        leafletMap={initialiseMap(fullId)}
+        stops={stops}
+        schools={schools}
+      />
+    );
   },
 };
