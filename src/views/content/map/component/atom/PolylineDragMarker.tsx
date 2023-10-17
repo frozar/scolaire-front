5520e2e9a2106d23e97a1bedac5f911ed786a672
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-17 11:46:50 +0400

    wip: cleaning PolylineDragMarker.tsx

--- src/views/content/map/component/atom/PolylineDragMarker.tsx
+++ src/views/content/map/component/atom/PolylineDragMarker.tsx
@@ -16,6 +16,59 @@ type PolylineDragMarkersProps = {
 export const [draggingWaypointIndex, setDraggingWaypointIndex] =
   createSignal<number>();
 
+function handleMouseUp(
+  map: L.Map,
+  index: number,
+  polylineDragMarker: L.CircleMarker
+) {
+  map.off("mousemove");
+  map.dragging.enable();
+  const waypoints = currentRace().waypoints;
+  if (!waypoints) {
+    return;
+  }
+
+  const newWaypoints = WaypointEntity.createWaypoint(
+    waypoints,
+    index,
+    polylineDragMarker.getLatLng().lat,
+    polylineDragMarker.getLatLng().lng
+  );
+
+  updateWaypoints(newWaypoints);
+
+  setDraggingWaypointIndex();
+}
+
+function handleMouseDown(
+  index: number,
+  map: L.Map,
+  polylineDragMarker: L.CircleMarker
+) {
+  let correctIndex = 0;
+  for (let i = 0; i < index; i++) {
+    if (
+      currentRace().waypoints?.at(i)?.idSchool != undefined ||
+      currentRace().waypoints?.at(i)?.idStop != undefined
+    ) {
+      correctIndex += 1;
+    }
+  }
+  // console.log("currentRace()", currentRace());
+  console.log("current wypoint index", correctIndex);
+
+  setDraggingWaypointIndex(correctIndex);
+  polylineDragMarker.on("mouseup", () =>
+    handleMouseUp(map, index, polylineDragMarker)
+  );
+  map.dragging.disable();
+  createEffect(() => {
+    map.on("mousemove", ({ latlng }) => {
+      polylineDragMarker.setLatLng(latlng);
+    });
+  });
+}
+
 export default function (props: PolylineDragMarkersProps) {
   // ! Check if working properly
   createEffect(() => {
@@ -23,7 +76,7 @@ export default function (props: PolylineDragMarkersProps) {
       !draggingWaypointIndex() &&
       polylineDragMarker.hasEventListeners("mouseup")
     ) {
-      polylineDragMarker.off("mouseup", handleMouseUp);
+      polylineDragMarker.off("mouseup");
       // console.log("mouseup event listener removed");
       // console.log(
       //   "event still present ? => ",
@@ -32,26 +85,6 @@ export default function (props: PolylineDragMarkersProps) {
       polylineDragMarker.setLatLng(props.latlngs);
     }
   });
-  function handleMouseUp() {
-    props.map.off("mousemove");
-    props.map.dragging.enable();
-    const waypoints = currentRace().waypoints;
-    if (!waypoints) {
-      return;
-    }
-
-    const newWaypoints = WaypointEntity.createWaypoint(
-      waypoints,
-      props.index,
-      polylineDragMarker.getLatLng().lat,
-      polylineDragMarker.getLatLng().lng
-    );
-
-    updateWaypoints(newWaypoints);
-
-    setDraggingWaypointIndex();
-    // polylineDragMarker.off("mouseup", handleMouseUp);
-  }
   // eslint-disable-next-line solid/reactivity
   const polylineDragMarker = L.circleMarker(props.latlngs, {
     fillColor: COLOR_WAYPOINT,
@@ -60,38 +93,20 @@ export default function (props: PolylineDragMarkersProps) {
     weight: 0,
     pane: "shadowPane",
     className: "dragMarker",
+  });
+
+  polylineDragMarker
     // eslint-disable-next-line solid/reactivity
-  }).on("mousedown", () => {
-    // ! Create handleMouseDown function
-    let correctIndex = 0;
-    for (let i = 0; i < props.index; i++) {
-      if (
-        currentRace().waypoints?.at(i)?.idSchool != undefined ||
-        currentRace().waypoints?.at(i)?.idStop != undefined
-      ) {
-        correctIndex += 1;
-      }
-    }
-    // console.log("currentRace()", currentRace());
-    console.log("current wypoint index", correctIndex);
-
-    setDraggingWaypointIndex(correctIndex);
-    polylineDragMarker.on("mouseup", handleMouseUp);
-    props.map.dragging.disable();
-    createEffect(() => {
-      props.map.on("mousemove", ({ latlng }) => {
-        polylineDragMarker.setLatLng(latlng);
-      });
+    .on("mousedown", () =>
+      handleMouseDown(props.index, props.map, polylineDragMarker)
+    )
+    .on("mouseover", () => {
+      polylineDragMarker.setStyle({ fillOpacity: 100 });
+    })
+    .on("mouseout", () => {
+      polylineDragMarker.setStyle({ fillOpacity: 0 });
     });
-  });
 
-  polylineDragMarker.on("mouseover", function () {
-    polylineDragMarker.setStyle({ fillOpacity: 100 });
-  });
-
-  polylineDragMarker.on("mouseout", function () {
-    polylineDragMarker.setStyle({ fillOpacity: 0 });
-  });
   // eslint-disable-next-line solid/reactivity
   polylineDragMarker.addTo(props.map);
 
