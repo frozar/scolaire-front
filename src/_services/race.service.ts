b1d77319f7ef6deb51d7423202d3a73e5e5f7546
richard <richard.fontaine@flaxib.re>
2023-10-19 13:11:01 +0400

    cleaning

--- src/_services/race.service.ts
+++ src/_services/race.service.ts
@@ -39,13 +39,11 @@ export class RaceService {
 
   static async update(race: Partial<RaceType>): Promise<RaceType> {
     const data = RaceEntity.dbPartialFormat(race);
-    console.log("partial", data);
     // TODO changer endpoint Xano pour /race
     const dbRace: RaceDBType = await ServiceUtils.patch(
       "/busline/" + getSelectedLine()?.id + "/course/" + race.id,
       data
     );
-    console.log("dbRace", dbRace);
     if (dbRace == null) return dbRace;
     return RaceEntity.build(dbRace);
   }
