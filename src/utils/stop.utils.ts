9c301494830a1aa6cc8617056bac2bc6ba847371
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-26 11:48:32 +0400

    display stop total remaining qty

--- src/utils/stop.utils.ts
+++ src/utils/stop.utils.ts
@@ -1,3 +1,5 @@
+import { NatureEnum } from "../type";
+import { getLines } from "../views/content/map/component/organism/BusLines";
 import { getStops } from "../views/content/map/component/organism/StopPoints";
 
 export namespace StopUtils {
@@ -8,5 +10,22 @@ export namespace StopUtils {
     stop.associated.forEach((assoc) => (quantity += assoc.quantity));
 
     return quantity;
+  }
+
+  export function getRemainingQuantity(stopId: number) {
+    const totalQuantity = getTotalQuantity(stopId);
+
+    let usedQuantity = 0;
+    getLines()
+      .flatMap((line) => line.trips)
+      .flatMap((trip) => trip.tripPoints)
+      .filter(
+        (tripPoint) =>
+          tripPoint.nature == NatureEnum.stop && tripPoint.id == stopId
+      )
+      .flatMap((_tripPoint) => _tripPoint.grades)
+      .forEach((grade) => (usedQuantity += grade.quantity));
+
+    return totalQuantity - usedQuantity;
   }
 }
