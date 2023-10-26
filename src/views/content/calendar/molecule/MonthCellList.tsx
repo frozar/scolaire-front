2ef01a8d7be155fcd7ace56926a625e8525c70d4
lucas <lucas.lvn97439@gmail.com>
2023-10-26 16:40:34 +0400

    setup active cell

--- src/views/content/calendar/molecule/MonthCellList.tsx
+++ src/views/content/calendar/molecule/MonthCellList.tsx
@@ -4,6 +4,11 @@ import { CalendarType } from "../template/Calendar";
 import { CellItemLogic } from "./CellItemLogic";
 import "./MonthCellList.css";
 
+const stringToDate = (dateString: string): Date => {
+  const [day, month, year] = dateString.split("-");
+  return new Date([month, day, year].join("/"));
+};
+
 export function MonthCellList(props: { month: Date; calendar: CalendarType }) {
   const actifDays = () =>
     CalendarUtils.getActifDaysOfMonth(props.calendar, props.month);
