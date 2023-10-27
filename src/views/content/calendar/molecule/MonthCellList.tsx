ff712056a87b45ee8a260188c7b54e18f8b0f6f2
lucas <lucas.lvn97439@gmail.com>
2023-10-27 22:59:32 +0400

    add CellItemLogic molecule wrapper

--- src/views/content/calendar/molecule/MonthCellList.tsx
+++ src/views/content/calendar/molecule/MonthCellList.tsx
@@ -1,7 +1,7 @@
 import { For } from "solid-js";
-import CellItem from "../atom/CellItem";
 import { CalendarUtils } from "../calendar.utils";
 import { CalendarType } from "../template/Calendar";
+import { CellItemLogic } from "./CellItemLogic";
 import "./MonthCellList.css";
 
 export function MonthCellList(props: { month: Date; calendar: CalendarType }) {
@@ -12,26 +12,13 @@ export function MonthCellList(props: { month: Date; calendar: CalendarType }) {
     <div class="month-item">
       <div class="month-item-cells">
         <For each={CalendarUtils.getDaysOfMonth(props.month)}>
-          {(day) => {
-            const currentDate = () =>
-              new Date(props.month.getFullYear(), props.month.getMonth(), day);
-
-            const isActive = () =>
-              actifDays().find(
-                (date) => date.getTime() == currentDate().getTime()
-              ) != undefined;
-
-            return (
-              <CellItem
-                isActive={isActive()}
-                isWeekend={CalendarUtils.isWeekend(currentDate())}
-                onClick={() => {
-                  console.log(CalendarUtils.getNameDay(currentDate()));
-                  console.log(isActive());
-                }}
-              />
-            );
-          }}
+          {(day) => (
+            <CellItemLogic
+              actifDays={actifDays()}
+              day={day}
+              month={props.month}
+            />
+          )}
         </For>
       </div>
     </div>
