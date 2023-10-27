43dfbfc388432af1efc1c66de12373829a6ac6ea
frozar <fabien.rozar@gmail.com>
2023-10-27 19:01:46 +0400

    CurrentUserLogo -> UnloggedUserLogo

--- src/views/layout/component/molecule/LoginAvatar.tsx
+++ src/views/layout/component/molecule/LoginAvatar.tsx
@@ -1,7 +1,7 @@
 import { Show } from "solid-js";
 
-import CurrentUserLogo from "../../../../icons/CurrentUserLogo";
 import LoggedInUserLogo from "../../../../icons/LoggedInUserLogo";
+import UnloggedUserLogo from "../../../../icons/UnloggedUserLogo";
 
 export interface LoginAvatarProps {
   profilePicture: string | undefined;
@@ -11,7 +11,7 @@ export default function (props: LoginAvatarProps) {
   const logoPath = () => (props.profilePicture ? props.profilePicture : "");
 
   return (
-    <Show when={props.profilePicture} fallback={<CurrentUserLogo />}>
+    <Show when={props.profilePicture} fallback={<UnloggedUserLogo />}>
       <LoggedInUserLogo path={logoPath()} />
     </Show>
   );
