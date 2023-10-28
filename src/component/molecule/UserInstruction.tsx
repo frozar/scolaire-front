d9beae9345901ffbe9ce691fd0be5c33ab282769
frozar <fabien.rozar@gmail.com>
2023-10-28 22:59:17 +0400

    Bugfix: display user instruction.

--- src/component/molecule/UserInstruction.tsx
+++ src/component/molecule/UserInstruction.tsx
@@ -1,4 +1,5 @@
 import { useStateGui } from "../../StateGui";
+import { getAuthenticatedUser } from "../../signaux";
 import UserInstructionContainer from "./UserInstructionContainer";
 import "./UserInstructionContainer.css";
 import UserInstructionContent from "./UserInstructionContent";
@@ -7,7 +8,11 @@ const [, { getActiveMapId, getSelectedMenu }] = useStateGui();
 
 export default function () {
   const toShow = () => {
-    return getSelectedMenu() == "dashboard" && getActiveMapId() == null;
+    return (
+      getAuthenticatedUser() != undefined &&
+      getSelectedMenu() == "dashboard" &&
+      getActiveMapId() == null
+    );
   };
 
   return (
