315087027089b4726e14cbfc2e5ff14e9a8fb703
frozar <fabien.rozar@gmail.com>
2023-10-28 19:34:30 +0400

    Add new componants :  - DialogueBox  - InnerModal  - UnloggedUserInformation
    Display user information when the user is not connected.

--- src/component/molecule/DialogueBox.tsx
+++ src/component/molecule/DialogueBox.tsx
@@ -8,7 +8,11 @@ export default function (props: DialogueBoxProps) {
   const childs = children(() => props.children);
 
   return (
+<<<<<<< HEAD
     <div class="relative w-[500px] h-[200px] rounded-lg bg-white p-4 shadow-xl transition-all">
+=======
+    <div class="relative min-w-[300px] w-2/3 min-h-[200px] h-1/5 rounded-lg bg-white p-4 shadow-xl transition-all">
+>>>>>>> aa5759bf (Add new componants :)
       {childs()}
     </div>
   );
