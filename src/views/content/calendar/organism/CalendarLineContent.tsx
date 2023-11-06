3249211720184b6acde7b566c15525f0adea834c
AdChiv <adrien@entropik.digital>
2023-11-06 11:26:17 +0400

    fix css import error on CalendarLineContent

--- src/views/content/calendar/organism/CalendarLineContent.tsx
+++ src/views/content/calendar/organism/CalendarLineContent.tsx
@@ -1,14 +1,14 @@
 import { CalendarType } from "../../../../_entities/calendar.entity";
 import { CalendarLineName } from "../molecule/CalendarLineName";
 import { CalendarMonthsDetails } from "../molecule/CalendarMonthsDetails";
-import "./MonthListContent.css";
+import "./CalendarLineContent.css";
 
 export function CalendarLineContent(props: {
   month: Date;
   calendar: CalendarType;
 }) {
   return (
-    <div class="month-list-cells-wrapper">
+    <div class="calendar-line-content">
       <CalendarLineName calendar={props.calendar} />
       <CalendarMonthsDetails month={props.month} calendar={props.calendar} />
     </div>
