731500f00cd0480f448d9faca0ab6c28be493db0
Quentin PAYET <quentinpayet1203@gmail.com>
2023-11-08 11:46:32 +0400

    wip

--- src/utils/grade.utils.ts
+++ src/utils/grade.utils.ts
@@ -1,9 +1,28 @@
+import { GradeType } from "../_entities/grade.entity";
 import { getSchools } from "../views/content/map/component/organism/SchoolPoints";
 
 export namespace GradeUtils {
-  export function getName(gradeId: number) {
+  function getGrade(gradeId: number): GradeType {
     return getSchools()
       .flatMap((school) => school.grades)
-      .filter((grade) => grade.id == gradeId)[0].name;
+      .filter((grade) => grade.id == gradeId)[0];
+  }
+  // ! Useless ?
+  export function getName(gradeId: number): string {
+    // return getSchools()
+    //   .flatMap((school) => school.grades)
+    //   .filter((grade) => grade.id == gradeId)[0].name;
+    return getGrade(gradeId).name;
+  }
+  //! useles ?
+  export function getSchoolId(gradeId: number): number {
+    // const test =  getSchools()
+    //   .flatMap((school) => school.grades)
+    //   .filter((grade) => grade.id == gradeId)[0];
+    return getSchools().filter((school) =>
+      school.grades.some((grade) => grade.id == gradeId)
+    )[0].id;
+    // ! schoolId est manquant (undefined)
+    // return getGrade(gradeId).schoolId as number;
   }
 }
--- src/views/content/board/component/atom/TripTimelineItem.tsx
+++ src/views/content/board/component/atom/TripTimelineItem.tsx
@@ -2,6 +2,7 @@ import { Setter, Show, createEffect } from "solid-js";
 
 import { TripPointType, TripType } from "../../../../../_entities/trip.entity";
 import { NatureEnum } from "../../../../../type";
+import { GradeUtils } from "../../../../../utils/grade.utils";
 import { TripTimelineRemovePointButton } from "./TripTimelineRemovePointButton";
 
 export function TripTimelineItem(props: {
@@ -86,18 +87,39 @@ function setDividerColor(color: string) {
 }
 
 // TODO: Adapt to drop quantity depending on grade school destination
+// ! Vérifier pour chaque grade, l'école de destination !
 function SumQuantity(tripPoints: TripPointType[], indice: number) {
+  console.log("--------------");
+  console.log("tripOint", tripPoints);
   let sum = 0;
+  const grades: { gradeId: number; schoolId: number; quantity: number }[] = [];
+  // ! Change to use lodash ?
+  // const schoolSum: { [schoolId: number]: number } = {};
   for (let i = 0; i < indice + 1; i++) {
+    const interGrades = tripPoints[i].grades.map((grade) => {
+      return {
+        gradeId: grade.gradeId,
+        schoolId: GradeUtils.getSchoolId(grade.gradeId),
+        quantity: grade.quantity,
+      };
+    });
+    grades.push(...interGrades);
+    console.log("grades", grades);
     if (tripPoints[i].nature == NatureEnum.stop) {
-      sum += tripPoints[i].grades
-        .map((grade) => grade.quantity)
-        .reduce((a, b) => a + b, 0);
+      // ! Rewrite
+      interGrades.forEach((grade) => (sum += grade.quantity));
     } else if (tripPoints[i].nature == NatureEnum.school) {
-      if (!(i + 1 < indice + 1)) {
-        return sum;
-      } else {
-        sum = 0;
+      if (i + 1 < indice + 1) {
+        console.log("in if");
+        let test = 0;
+        grades.forEach((grade) => {
+          // ! Fix no entry here
+          if (GradeUtils.getSchoolId(grade.gradeId) == tripPoints[i].id) {
+            test += grade.quantity;
+          }
+        });
+        console.log("valeur soustraite =>", test);
+        sum -= test;
       }
     }
   }
