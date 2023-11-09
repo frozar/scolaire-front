e0ed590bb8ba34a1ecc08283cb811c170e7b665c
lucas <lucas.lvn97439@gmail.com>
2023-11-09 15:23:23 +0400

    clean

--- src/views/content/calendar/template/Calendar.tsx
+++ src/views/content/calendar/template/Calendar.tsx
@@ -118,7 +118,6 @@ export default function () {
     setCurrentMonth(new Date(today.getFullYear(), today.getMonth()));
     setCalendars(await CalendarService.getAll());
     setCalendarsPeriod(await CalendarService.getAllCalendarPeriod());
-
     disableSpinningWheel();
   });
 
