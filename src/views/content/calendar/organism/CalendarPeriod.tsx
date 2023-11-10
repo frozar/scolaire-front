06db1437b04a920030875e6906b11b227ca27738
lucas <lucas.lvn97439@gmail.com>
2023-11-10 11:23:16 +0400

    atomise & refactor code of CalendarPeriod

--- src/views/content/calendar/organism/CalendarPeriod.tsx
+++ src/views/content/calendar/organism/CalendarPeriod.tsx
@@ -1,54 +1,25 @@
-import { For, Show } from "solid-js";
 import { CalendarPeriodType } from "../../../../_entities/calendar.entity";
-import CellItem from "../atom/CellItem";
-import { CalendarUtils } from "../calendar.utils";
-import { onCalendarsPeriod } from "../template/Calendar";
+import { CalendarSectionTitle } from "../atom/CalendarSectionTitle";
+import { CalendarMonthsDetails } from "../molecule/CalendarMonthsDetails";
 import { CalendarHeader } from "./CalendarHeader";
-
+import "./CalendarPeriod.css";
 interface SchoolCalendarProps {
   date: Date;
+  calendarPeriod: CalendarPeriodType;
 }
 
 export function CalendarPeriod(props: SchoolCalendarProps) {
   return (
-    <section class="mt-10">
-      <p class="text-2xl">Calendrier scolaire</p>
+    <section class="calendar-period">
+      <CalendarSectionTitle title="Calendrier scolaire" />
       <CalendarHeader month={props.date} />
 
-      <Show when={onCalendarsPeriod() != undefined}>
-        <div class="calendar-cells ml-[100px]">
-          <For each={[0, 1, 2]}>
-            {(index) => (
-              <div class="calendar-month-details">
-                <For
-                  each={CalendarUtils.getDaysOfMonth(
-                    new Date(
-                      props.date.getFullYear(),
-                      props.date.getMonth() + index
-                    )
-                  )}
-                >
-                  {(date) => (
-                    <CellItem
-                      isActive={false}
-                      isWeekend={CalendarUtils.isWeekend(date)}
-                      onClick={() => {
-                        console.log(date);
-                      }}
-                      outPeriod={
-                        !CalendarUtils.dayIsInPeriod(
-                          date,
-                          onCalendarsPeriod() as CalendarPeriodType
-                        )
-                      }
-                    />
-                  )}
-                </For>
-              </div>
-            )}
-          </For>
-        </div>
-      </Show>
+      <div class="calendar-period-calendar">
+        <CalendarMonthsDetails
+          month={props.date}
+          calendarPeriod={props.calendarPeriod}
+        />
+      </div>
     </section>
   );
 }
