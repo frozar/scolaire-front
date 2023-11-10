a8de4430d1bf70eb1723503746d775a617878d59
lucas <lucas.lvn97439@gmail.com>
2023-11-10 10:41:48 +0400

    fix Calendar storybook import

--- src/views/content/calendar/template/Calendar.stories.tsx
+++ src/views/content/calendar/template/Calendar.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "storybook-solidjs";
-import CalendarLayout from "./Calendar";
+import { Calendar as CalendarLayout } from "./Calendar";
 
 const meta = {
   component: CalendarLayout,
