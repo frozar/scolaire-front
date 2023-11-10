fcbc96ee327bc480f480bc075d31ce07c6538228
Quentin PAYET <quentinpayet1203@gmail.com>
2023-11-10 10:16:08 +0400

    wip

--- src/_services/osrm.service.ts
+++ src/_services/osrm.service.ts
@@ -1,11 +1,12 @@
 import L from "leaflet";
-import _ from "lodash";
 import {
   TripMetricType,
   TripPointType,
   TripType,
 } from "../_entities/trip.entity";
 import { WaypointType } from "../_entities/waypoint.entity";
+import { NatureEnum } from "../type";
+import { getSchools } from "../views/content/map/component/organism/SchoolPoints";
 import { ServiceUtils } from "./_utils.service";
 
 const osrm = import.meta.env.VITE_API_OSRM_URL;
@@ -118,20 +119,80 @@ function getMetrics(
   return { distance, duration, deviation, kmPassager, txRemplissMoy };
 }
 
+// // TODO: Update to adapt to the case: There is a school not at the end of the trip
+// function getKmPassagers(
+//   response: osrmResponseType,
+//   points: TripPointType[],
+//   distance: number
+// ) {
+//   let kmPassager = 0;
+//   // ! Déterminer distance restante par destination
+//   let distance_restante = distance;
+
+//   // ! Multiplier la bonne quantité par la bonne distance restante
+//   // ! Mettre à jour les distances restantes
+//   response.routes[0].legs.map((elem, k) => {
+//     const quantity = _.sum(points.at(k)?.grades.map((grade) => grade.quantity));
+
+//     kmPassager += (quantity ?? 0) * (distance_restante ?? 0);
+//     distance_restante -= elem.distance;
+//   });
+
+//   kmPassager = kmPassager / 1000;
+//   return kmPassager;
+// }
+
 // TODO: Update to adapt to the case: There is a school not at the end of the trip
 function getKmPassagers(
   response: osrmResponseType,
-  points: TripPointType[],
+  tripPoints: TripPointType[],
   distance: number
 ) {
   let kmPassager = 0;
-  let distance_restante = distance;
-
+  // let distance_restante = distance;
+  // ! Déterminer distance restante par destination
+  const schoolIds = tripPoints
+    .filter((tripPoint) => tripPoint.nature == NatureEnum.school)
+    .map((tripPoint) => tripPoint.id);
+  const distances: { [schoolId: number]: number } = {};
+  let destinationStill = schoolIds;
   response.routes[0].legs.map((elem, k) => {
-    const quantity = _.sum(points.at(k)?.grades.map((grade) => grade.quantity));
+    console.log("k", tripPoints.at(k));
+    // ! Rewrite
+    if (k == 0) {
+      destinationStill.forEach(
+        (destination) => (distances[destination] = elem.distance)
+      );
+    } else {
+      destinationStill.forEach(
+        (destination) => (distances[destination] += elem.distance)
+      );
+    }
+    if (tripPoints.at(k + 1)?.nature == NatureEnum.school) {
+      destinationStill = destinationStill.filter(
+        (destination) => destination != tripPoints.at(k + 1)?.id
+      );
+    }
+  });
+  console.log("distances", distances);
+  // ! Multiplier la bonne quantité par la bonne distance restante
+  // ! Mettre à jour les distances restantes
+  // response.routes[0].legs.map((elem, k) => {
+  //   const quantity = _.sum(
+  //     tripPoints.at(k)?.grades.map((grade) => grade.quantity)
+  //   );
 
-    kmPassager += (quantity ?? 0) * (distance_restante ?? 0);
-    distance_restante -= elem.distance;
+  //   kmPassager += (quantity ?? 0) * (distance_restante ?? 0);
+  //   distance_restante -= elem.distance;
+  // });
+  // TODO: Move ?
+  function getSchoolId(gradeId: number) {
+    return getSchools().filter((school) =>
+      school.grades.some((grade) => grade.id == gradeId)
+    )[0].id;
+  }
+  response.routes[0].legs.map((elem, k) => {
+    const grades = tripPoints.at(k)?.grades;
   });
 
   kmPassager = kmPassager / 1000;
