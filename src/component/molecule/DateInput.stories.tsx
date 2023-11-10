6df2584449bed546d17a403b037f61fca4d86a92
lucas <lucas.lvn97439@gmail.com>
2023-11-10 16:58:58 +0400

    create storybook of DateInput component

---
+++ src/component/molecule/DateInput.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from "storybook-solidjs";
+import { DateInput as DateInputComponent } from "./DateInput";
+
+const meta = {
+  component: DateInputComponent,
+  tags: ["autodocs"],
+} satisfies Meta<typeof DateInputComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const currentDate = new Date(Date.now());
+
+export const DateInput: Story = {
+  render: (props: {
+    onChange: (date: Date) => void;
+    label: string;
+    minDate: Date;
+    maxDate: Date;
+    defaultValue: Date;
+  }) => {
+    return <DateInputComponent {...props} />;
+  },
+
+  args: {
+    label: "Selecteur de date",
+    maxDate: new Date(currentDate.getFullYear(), currentDate.getMonth() + 1),
+    minDate: new Date(currentDate.getFullYear(), currentDate.getMonth() - 1),
+    defaultValue: new Date(Date.now()),
+  },
+};
