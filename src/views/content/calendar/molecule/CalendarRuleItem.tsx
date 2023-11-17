111e5956fced3a4e93fbed72f2ecef6a540a2b6a
lucas <lucas.lvn97439@gmail.com>
2023-11-17 09:31:09 +0400

    atomise rules radios button group to molecule

--- src/views/content/calendar/molecule/CalendarRuleItem.tsx
+++ src/views/content/calendar/molecule/CalendarRuleItem.tsx
@@ -1,15 +1,11 @@
-import { createSignal, onMount } from "solid-js";
 import {
   CalendarDayEnum,
   CalendarType,
 } from "../../../../_entities/calendar.entity";
-import {
-  TripDirectionEntity,
-  TripDirectionEnum,
-} from "../../../../_entities/trip-direction.entity";
 import { LabeledCheckbox } from "../../../../component/molecule/LabeledCheckbox";
 import { CalendarManager } from "../calendar.manager";
 import { CalendarUtils } from "../calendar.utils";
+import { RuleItemRadioBtnGroup } from "./RuleItemRadioBtnGroup";
 
 interface CalendarRuleItemProps {
   calendar: CalendarType;
@@ -17,35 +13,8 @@ interface CalendarRuleItemProps {
 }
 
 export function CalendarRuleItem(props: CalendarRuleItemProps) {
-  const [direction, setDirection] = createSignal<TripDirectionEnum>();
-
-  onMount(() => {
-    const index = props.calendar.rules.findIndex(
-      (item) => item.day == props.day
-    );
-    const rule = props.calendar.rules[index];
-
-    if (rule && !!rule.tripTypeId) {
-      const tripDirection = TripDirectionEntity.findTripById(rule.tripTypeId);
-      console.log(tripDirection);
-
-      if (!tripDirection) return;
-      setDirection(tripDirection);
-      console.log(direction() == TripDirectionEnum.coming);
-    }
-  });
-
   const isDayChecked = () =>
     CalendarUtils.isDayInRules(props.day as CalendarDayEnum, props.calendar);
-
-  function onChangeDirectionTrip(event: Event & { target: HTMLInputElement }) {
-    const directionValue = event.target.value as TripDirectionEnum;
-    CalendarManager.updateTripDirection(
-      props.calendar,
-      props.day,
-      directionValue
-    );
-  }
 
   return (
     <div class="calendar-rule-item">
@@ -57,35 +26,11 @@ export function CalendarRuleItem(props: CalendarRuleItemProps) {
           CalendarManager.updateCalendarRules(props.day as CalendarDayEnum);
         }}
       />
-      <fieldset class="flex flex-col justify-end align-bottom">
-        <input
-          name={"trip-type-" + props.day}
-          class="my-[5px]"
-          type="radio"
-          disabled={!isDayChecked()}
-          onChange={onChangeDirectionTrip}
-          value="roundTrip"
-          checked={direction() == TripDirectionEnum.roundTrip}
-        />
-        <input
-          name={"trip-type-" + props.day}
-          class="my-[5px]"
-          type="radio"
-          disabled={!isDayChecked()}
-          onChange={onChangeDirectionTrip}
-          value="going"
-          checked={direction() == TripDirectionEnum.going}
-        />
-        <input
-          name={"trip-type-" + props.day}
-          class="my-[5px]"
-          type="radio"
-          disabled={!isDayChecked()}
-          onChange={onChangeDirectionTrip}
-          value="coming"
-          checked={direction() == TripDirectionEnum.coming}
-        />
-      </fieldset>
+      <RuleItemRadioBtnGroup
+        calendar={props.calendar}
+        day={props.day}
+        isDayChecked={isDayChecked()}
+      />
     </div>
   );
 }
---
+++ src/views/content/calendar/molecule/RuleItemRadioBtnGroup.tsx
@@ -0,0 +1,77 @@
+import { createSignal, onMount } from "solid-js";
+import {
+  CalendarDayEnum,
+  CalendarType,
+} from "../../../../_entities/calendar.entity";
+import {
+  TripDirectionEntity,
+  TripDirectionEnum,
+} from "../../../../_entities/trip-direction.entity";
+import { CalendarManager } from "../calendar.manager";
+
+interface RuleItemRadioBtnGroupProps {
+  calendar: CalendarType;
+  day: CalendarDayEnum;
+  isDayChecked: boolean;
+}
+
+export function RuleItemRadioBtnGroup(props: RuleItemRadioBtnGroupProps) {
+  const [direction, setDirection] = createSignal<TripDirectionEnum>();
+
+  onMount(() => {
+    const index = props.calendar.rules.findIndex(
+      (item) => item.day == props.day
+    );
+    const rule = props.calendar.rules[index];
+
+    if (rule && !!rule.tripTypeId) {
+      const tripDirection = TripDirectionEntity.findTripById(rule.tripTypeId);
+      console.log(tripDirection);
+
+      if (!tripDirection) return;
+      setDirection(tripDirection);
+      console.log(direction() == TripDirectionEnum.coming);
+    }
+  });
+
+  function onChangeDirectionTrip(event: Event & { target: HTMLInputElement }) {
+    const directionValue = event.target.value as TripDirectionEnum;
+    CalendarManager.updateTripDirection(
+      props.calendar,
+      props.day,
+      directionValue
+    );
+  }
+
+  return (
+    <fieldset class="flex flex-col justify-end align-bottom">
+      <input
+        name={"trip-type-" + props.day}
+        class="my-[5px]"
+        type="radio"
+        disabled={!props.isDayChecked}
+        onChange={onChangeDirectionTrip}
+        value="roundTrip"
+        checked={direction() == TripDirectionEnum.roundTrip}
+      />
+      <input
+        name={"trip-type-" + props.day}
+        class="my-[5px]"
+        type="radio"
+        disabled={!props.isDayChecked}
+        onChange={onChangeDirectionTrip}
+        value="going"
+        checked={direction() == TripDirectionEnum.going}
+      />
+      <input
+        name={"trip-type-" + props.day}
+        class="my-[5px]"
+        type="radio"
+        disabled={!props.isDayChecked}
+        onChange={onChangeDirectionTrip}
+        value="coming"
+        checked={direction() == TripDirectionEnum.coming}
+      />
+    </fieldset>
+  );
+}
