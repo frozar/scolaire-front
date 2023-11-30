44ea3460e7afbc316bfa998176f71679a208e284
lucas <lucas.lvn97439@gmail.com>
2023-11-30 15:58:51 +0400

    ortho correction

--- src/views/content/schools/component/organism/GradeTimesScheduleWrapper.tsx
+++ src/views/content/schools/component/organism/GradeTimesScheduleWrapper.tsx
@@ -90,7 +90,7 @@ export function GradeTimesScheduleWrapper(): JSXElement {
   return (
     <div>
       <LabeledCheckbox
-        label="Utilisé les horaires école"
+        label="Utiliser les horaires de école"
         checked={useSchoolSchedule()}
         onChange={onChangeUseSchoolSchedule}
       />
