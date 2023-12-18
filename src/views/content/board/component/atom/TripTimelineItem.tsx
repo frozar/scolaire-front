fad84ef0c82f308f4d3391b4aa11f675b9c8ce40
lucas <lucas.lvn97439@gmail.com>
2023-12-18 11:48:48 +0400

    add molecule wrapper for the trip timeline item

--- src/views/content/board/component/atom/TripTimelineItem.tsx
+++ src/views/content/board/component/atom/TripTimelineItem.tsx
@@ -1,17 +1,8 @@
-import { Setter, Show, createEffect, createSignal, on } from "solid-js";
+import { Show, createEffect } from "solid-js";
 
-import { range } from "lodash";
-import { GradeEntity } from "../../../../../_entities/grade.entity";
-import { TripPointType, TripType } from "../../../../../_entities/trip.entity";
 import { NatureEnum } from "../../../../../type";
-import { TripUtils } from "../../../../../utils/trip.utils";
 import { TripTimelineRemovePointButton } from "./TripTimelineRemovePointButton";
 
-import {
-  TripDirectionEntity,
-  TripDirectionEnum,
-} from "../../../../../_entities/trip-direction.entity";
-import { QuantityUtils } from "../../../../../utils/quantity.utils";
 import {
   DrawTripStep,
   currentStep,
@@ -20,144 +11,32 @@ import {
 } from "../organism/DrawTripBoard";
 import "./TripTimelineItem.css";
 
-export function TripTimelineItem(props: {
-  trip: TripType;
-  setTrip?: Setter<TripType>;
-  tripPoint: TripPointType;
-  indice: number;
-}) {
-  const [quantity, setQuantity] = createSignal<number>(0);
-  const trippoints = () => props.trip.tripPoints;
+interface TripTimelineItemProps {
+  pointName: string;
+  pointNature: NatureEnum;
+  quantityToGetOrDrop: string;
+  calculatedQuantity: number;
+  timePassage: string;
+  tripColor: string;
+  onClickRemovePointFromTrip: () => void;
+}
 
-  createEffect(
-    on(trippoints, () => {
-      getQuantity();
-    })
-  );
-
-  const tripDirection = () =>
-    TripDirectionEntity.FindDirectionById(props.trip.tripDirectionId);
-
+export function TripTimelineItem(props: TripTimelineItemProps) {
   const pointColor =
     // eslint-disable-next-line solid/reactivity
-    props.tripPoint.nature == NatureEnum.stop
-      ? " !bg-dark-teal"
-      : " !bg-red-base";
+    props.pointNature == NatureEnum.stop ? " !bg-dark-teal" : " !bg-red-base";
 
-  createEffect(() => {
-    setDividerColor(props.trip.color);
-  });
-
-  const timePassage = (): string => {
-    if (props.indice == 0 || !props.tripPoint.passageTime)
-      return GradeEntity.getStringFromHourFormat(props.trip.startTime);
-
-    const firstHour: string = GradeEntity.getStringFromHourFormat(
-      props.trip.startTime
-    );
-
-    let seconds = 0;
-    for (const i in range(props.indice + 1)) {
-      seconds += props.trip.tripPoints[i].passageTime ?? 0;
-    }
-    const hourMinute = TripUtils.convertSecondesToHourMinute(seconds);
-    return TripUtils.addHourTogether(firstHour, hourMinute);
-  };
-
-  function getQuantity() {
-    const append = tripDirection().type == TripDirectionEnum.going;
-    if (append) {
-      setQuantity(
-        QuantityUtils.SumQuantity(props.trip.tripPoints, props.indice)
-      );
-    } else {
-      setQuantity(
-        QuantityUtils.DropQuantity(
-          props.trip.tripPoints,
-          props.indice,
-          setQuantity
-        )
-      );
-    }
-  }
-
-  function getToAppendQuantity() {
-    if (props.tripPoint.nature === NatureEnum.stop)
-      return (
-        "+ " +
-        props.tripPoint.grades
-          .map((grade) => grade.quantity)
-          .reduce((total, quantity) => total + quantity, 0)
-      );
-    else
-      return (
-        QuantityUtils.SumQuantity(props.trip.tripPoints, props.indice - 1) * -1
-      );
-  }
-
-  function getTotalDropped(points: TripPointType[], indice: number) {
-    let total = 0;
-    points.every((item, index) => {
-      if (index < indice) {
-        total += item.grades.reduce(
-          (total, grade) => total + grade.quantity,
-          0
-        );
-        return true;
-      } else return false;
-    });
-    return total;
-  }
-
-  function getToDropQuantity() {
-    const points = TripUtils.getSplitedTripPointsAtFirstSchoolPosition(
-      props.trip.tripPoints
-    );
-    const tripTotalQuantity = QuantityUtils.getTripTotalQuantities(points);
-    const indice = points.findIndex(
-      (item) => item.id == props.trip.tripPoints[props.indice].id
-    );
-    let bufferTotal = tripTotalQuantity - getTotalDropped(points, indice);
-
-    if (props.tripPoint.nature === NatureEnum.stop) {
-      const result = bufferTotal - quantity();
-      bufferTotal -= result;
-      return "- " + result;
-    } else return "+ " + tripTotalQuantity;
-  }
-
-  // * The left side of timeline
-  function getToCalculQuantity() {
-    const appendQuantity = tripDirection().type == TripDirectionEnum.going;
-    if (appendQuantity) {
-      return getToAppendQuantity();
-    } else {
-      if (
-        !QuantityUtils.haveSchoolBefore(props.trip.tripPoints, props.indice)
-      ) {
-        if (props.tripPoint.nature == NatureEnum.school) {
-          const slicedTripPoint =
-            TripUtils.getSplitedTripPointsAtFirstSchoolPosition(
-              props.trip.tripPoints
-            );
-          return "+" + QuantityUtils.getTripTotalQuantities(slicedTripPoint);
-        }
-        return "--";
-      }
-
-      return getToDropQuantity();
-    }
-  }
+  createEffect(() => setDividerColor(props.tripColor));
 
   return (
     <div class="v-timeline-item">
       <div class="v-timeline-item__body">
         <div class="d-flex">
           <div class="first-items">
-            <div class="me-4">{getToCalculQuantity()}</div>
-            <p class="resource-name">{props.tripPoint.name}</p>
+            <div class="me-4">{props.quantityToGetOrDrop}</div>
+            <p class="resource-name">{props.pointName}</p>
           </div>
-          <div class="ms-4">{quantity()}</div>
+          <div class="ms-4">{props.calculatedQuantity}</div>
         </div>
       </div>
 
@@ -171,12 +50,9 @@ export function TripTimelineItem(props: {
         >
           <div class={"v-timeline-divider__inner-dot " + pointColor}>
             <i class="" aria-hidden="true" />
-            {/* TODO merge */}
-            <Show when={props.setTrip}>
+            <Show when={currentStep() == DrawTripStep.editTrip}>
               <TripTimelineRemovePointButton
-                indice={props.indice}
-                setTrip={props.setTrip}
-                trip={props.trip}
+                onClick={props.onClickRemovePointFromTrip}
               />
             </Show>
           </div>
@@ -190,7 +66,7 @@ export function TripTimelineItem(props: {
           displayTripMode() == displayTripModeEnum.onRoad
         }
       >
-        <p class="time-passage">{timePassage()}</p>
+        <p class="time-passage">{props.timePassage}</p>
       </Show>
     </div>
   );
---
+++ src/views/content/board/component/molecule/TripTimelineItemWrapper.tsx
@@ -0,0 +1,180 @@
+import { range } from "lodash";
+import { Setter, createEffect, createSignal, on } from "solid-js";
+import { GradeEntity } from "../../../../../_entities/grade.entity";
+import {
+  TripDirectionEntity,
+  TripDirectionEnum,
+} from "../../../../../_entities/trip-direction.entity";
+import { TripPointType, TripType } from "../../../../../_entities/trip.entity";
+import { WaypointEntity } from "../../../../../_entities/waypoint.entity";
+import { NatureEnum } from "../../../../../type";
+import { CurrentDrawTripUtils } from "../../../../../utils/currentDrawTrip.utils";
+import { QuantityUtils } from "../../../../../utils/quantity.utils";
+import { TripUtils } from "../../../../../utils/trip.utils";
+import { linkMap } from "../../../map/component/organism/Points";
+import { COLOR_SCHOOL_FOCUS, COLOR_STOP_FOCUS } from "../../../map/constant";
+import { TripTimelineItem } from "../atom/TripTimelineItem";
+
+interface TripTimelineItemWrapperProps {
+  setTrip?: Setter<TripType>;
+  tripPoint: TripPointType;
+  trip: TripType;
+  indice: number;
+}
+
+export function TripTimelineItemWrapper(props: TripTimelineItemWrapperProps) {
+  const [quantity, setQuantity] = createSignal<number>(0);
+  const trippoints = () => props.trip.tripPoints;
+
+  createEffect(on(trippoints, () => getQuantity()));
+
+  const tripDirection = () =>
+    TripDirectionEntity.FindDirectionById(props.trip.tripDirectionId);
+
+  function timePassage(): string {
+    if (props.indice == 0 || !props.tripPoint.passageTime)
+      return GradeEntity.getStringFromHourFormat(props.trip.startTime);
+
+    const firstHour: string = GradeEntity.getStringFromHourFormat(
+      props.trip.startTime
+    );
+
+    let seconds = 0;
+    for (const i in range(props.indice + 1)) {
+      seconds += props.trip.tripPoints[i].passageTime ?? 0;
+    }
+    const hourMinute = TripUtils.convertSecondesToHourMinute(seconds);
+    return TripUtils.addHourTogether(firstHour, hourMinute);
+  }
+
+  function getQuantity() {
+    const append = tripDirection().type == TripDirectionEnum.going;
+    if (append) {
+      setQuantity(
+        QuantityUtils.SumQuantity(props.trip.tripPoints, props.indice)
+      );
+    } else {
+      setQuantity(
+        QuantityUtils.DropQuantity(
+          props.trip.tripPoints,
+          props.indice,
+          setQuantity
+        )
+      );
+    }
+  }
+
+  function getToAppendQuantity() {
+    if (props.tripPoint.nature === NatureEnum.stop)
+      return (
+        "+ " +
+        props.tripPoint.grades
+          .map((grade) => grade.quantity)
+          .reduce((total, quantity) => total + quantity, 0)
+      );
+    else
+      return (
+        QuantityUtils.SumQuantity(props.trip.tripPoints, props.indice - 1) * -1
+      );
+  }
+
+  function getTotalDropped(points: TripPointType[], indice: number) {
+    let total = 0;
+    points.every((item, index) => {
+      if (index < indice) {
+        total += item.grades.reduce(
+          (total, grade) => total + grade.quantity,
+          0
+        );
+        return true;
+      } else return false;
+    });
+    return total;
+  }
+
+  function getToDropQuantity() {
+    const points = TripUtils.getSplitedTripPointsAtFirstSchoolPosition(
+      props.trip.tripPoints
+    );
+    const tripTotalQuantity = QuantityUtils.getTripTotalQuantities(points);
+    const indice = points.findIndex(
+      (item) => item.id == props.trip.tripPoints[props.indice].id
+    );
+    let bufferTotal = tripTotalQuantity - getTotalDropped(points, indice);
+
+    if (props.tripPoint.nature === NatureEnum.stop) {
+      const result = bufferTotal - quantity();
+      bufferTotal -= result;
+      return "- " + result;
+    } else return "+ " + tripTotalQuantity;
+  }
+
+  // * The left side of timeline
+  function getToCalculQuantity(): string {
+    const appendQuantity = tripDirection().type == TripDirectionEnum.going;
+    if (appendQuantity) {
+      return String(getToAppendQuantity());
+    } else {
+      if (
+        !QuantityUtils.haveSchoolBefore(props.trip.tripPoints, props.indice)
+      ) {
+        if (props.tripPoint.nature == NatureEnum.school) {
+          const slicedTripPoint =
+            TripUtils.getSplitedTripPointsAtFirstSchoolPosition(
+              props.trip.tripPoints
+            );
+          return "+" + QuantityUtils.getTripTotalQuantities(slicedTripPoint);
+        }
+        return "--";
+      }
+
+      return String(getToDropQuantity());
+    }
+  }
+
+  function deletePoint(indice: number) {
+    if (props.setTrip) {
+      const points = [...props.trip.tripPoints];
+      const pointId = points[indice].id;
+      const pointNature = points[indice].nature;
+
+      // TODO pas de l'ordre de la timeline !!
+      const circle = linkMap.get(props.trip.tripPoints[indice].leafletId);
+      pointNature == NatureEnum.stop
+        ? circle?.setStyle({ fillColor: COLOR_STOP_FOCUS })
+        : circle?.setStyle({ fillColor: COLOR_SCHOOL_FOCUS });
+
+      points.splice(indice, 1);
+
+      props.setTrip((trip) => {
+        return { ...trip, points };
+      });
+
+      // Update waypoints array
+      const waypoints = props.trip.waypoints;
+      if (waypoints) {
+        let newWaypoints = [...waypoints];
+
+        newWaypoints = WaypointEntity.deleteSchoolOrStopWaypoint(
+          waypoints,
+          pointId,
+          pointNature
+        );
+
+        CurrentDrawTripUtils.updateWaypoints(newWaypoints);
+        CurrentDrawTripUtils.removeTripPoint(pointId, pointNature);
+      }
+    }
+  }
+  return (
+    <TripTimelineItem
+      pointNature={props.tripPoint.nature}
+      pointName={props.tripPoint.name}
+      tripColor={props.trip.color}
+      timePassage={timePassage()}
+      calculatedQuantity={quantity()}
+      quantityToGetOrDrop={getToCalculQuantity()}
+      onClickRemovePointFromTrip={() => deletePoint(props.indice)}
+    />
+  );
+}
