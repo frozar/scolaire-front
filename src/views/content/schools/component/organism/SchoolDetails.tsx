35e463f15e6bd48447abb0cc3ed8466051e3d327
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-12 11:07:45 +0400

    cleaning => SchoolDetailsContent.tsx added

--- src/views/content/schools/component/organism/SchoolDetails.tsx
+++ src/views/content/schools/component/organism/SchoolDetails.tsx
@@ -1,19 +1,12 @@
 import { Show, createSignal, onCleanup, onMount } from "solid-js";
 import { SchoolType } from "../../../../../_entities/school.entity";
-import { LabeledInputSelect } from "../../../../../component/molecule/LabeledInputSelect";
 import { MapElementUtils } from "../../../../../utils/mapElement.utils";
 import { SchoolDetailUtils } from "../../../../../utils/school-details.utils";
-import { SchoolUtils } from "../../../../../utils/school.utils";
 import BoardFooterActions from "../../../board/component/molecule/BoardFooterActions";
-import CollapsibleElement from "../../../board/component/organism/CollapsibleElement";
 import { changeBoard } from "../../../board/component/template/ContextManager";
-import { calendars } from "../../../calendar/template/Calendar";
 import SchoolDetailsHeader from "../molecule/SchoolDetailsHeader";
-import { HourRuleList } from "./HourRuleList";
+import { SchoolDetailsContent } from "./SchoolDetailsContent";
 import { SchoolDetailsPanels } from "./SchoolDetailsPanels";
-import { SchoolHoursSlots } from "./SchoolHoursSlots";
-
-import "./SchoolDetails.css";
 
 export const [schoolDetailsItem, setSchoolDetailsItem] =
   createSignal<SchoolType>();
@@ -28,42 +21,13 @@ export default function () {
     }
   });
 
-  function onChangeCalendarSelect(value: number | string) {
-    SchoolUtils.linkSchoolToCalendar(value as number);
-  }
-
   onCleanup(() => setSchoolDetailEditing(false));
   return (
     <section>
       <SchoolDetailsHeader school={schoolDetailsItem() as SchoolType} />
-      {/* TODO: Put following in an other file */}
-      <LabeledInputSelect
-        defaultOptions="Sélectionner calendrier"
-        defaultValue={schoolDetailsItem()?.calendar?.id ?? 0}
-        label="Calendrier lié"
-        onChange={onChangeCalendarSelect}
-        options={calendars().map((item) => {
-          return { value: item.id, text: item.name };
-        })}
-        disabled={!schoolDetailEditing()}
-        indented={true}
-      />
 
-      <div class="time">
-        <CollapsibleElement
-          title="Tranches horaires"
-          titleClass="text-xl"
-          closedByDefault={true}
-        >
-          <SchoolHoursSlots school={schoolDetailsItem() as SchoolType} />
+      <SchoolDetailsContent />
 
-          <HourRuleList
-            item={schoolDetailsItem}
-            setItem={setSchoolDetailsItem}
-            disabled={schoolDetailEditing()}
-          />
-        </CollapsibleElement>
-      </div>
       <Show
         when={!schoolDetailEditing()}
         fallback={
---
+++ src/views/content/schools/component/organism/SchoolDetailsContent.tsx
@@ -0,0 +1,50 @@
+import { SchoolType } from "../../../../../_entities/school.entity";
+import { LabeledInputSelect } from "../../../../../component/molecule/LabeledInputSelect";
+import { SchoolUtils } from "../../../../../utils/school.utils";
+import CollapsibleElement from "../../../board/component/organism/CollapsibleElement";
+import { calendars } from "../../../calendar/template/Calendar";
+import { HourRuleList } from "./HourRuleList";
+import {
+  schoolDetailEditing,
+  schoolDetailsItem,
+  setSchoolDetailsItem,
+} from "./SchoolDetails";
+import { SchoolHoursSlots } from "./SchoolHoursSlots";
+
+import "./SchoolDetailsContent.css";
+
+export function SchoolDetailsContent() {
+  function onChangeCalendarSelect(value: number | string) {
+    SchoolUtils.linkSchoolToCalendar(value as number);
+  }
+  return (
+    <>
+      <LabeledInputSelect
+        defaultOptions="Sélectionner calendrier"
+        defaultValue={schoolDetailsItem()?.calendar?.id ?? 0}
+        label="Calendrier lié"
+        onChange={onChangeCalendarSelect}
+        options={calendars().map((item) => {
+          return { value: item.id, text: item.name };
+        })}
+        disabled={!schoolDetailEditing()}
+        indented={true}
+      />
+      <div class="time">
+        <CollapsibleElement
+          title="Tranches horaires"
+          titleClass="text-xl"
+          closedByDefault={true}
+        >
+          <SchoolHoursSlots school={schoolDetailsItem() as SchoolType} />
+
+          <HourRuleList
+            item={schoolDetailsItem}
+            setItem={setSchoolDetailsItem}
+            disabled={schoolDetailEditing()}
+          />
+        </CollapsibleElement>
+      </div>
+    </>
+  );
+}
