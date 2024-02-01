54e8f7582d44e41f5ac3472c250d7f7f8291b49e
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-01 10:19:08 +0400

    wip: ServiceTemplate.tsx attempt to make the service cards draggable

--- src/views/content/service/template/ServiceTemplate.tsx
+++ src/views/content/service/template/ServiceTemplate.tsx
@@ -1,3 +1,8 @@
+import {
+  DragDropProvider,
+  DragDropSensors,
+  DragOverlay,
+} from "@thisbeyond/solid-dnd";
 import { JSXElement } from "solid-js";
 import InputSearch from "../../schools/component/molecule/InputSearch";
 import { ServiceLeftBoardContent } from "../organism/ServiceLeftBoardContent";
@@ -5,20 +10,34 @@ import "./ServiceTemplate.css";
 
 // TODO: Make components
 export function ServiceTemplate(): JSXElement {
+  function onDragOver({ draggable, droppable }) {
+    console.log("TODO");
+  }
+
+  function onDragEnd({ draggable, droppable }) {
+    console.log("TODO");
+  }
   return (
     <div>
-      <div id="service-left-board">
-        <div id="service-left-board-header">
-          <div id="service-left-board-header-title">
-            Liste des courses à assigner
+      <DragDropProvider onDragOver={onDragOver} onDragEnd={onDragEnd}>
+        <DragDropSensors />
+
+        <div id="service-left-board">
+          <div id="service-left-board-header">
+            <div id="service-left-board-header-title">
+              Liste des courses à assigner
+            </div>
+            {/* SearchBar */}
+            <div id="service-left-board-searchbar">
+              <InputSearch onInput={() => console.log("todo")} />
+            </div>
           </div>
-          {/* SearchBar */}
-          <div id="service-left-board-searchbar">
-            <InputSearch onInput={() => console.log("todo")} />
-          </div>
+          <ServiceLeftBoardContent />
         </div>
-        <ServiceLeftBoardContent />
-      </div>
+        <DragOverlay>
+          {(draggable) => <div class="sortable">{draggable.id}</div>}
+        </DragOverlay>
+      </DragDropProvider>
     </div>
   );
 }
