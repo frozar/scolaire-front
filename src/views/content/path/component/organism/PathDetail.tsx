3ae2e39f3d2cc23027c1caab433da50648fffd5d
lucas <lucas.lvn97439@gmail.com>
2024-02-02 09:20:39 +0400

    wip: timeline for path

--- src/views/content/path/component/organism/PathDetail.tsx
+++ src/views/content/path/component/organism/PathDetail.tsx
@@ -1,17 +1,23 @@
 import { createSignal } from "solid-js";
 import { PathEntity, PathType } from "../../../../../_entities/path.entity";
+import { PathUtil } from "../../../../../utils/path.utils";
+import SchoolsEnumeration from "../../../board/component/molecule/SchoolsEnumeration";
 import { PathDetailHeader } from "../molecule/PathDetailHeader";
+import { PathTimeLine } from "./PathLine";
 
 export const [selectedPath, setSelectedPath] = createSignal<PathType>();
 
 export function PathDetail() {
-  const path = () => selectedPath() ?? PathEntity.defaultPath();
+  const path = selectedPath() ?? PathEntity.defaultPath();
+  const schoolsNames = PathUtil.getSchoolsInPath(selectedPath())?.map(
+    (school) => school.name
+  );
 
   return (
     <div>
-      <PathDetailHeader path={path()} />
-      <p>TODO: School énumération</p>
-      <p>TODO: Timeline sans la notion de temps / quantité ?</p>
+      <PathDetailHeader path={path} />
+      <SchoolsEnumeration schoolsName={schoolsNames ?? []} />
+      <PathTimeLine path={path} />
     </div>
   );
 }
