8682f4525ea4236f531bb20c5f2b17375e4bf378
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-05 15:36:45 +0400

    wip: displaying hours

--- src/views/content/service/molecule/ServiceGridTop.tsx
+++ src/views/content/service/molecule/ServiceGridTop.tsx
@@ -7,6 +7,10 @@ interface ServiceGridTopProps {
 }
 
 export function ServiceGridTop(props: ServiceGridTopProps): JSXElement {
+  function gridWidth(): string {
+    const width = 1440 * zoom();
+    return width + "px";
+  }
   function hourWidth(): string {
     const width = (1440 * zoom()) / 24;
     return width + "px";
@@ -16,38 +20,63 @@ export function ServiceGridTop(props: ServiceGridTopProps): JSXElement {
     const width = (1440 * zoom()) / 24 / 6;
     return width + "px";
   }
+  const hourDisplayedWidth = 40;
+  function hourDisplayedMarginX(): string {
+    const value = (1440 * zoom() - 23 * hourDisplayedWidth) / 24;
+    return value + "px";
+  }
+  // TODO: Make components
   return (
-    <div id="service-grid-top" style={{ width: props.width }}>
-      <For each={[...Array(24).keys()]}>
-        {(i) => (
-          <div class="service-grid-top-hour" style={{ width: hourWidth() }}>
-            {/* {i} */}
-            <div class="flex flex-col">
-              <div
-                class="service-grid-top-minute-displayed"
-                style={{ width: hourWidth() }}
-              >
-                <For each={[...Array(5).keys()]}>
-                  {(i) => {
-                    const minuteToDisplay = i + 1;
-                    return <div>{minuteToDisplay + "0"}</div>;
-                  }}
-                </For>
-              </div>
-              <div class="flex">
-                <For each={[...Array(5).keys()]}>
-                  {() => (
-                    <div
-                      class="service-grid-top-minute-tick"
-                      style={{ width: minuteWidth() }}
-                    />
-                  )}
-                </For>
+    <>
+      <div
+        class="service-grid-list-hour-displayed"
+        style={{ width: gridWidth() }}
+      >
+        <For each={[...Array(23).keys()]}>
+          {(i) => (
+            <div
+              class="service-grid-hour-displayed"
+              style={{
+                width: hourDisplayedWidth + "px",
+                "margin-left": hourDisplayedMarginX(),
+              }}
+            >
+              {i + 1}
+            </div>
+          )}
+        </For>
+      </div>
+      <div id="service-grid-top" style={{ width: props.width }}>
+        <For each={[...Array(24).keys()]}>
+          {() => (
+            <div class="service-grid-top-hour" style={{ width: hourWidth() }}>
+              <div class="flex flex-col">
+                <div
+                  class="service-grid-top-minute-displayed"
+                  style={{ width: hourWidth() }}
+                >
+                  <For each={[...Array(5).keys()]}>
+                    {(i) => {
+                      const minuteToDisplay = i + 1;
+                      return <div>{minuteToDisplay + "0"}</div>;
+                    }}
+                  </For>
+                </div>
+                <div class="flex">
+                  <For each={[...Array(5).keys()]}>
+                    {() => (
+                      <div
+                        class="service-grid-top-minute-tick"
+                        style={{ width: minuteWidth() }}
+                      />
+                    )}
+                  </For>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-      </For>
-    </div>
+          )}
+        </For>
+      </div>
+    </>
   );
 }
