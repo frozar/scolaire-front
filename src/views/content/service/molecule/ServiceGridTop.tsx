1fef886443360f166266e77d29a69c187ff3f106
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-05 14:19:16 +0400

    service list width fixed

--- src/views/content/service/molecule/ServiceGridTop.tsx
+++ src/views/content/service/molecule/ServiceGridTop.tsx
@@ -1,4 +1,5 @@
 import { For, JSXElement } from "solid-js";
+import { zoom } from "../organism/ServiceGrid";
 import "./ServiceGridTop.css";
 
 interface ServiceGridTopProps {
@@ -6,11 +7,18 @@ interface ServiceGridTopProps {
 }
 
 export function ServiceGridTop(props: ServiceGridTopProps): JSXElement {
+  function getWidth(): string {
+    const width = (1440 * zoom()) / 24;
+    return width + "px";
+  }
   return (
-    <div id="service-grid-top">
+    <div id="service-grid-top" style={{ width: props.width }}>
       <For each={[...Array(24).keys()]}>
         {(i) => (
-          <div class="bg-slate-400 border border-black border-y-0 border-l-0 border-x">
+          <div
+            class="bg-slate-400 border border-black border-y-0 border-l-0 border-x"
+            style={{ width: getWidth() }}
+          >
             {i}
           </div>
         )}
