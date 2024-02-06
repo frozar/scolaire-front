7ad567b65b9b782ef1958850b3108d6873c26fee
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-06 17:00:56 +0400

    fns added to serviceGridUtils

--- src/utils/serviceGrid.utils.ts
+++ src/utils/serviceGrid.utils.ts
@@ -1,6 +1,11 @@
+import { GradeEntity } from "../_entities/grade.entity";
 import { zoom } from "../views/content/service/organism/ServiceGrid";
-import { services } from "../views/content/service/organism/Services";
+import {
+  ServiceTripType,
+  services,
+} from "../views/content/service/organism/Services";
 import { selectedService } from "../views/content/service/template/ServiceTemplate";
+import { TripUtils } from "./trip.utils";
 
 export namespace ServiceGridUtils {
   export function scrollToServiceStart(ref: HTMLDivElement): void {
@@ -9,5 +14,52 @@ export namespace ServiceGridUtils {
     )[0].serviceTrips[0].endHour;
 
     ref.scrollTo((endHour - 25) * zoom(), 0);
+  }
+
+  export function getTripWidth(tripId: number): string {
+    return (
+      Math.round(
+        ((TripUtils.get(tripId).metrics?.duration as number) / 60) * zoom()
+      ) + "px"
+    );
+  }
+
+  export function getHlpWidth(): string {
+    // TODO: Use real value
+    return String(5 * zoom()) + "px";
+  }
+
+  export function getServiceTripStartHour(
+    i: number,
+    serviceTrip: ServiceTripType
+  ): string {
+    if (i == 0) {
+      const endHour = serviceTrip.endHour;
+
+      const duration = Math.round(
+        (TripUtils.get(serviceTrip.tripId).metrics?.duration as number) / 60
+      );
+
+      const startHour = endHour - duration;
+
+      const hour = Math.trunc(startHour / 60);
+      const minutes = startHour % 60;
+
+      return GradeEntity.getStringFromHourFormat({ hour, minutes });
+      // TODO
+    } else return "--:--";
+  }
+
+  export function getServiceEndHour(
+    i: number,
+    serviceTrip: ServiceTripType
+  ): string {
+    if (i == 0) {
+      const hour = Math.round(serviceTrip.endHour / 60);
+      const minutes = (serviceTrip.endHour % 60) * 60;
+
+      return GradeEntity.getStringFromHourFormat({ hour, minutes });
+      // TODO
+    } else return "--:--";
   }
 }
--- src/views/content/service/molecule/ServiceGridItem.tsx
+++ src/views/content/service/molecule/ServiceGridItem.tsx
@@ -1,8 +1,7 @@
 import { JSXElement } from "solid-js";
 import { TripUtils } from "../../../../utils/trip.utils";
 
-import { GradeEntity } from "../../../../_entities/grade.entity";
-import { zoom } from "../organism/ServiceGrid";
+import { ServiceGridUtils } from "../../../../utils/serviceGrid.utils";
 import { ServiceTripType } from "../organism/Services";
 import "./ServiceGridItem.css";
 
@@ -12,66 +11,29 @@ interface ServiceGridItemProps {
 }
 
 export function ServiceGridItem(props: ServiceGridItemProps): JSXElement {
-  function tripWidth(): string {
-    return (
-      Math.round(
-        ((TripUtils.get(props.serviceTrip.tripId).metrics?.duration as number) /
-          60) *
-          zoom()
-      ) + "px"
-    );
-  }
-
-  function hlpWidth(): string {
-    // ! Temporary value of 10 minutes used
-    return String(5 * zoom()) + "px";
-  }
-
-  // TODO: Put in utils
-  function startHour(): string {
-    if (props.i == 0) {
-      const endHour = props.serviceTrip.endHour;
-
-      const duration = Math.round(
-        (TripUtils.get(props.serviceTrip.tripId).metrics?.duration as number) /
-          60
-      );
-
-      const startHour = endHour - duration;
-
-      const hour = Math.trunc(startHour / 60);
-      const minutes = startHour % 60;
-
-      return GradeEntity.getStringFromHourFormat({ hour, minutes });
-      // TODO
-    } else return "--:--";
-  }
-  // TODO: Put in utils
-  function endHour(): string {
-    if (props.i == 0) {
-      const hour = Math.round(props.serviceTrip.endHour / 60);
-      const minutes = (props.serviceTrip.endHour % 60) * 60;
-
-      return GradeEntity.getStringFromHourFormat({ hour, minutes });
-      // TODO
-    } else return "--:--";
-  }
-
-  // TODO: Make itemTrip, itemHlp and itemHalt components
+  // TODO: Make itemTrip, itemHlp
   return (
     <div class="service-grid-item">
-      <div class="service-grid-item-trip" style={{ width: tripWidth() }}>
+      <div
+        class="service-grid-item-trip"
+        style={{
+          width: ServiceGridUtils.getTripWidth(props.serviceTrip.tripId),
+        }}
+      >
         <div class="service-grid-item-trip-name">
           {TripUtils.get(props.serviceTrip.tripId).name}
         </div>
         <div class="absolute text-xs -rotate-45 -bottom-6 -left-4">
-          {startHour()}
+          {ServiceGridUtils.getServiceTripStartHour(props.i, props.serviceTrip)}
         </div>
         <div class="absolute text-xs -rotate-45 -bottom-6 -right-4">
-          {endHour()}
+          {ServiceGridUtils.getServiceEndHour(props.i, props.serviceTrip)}
         </div>
       </div>
-      <div class="service-grid-item-hlp" style={{ width: hlpWidth() }} />
+      <div
+        class="service-grid-item-hlp"
+        style={{ width: ServiceGridUtils.getHlpWidth() }}
+      />
     </div>
   );
 }
