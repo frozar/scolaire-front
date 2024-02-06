83fb194c8300ce6fd9385df89c2097175977b3b9
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-06 14:03:06 +0400

    temp hlp value used and is adapting to zoom level

--- src/views/content/service/molecule/ServiceGridItem.tsx
+++ src/views/content/service/molecule/ServiceGridItem.tsx
@@ -5,7 +5,7 @@ import { zoom } from "../organism/ServiceGrid";
 import "./ServiceGridItem.css";
 
 export function ServiceGridItem(props: { tripId: number }): JSXElement {
-  function width() {
+  function tripWidth(): string {
     return (
       Math.round(
         ((TripUtils.get(props.tripId).metrics?.duration as number) / 60) *
@@ -14,18 +14,20 @@ export function ServiceGridItem(props: { tripId: number }): JSXElement {
     );
   }
 
+  function hlpWidth(): string {
+    // ! Temporary value of 10 minutes used
+    return String(5 * zoom()) + "px";
+  }
+
   // TODO: Make itemTrip, itemHlp and itemHalt components
   return (
     <div class="service-grid-item">
-      <div class="service-grid-item-trip" style={{ width: width() }}>
+      <div class="service-grid-item-trip" style={{ width: tripWidth() }}>
         <div class="service-grid-item-trip-name">
           {TripUtils.get(props.tripId).name}
         </div>
       </div>
-      <div class="service-grid-item-hlp" />
-      <div class="service-grid-item-halt">
-        <div class="service-grid-item-halt-element" />
-      </div>
+      <div class="service-grid-item-hlp" style={{ width: hlpWidth() }} />
     </div>
   );
 }
