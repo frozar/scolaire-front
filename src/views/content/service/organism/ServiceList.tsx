85f3ac10a69b78313c521370cdbfe813abd18d71
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-07 14:47:09 +0400

    ServiceListHeader.tsx added

--- src/views/content/service/organism/ServiceList.tsx
+++ src/views/content/service/organism/ServiceList.tsx
@@ -1,4 +1,5 @@
 import { For, JSXElement } from "solid-js";
+import { ServiceListHeader } from "../atom/ServiceListHeader";
 import { ServiceListAdd } from "../molecule/ServiceListAdd";
 import { ServiceListItem } from "../molecule/ServiceListItem";
 import "./ServiceList.css";
@@ -7,9 +8,7 @@ import { services } from "./Services";
 export function ServiceList(): JSXElement {
   return (
     <div id="service-list">
-      <div id="service-list-header">
-        <div id="service-list-header-title">Liste de services</div>
-      </div>
+      <ServiceListHeader />
 
       <For each={services()}>
         {(service) => <ServiceListItem service={service} />}
