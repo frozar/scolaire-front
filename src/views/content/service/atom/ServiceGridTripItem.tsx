610b9c9ac0432349b9c9a71b4801e5a413c5ec69
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-07 10:23:34 +0400

    wip: adding btn trashicon to remove ServiceTrip from a service

--- src/views/content/service/atom/ServiceGridTripItem.tsx
+++ src/views/content/service/atom/ServiceGridTripItem.tsx
@@ -1,7 +1,10 @@
-import { JSXElement } from "solid-js";
+import { JSXElement, Show } from "solid-js";
+import TrashIcon from "../../../../icons/TrashIcon";
 import { ServiceGridUtils } from "../../../../utils/serviceGrid.utils";
 import { TripUtils } from "../../../../utils/trip.utils";
-import { ServiceTripType } from "../organism/Services";
+import ButtonIcon from "../../board/component/molecule/ButtonIcon";
+import { ServiceTripType, services, setServices } from "../organism/Services";
+import { selectedService } from "../template/ServiceTemplate";
 import "./ServiceGridTripItem.css";
 
 interface ServiceGridTripItemProps {
@@ -12,6 +15,21 @@ interface ServiceGridTripItemProps {
 export function ServiceGridTripItem(
   props: ServiceGridTripItemProps
 ): JSXElement {
+  function removeTripFromService(tripId: number): void {
+    setServices((prev) => {
+      const services = [...prev];
+      const serviceToChange = services.filter(
+        (service) => service.id == selectedService()
+      )[0];
+      const index = services.indexOf(serviceToChange);
+      serviceToChange.serviceTrips = serviceToChange.serviceTrips.filter(
+        (serviceTrip) => serviceTrip.tripId != tripId
+      );
+
+      services.splice(index, 1, serviceToChange);
+      return services;
+    });
+  }
   return (
     <div
       class="service-grid-item-trip"
@@ -28,6 +46,13 @@ export function ServiceGridTripItem(
       <div class="absolute text-xs -rotate-45 -bottom-6 -right-4">
         {ServiceGridUtils.getServiceEndHour(props.i, props.serviceTrip)}
       </div>
+      <Show when={selectedService() == services()[props.i].id}>
+        <ButtonIcon
+          icon={<TrashIcon />}
+          onClick={() => removeTripFromService(props.serviceTrip.tripId)}
+          class="absolute bottom-0 right-0"
+        />
+      </Show>
     </div>
   );
 }
