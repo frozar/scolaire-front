00c565f465af83be4fa9e388d9a4f7a29b3c443b
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-07 14:25:02 +0400

    ServiceListItemTop.tsx and ServiceListItemBottom.tsx added

--- src/views/content/service/molecule/ServiceListItem.tsx
+++ src/views/content/service/molecule/ServiceListItem.tsx
@@ -1,14 +1,10 @@
 import { JSXElement } from "solid-js";
 import { ServiceType } from "../organism/Services";
 
-import { LockIconClose } from "../../../../icons/LockIconClosed";
-import { LockIconOpen } from "../../../../icons/LockIconOpen";
-import ButtonIcon from "../../board/component/molecule/ButtonIcon";
-import {
-  selectedService,
-  setSelectedService,
-} from "../template/ServiceTemplate";
+import { selectedService } from "../template/ServiceTemplate";
 import "./ServiceListItem.css";
+import { ServiceListItemBottom } from "./ServiceListItemBottom";
+import { ServiceListItemTop } from "./ServiceListItemTop";
 
 interface ServiceListItemProps {
   service: ServiceType;
@@ -19,34 +15,14 @@ export function ServiceListItem(props: ServiceListItemProps): JSXElement {
     return selectedService() == props.service.id;
   }
 
-  function displayIcon(): JSXElement {
-    return isSelected() ? <LockIconOpen /> : <LockIconClose />;
-  }
-
-  function onClick(): void {
-    if (selectedService() != props.service.id) {
-      setSelectedService(props.service.id);
-    } else {
-      setSelectedService();
-    }
-  }
   return (
     <div class="service-list-item" classList={{ active: isSelected() }}>
-      <div class="service-list-item-top">
-        <div
-          class="service-list-item-top-name"
-          classList={{ active: isSelected() }}
-        >
-          {props.service.name}
-        </div>
-        <ButtonIcon icon={displayIcon()} onClick={onClick} />
-      </div>
-      <div
-        class="service-list-item-bottom"
-        classList={{ active: isSelected() }}
-      >
-        {"Temps de trajet global : " + props.service.totalDuration}
-      </div>
+      <ServiceListItemTop isSelected={isSelected()} service={props.service} />
+
+      <ServiceListItemBottom
+        isSelected={isSelected()}
+        service={props.service}
+      />
     </div>
   );
 }
