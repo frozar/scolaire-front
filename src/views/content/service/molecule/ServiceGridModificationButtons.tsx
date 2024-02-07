9b26f9a830bf32f592cbbb44140f2d47d35d2c37
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-07 16:57:41 +0400

    ServiceGrid cancel modification buttons working

--- src/views/content/service/molecule/ServiceGridModificationButtons.tsx
+++ src/views/content/service/molecule/ServiceGridModificationButtons.tsx
@@ -1,18 +1,19 @@
 import { JSXElement } from "solid-js";
 import Button from "../../../../component/atom/Button";
+import { servicesBeforeModification, setServices } from "../organism/Services";
 
 export function ServiceGridModificationButtons(): JSXElement {
   return (
     <div>
-      <Button
-        onClick={() => console.log("TODO: Annuler modif")}
-        label="Annuler"
-        variant="danger"
-      />
+      <Button onClick={cancel} label="Annuler" variant="danger" />
       <Button
         onClick={() => console.log("TODO: Appliquer modifications")}
         label="Valider"
       />
     </div>
   );
+}
+
+function cancel() {
+  setServices([...servicesBeforeModification()]);
 }
