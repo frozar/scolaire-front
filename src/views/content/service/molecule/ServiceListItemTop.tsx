d6e3a9118d6cbe69b213542c8ee7cb602ca37fa0
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-14 11:27:20 +0400

    ServiceListItemNameInput.tsx added

--- src/views/content/service/molecule/ServiceListItemTop.tsx
+++ src/views/content/service/molecule/ServiceListItemTop.tsx
@@ -1,9 +1,8 @@
-import { JSXElement, Show } from "solid-js";
-import { TextInput } from "../../../../component/atom/TextInput";
+import { JSXElement } from "solid-js";
 import { LockIconClose } from "../../../../icons/LockIconClosed";
 import { LockIconOpen } from "../../../../icons/LockIconOpen";
-import { BusServiceUtils } from "../../../../utils/busService.utils";
 import ButtonIcon from "../../board/component/molecule/ButtonIcon";
+import { ServiceListItemNameInput } from "../atom/ServiceListItemNameInput";
 import { ServiceType } from "../organism/Services";
 import {
   selectedService,
@@ -21,24 +20,12 @@ export function ServiceListItemTop(props: ServiceListItemTopProps): JSXElement {
     return props.isSelected ? <LockIconOpen /> : <LockIconClose />;
   }
 
-  function onInputText(serviceName: string): void {
-    BusServiceUtils.updateServiceName(props.service.id, serviceName);
-  }
-
   return (
     <div class="service-list-item-top">
-      <Show
-        when={!props.isSelected}
-        fallback={
-          <TextInput
-            onInput={onInputText}
-            defaultValue={props.service.name}
-            class="service-list-item-top-name-input"
-          />
-        }
-      >
-        <div class="service-list-item-top-name">{props.service.name}</div>
-      </Show>
+      <ServiceListItemNameInput
+        service={props.service}
+        isSelected={props.isSelected}
+      />
 
       <ButtonIcon
         icon={displayIcon()}
