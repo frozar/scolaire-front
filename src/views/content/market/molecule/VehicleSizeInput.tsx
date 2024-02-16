85cf227cfd2c72bc2e74dbbeec36485a9e2583da
GryphusOneSeven <gael.edmond@epitech.eu>
2024-02-16 10:17:52 +0400

    wip vehicle size input

--- src/views/content/market/molecule/VehicleSizeInput.tsx
+++ src/views/content/market/molecule/VehicleSizeInput.tsx
@@ -1,3 +1,12 @@
-export function VehicleSizeInput() {
-  return <div />;
+interface VehicleSizeInputProps {
+  defaultLength: number;
+  defaultWidth: number;
+  defaultHeight: number;
+  onInputLenght: (value: number) => void;
+  onInputWidth: (value: number) => void;
+  onInputHeight: (value: number) => void;
+}
+
+export function VehicleSizeInput(props: VehicleSizeInputProps) {
+  return <div>{props.defaultHeight}</div>;
 }
