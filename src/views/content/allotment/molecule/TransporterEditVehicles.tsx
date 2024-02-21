c9144732cae9556c9628ce53e8cc57ef245f9b03
GryphusOneSeven <gael.edmond@epitech.eu>
2024-02-21 14:30:57 +0400

    fix

--- src/views/content/allotment/molecule/TransporterEditVehicles.tsx
+++ src/views/content/allotment/molecule/TransporterEditVehicles.tsx
@@ -1,12 +1,27 @@
+import { For, createSignal } from "solid-js";
 import "./TransporterEditVehicles.css";
 import { TransporterEditVehiclesHeader } from "./TransporterEditVehiclesHeader";
 import { TransporterEditVehiclesInputs } from "./TransporterEditVehiclesInputs";
 
 export function TransporterEditVehicles() {
+  const [fakeData] = createSignal([
+    { licence: "a", type: "autocar" },
+    { licence: "b", type: "bus" },
+  ]);
+
   return (
     <div class="transporter-vehicles-content">
       <TransporterEditVehiclesHeader />
-      <TransporterEditVehiclesInputs />
+      <For each={fakeData()}>
+        {(item) => (
+          <TransporterEditVehiclesInputs
+            license={item.licence}
+            type={item.type}
+            onLicenseChange={() => console.log("license changed")}
+            onTypeChange={() => console.log("type changed")}
+          />
+        )}
+      </For>
     </div>
   );
 }
--- src/views/content/allotment/molecule/TransporterEditVehiclesInputs.tsx
+++ src/views/content/allotment/molecule/TransporterEditVehiclesInputs.tsx
@@ -2,15 +2,24 @@ import { LabeledInputSelect } from "../../../../component/molecule/LabeledInputS
 import LabeledInputField from "../../board/component/molecule/LabeledInputField";
 import "./TransporterEditVehicles.css";
 
-export function TransporterEditVehiclesInputs() {
+interface TransporterEditVehiclesInputsProps {
+  license: string;
+  type: string;
+  onLicenseChange: (license: string) => void;
+  onTypeChange: (type: string) => void;
+}
+
+export function TransporterEditVehiclesInputs(
+  props: TransporterEditVehiclesInputsProps
+) {
   return (
     <div class="transporter-vehicles-input-container">
       <div class="transporter-vehicles-input">
         <LabeledInputField
           label="Immatriculation"
           name="license"
-          value={""}
-          onInput={() => console.log("a")}
+          value={props.license}
+          onInput={(e) => props.onLicenseChange(e.target.value)}
           placeholder="Immatriculation"
         />
       </div>
@@ -18,7 +27,7 @@ export function TransporterEditVehiclesInputs() {
         <LabeledInputSelect
           defaultValue={0}
           label="Type de vehicule"
-          onChange={() => console.log("")}
+          onChange={(e) => props.onTypeChange(e.toString())}
           options={[
             { value: 0, text: "Autocar" },
             { value: 1, text: "Bus" },
