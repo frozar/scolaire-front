e52ed17e030bb6c58b5500aa9fe992e6f415b3a7
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-27 15:46:40 +0400

    wip

--- src/views/content/service/organism/ServiceGrid.tsx
+++ src/views/content/service/organism/ServiceGrid.tsx
@@ -1,5 +1,12 @@
 import _ from "lodash";
-import { For, JSXElement, createEffect, createSignal, onMount } from "solid-js";
+import {
+  For,
+  JSXElement,
+  createEffect,
+  createSignal,
+  on,
+  onMount,
+} from "solid-js";
 import { ServiceGridUtils } from "../../../../utils/serviceGrid.utils";
 import { ServiceTripOrderedUtils } from "../../../../utils/serviceTripPlacement.utils";
 import { ServiceGridTop } from "../molecule/ServiceGridTop";
@@ -24,8 +31,11 @@ export function ServiceGrid(): JSXElement {
 
   // TODO: Simplify, make it only reactive to services()
   // => Make another createEffect for hlpMatrix ?
-  createEffect(() => {
-    /*
+  // createEffect(() => {
+  // ! TEMPORAIRE
+  createEffect(
+    on(hlpMatrix, () => {
+      /*
 
     React on services() and hlpMatrix()
     
@@ -34,27 +44,28 @@ export function ServiceGrid(): JSXElement {
 
     */
 
-    const _services = _.cloneDeep(services());
+      const _services = _.cloneDeep(services());
 
-    const serviceTripIds = _services
-      .flatMap((service) => service.tripIds)
-      .sort((a, b) => a - b);
+      const serviceTripIds = _services
+        .flatMap((service) => service.tripIds)
+        .sort((a, b) => a - b);
 
-    const serviceTripOrderedIds = _services
-      .flatMap((service) => service.serviceTripsOrdered)
-      .map((serviceTrip) => serviceTrip.tripId)
-      .sort((a, b) => a - b);
+      const serviceTripOrderedIds = _services
+        .flatMap((service) => service.serviceTripsOrdered)
+        .map((serviceTrip) => serviceTrip.tripId)
+        .sort((a, b) => a - b);
 
-    // Avoiding infinite loop
-    if (
-      !_.isEqual(serviceTripIds, serviceTripOrderedIds) &&
-      Object.keys(hlpMatrix()).length > 0
-    ) {
-      const updatedServices =
-        ServiceTripOrderedUtils.getUpdatedServices(_services);
-      if (updatedServices) setServices(updatedServices);
-    }
-  });
+      // Avoiding infinite loop
+      if (
+        !_.isEqual(serviceTripIds, serviceTripOrderedIds) &&
+        Object.keys(hlpMatrix()).length > 0
+      ) {
+        const updatedServices =
+          ServiceTripOrderedUtils.getUpdatedServices(_services);
+        if (updatedServices) setServices(updatedServices);
+      }
+    })
+  );
 
   return (
     <div ref={setRef}>
--- src/views/content/service/organism/ServiceGridLine.tsx
+++ src/views/content/service/organism/ServiceGridLine.tsx
@@ -1,11 +1,11 @@
-import { For, JSXElement, Show } from "solid-js";
-import { services } from "./Services";
-
+import { dragAndDrop } from "@formkit/drag-and-drop";
+import { For, JSXElement, Show, createSignal, onMount } from "solid-js";
 import { ServiceGridUtils } from "../../../../utils/serviceGrid.utils";
 import { ServiceGridLineFirstDiv } from "../atom/ServiceGridLineFirstDiv";
 import { ServiceGridItem } from "../molecule/ServiceGridItem";
 import { selectedService } from "../template/ServiceTemplate";
 import "./ServiceGridLine.css";
+import { services } from "./Services";
 
 interface ServiceGridLineProps {
   serviceIndex: number;
@@ -13,8 +13,20 @@ interface ServiceGridLineProps {
 }
 
 export function ServiceGridLine(props: ServiceGridLineProps): JSXElement {
+  const [ref, setRef] = createSignal<HTMLDivElement>(
+    document.createElement("div")
+  );
+  // TODO: Specify type
+  onMount(() => {
+    dragAndDrop({
+      parent: ref(),
+      getValues: services()[props.serviceIndex].serviceTripsOrdered,
+      setValues: 
+    });
+  });
   return (
     <div
+      ref={setRef}
       class={"service-grid-line"}
       style={{ width: props.width }}
       classList={{
