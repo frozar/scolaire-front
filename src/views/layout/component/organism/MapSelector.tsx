f903da9ca8facd6a11f977623b001513fc4275b5
lucas <lucas.lvn97439@gmail.com>
2024-02-28 09:56:37 +0400

    remove data when no map selected

--- src/views/layout/component/organism/MapSelector.tsx
+++ src/views/layout/component/organism/MapSelector.tsx
@@ -3,6 +3,12 @@ import { createEffect, createSignal, onMount } from "solid-js";
 import { MapType } from "../../../../_entities/map.entity";
 import { MapStore, userMaps } from "../../../../_stores/map.store";
 import { MapsUtils } from "../../../../utils/maps.utils";
+import { setCalendars } from "../../../content/calendar/calendar.manager";
+import { setCalendarsPeriod } from "../../../content/calendar/template/Calendar";
+import { setLines } from "../../../content/map/component/organism/BusLines";
+import { setSchools } from "../../../content/map/component/organism/SchoolPoints";
+import { setStops } from "../../../content/map/component/organism/StopPoints";
+import { setTrips } from "../../../content/map/component/organism/Trips";
 import { setSelectedMenu } from "../../menuItemFields";
 import "./MapSelector.css";
 
@@ -16,6 +22,14 @@ export function MapSelector() {
 
   createEffect(() => {
     setSelectedMap(MapsUtils.getSelectedMap(userMaps()));
+    if (!selectedMap()) {
+      setSchools([]);
+      setStops([]);
+      setCalendars([]);
+      setCalendarsPeriod([]);
+      setTrips([]);
+      setLines([]);
+    }
   });
 
   return (
