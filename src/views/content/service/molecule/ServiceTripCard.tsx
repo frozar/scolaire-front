2b0f6248f9f74c4ae5bbbbb1563579c3e79d443e
GryphusOneSeven <gael.edmond@epitech.eu>
2024-03-07 15:20:58 +0400

    added details on trip card

--- src/views/content/service/molecule/ServiceTripCard.tsx
+++ src/views/content/service/molecule/ServiceTripCard.tsx
@@ -1,7 +1,10 @@
-import { JSXElement } from "solid-js";
+import { JSXElement, Show, createSignal, onMount } from "solid-js";
 
+import { TripType } from "../../../../_entities/trip.entity";
 import { addNewGlobalWarningInformation } from "../../../../signaux";
 import { ServiceTripsUtils } from "../../../../utils/serviceTrips.utils";
+import { TripTimeline } from "../../board/component/organism/TripTimeline";
+import { getLines } from "../../map/component/organism/BusLines";
 import { ServiceTripCardLeft } from "../atom/ServiceTripCardLeft";
 import { ServiceTripCardMiddle } from "../atom/ServiceTripCardMiddle";
 import { ServiceTripCardRight } from "../atom/ServiceTripCardRight";
@@ -9,20 +12,41 @@ import { DraggableTripType } from "../organism/ServiceLeftBoardContent";
 import { setServices } from "../organism/Services";
 import { selectedService } from "../template/ServiceTemplate";
 import "./ServiceTripCard.css";
+import { ServiceTripCardDetails } from "./ServiceTripCardDetails";
 
 interface ServiceTripCardProps {
-  trip: DraggableTripType;
+  draggableTrip: DraggableTripType;
 }
 
 export function ServiceTripCard(props: ServiceTripCardProps): JSXElement {
+  const [isInfoOpen, setIsInfoOpen] = createSignal(false);
+  const [currentTrip, setCurrentTrip] = createSignal<TripType>({} as TripType);
+
+  onMount(() => {
+    getLines().flatMap((line) =>
+      line.trips.map((item) => {
+        if (item.id == props.draggableTrip.tripId) {
+          setCurrentTrip(item);
+        }
+      })
+    );
+  });
+
   return (
-    <div
-      class="service-trip-card"
-      onDblClick={() => onDblClick(props.trip.tripId)}
-    >
-      <ServiceTripCardLeft trip={props.trip} />
-      <ServiceTripCardMiddle />
-      <ServiceTripCardRight trip={props.trip} />
+    <div class="service-trip-card-container">
+      <div
+        class="service-trip-card"
+        onDblClick={() => onDblClick(props.draggableTrip.tripId)}
+        onClick={() => setIsInfoOpen(!isInfoOpen())}
+      >
+        <ServiceTripCardLeft trip={props.draggableTrip} />
+        <ServiceTripCardMiddle />
+        <ServiceTripCardRight trip={props.draggableTrip} />
+      </div>
+      <Show when={isInfoOpen()}>
+        <ServiceTripCardDetails trip={currentTrip()} />
+        <TripTimeline inDraw={false} trip={currentTrip()} />
+      </Show>
     </div>
   );
 }
---
+++ src/views/content/service/molecule/ServiceTripCardDetails.tsx
@@ -0,0 +1,28 @@
+import { For } from "solid-js";
+import { TripDirectionEntity } from "../../../../_entities/trip-direction.entity";
+import { TripType } from "../../../../_entities/trip.entity";
+import { TripUtils } from "../../../../utils/trip.utils";
+import { CalendarUtils } from "../../calendar/calendar.utils";
+import "./ServiceTripCardDetails.css";
+
+export function ServiceTripCardDetails(props: { trip: TripType }) {
+  return (
+    <div class="service-trip-card-details-border">
+      <p>
+        Direction :{" "}
+        {TripUtils.tripDirectionTypeTofrench(
+          TripDirectionEntity.FindDirectionById(props.trip.tripDirectionId).type
+        )}
+      </p>
+      <div class="service-trip-card-details-days-container">
+        <For each={props.trip.days}>
+          {(day) => (
+            <p class="service-trip-card-details-days">
+              {CalendarUtils.dayToFrench(day)}
+            </p>
+          )}
+        </For>
+      </div>
+    </div>
+  );
+}
