c2dc560c20d75ea4beb6e32fedca5460dcc98d36
GryphusOneSeven <gael.edmond@epitech.eu>
2024-03-11 11:52:28 +0400

    wip : dashboard allotment display stops

--- src/views/content/dashboard/molecule/DashboardAllotment.tsx
+++ src/views/content/dashboard/molecule/DashboardAllotment.tsx
@@ -1,13 +1,32 @@
 import { createSignal } from "solid-js";
+import { SchoolType } from "../../../../_entities/school.entity";
+import { TripType } from "../../../../_entities/trip.entity";
+import { getTrips } from "../../../../_stores/trip.store";
 import { LabeledInputSelect } from "../../../../component/molecule/LabeledInputSelect";
+import { setDisplaySchools } from "../../_component/organisme/SchoolPoints";
+import { setDisplayTrips } from "../../_component/organisme/Trips";
 import { getAllotment } from "../../allotment/organism/Allotment";
 
 export function DashboardAllotment() {
   const [currentAllotment, setCurrentAllotment] = createSignal(0);
-  // const [currentTrips, setCurrentTrips] = createSignal();
+  const [currentTrips, setCurrentTrips] = createSignal<TripType[]>([]);
 
   function onSelectChange(value: string | number) {
+    const schoolList: SchoolType[] = [];
     setCurrentAllotment(Number(value));
+    setCurrentTrips(
+      getTrips().filter((item) => item.allotmentId == currentAllotment())
+    );
+    currentTrips().forEach((trip) => {
+      trip.schools.forEach((school) => {
+        if (!schoolList.includes(school)) {
+          schoolList.push(school);
+        }
+      });
+    });
+    // TODO find a way to display only currenttrip stops
+    setDisplaySchools(schoolList);
+    setDisplayTrips(currentTrips());
   }
 
   return (
