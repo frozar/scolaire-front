2a1d7fe7e665e0187aa935334ee522586ed926a1
GryphusOneSeven <gael.edmond@epitech.eu>
2024-03-13 10:17:32 +0400

    added trips details on dashboard global view

--- src/views/content/dashboard/molecule/DashboardGlobal.tsx
+++ src/views/content/dashboard/molecule/DashboardGlobal.tsx
@@ -1,17 +1,72 @@
-import { onMount } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import { getSchools } from "../../../../_stores/school.store";
 import { getStops } from "../../../../_stores/stop.store";
 import { getTrips } from "../../../../_stores/trip.store";
 import { setDisplaySchools } from "../../_component/organisme/SchoolPoints";
 import { setDisplayStops } from "../../_component/organisme/StopPoints";
 import { setDisplayTrips } from "../../_component/organisme/Trips";
+import { DashboardMetrics } from "./DashboardMetrics";
 
 export function DashboardGlobal() {
+  const [totalDistance, setTotalDistance] = createSignal(0);
+  const [totalKmPassager, setTotalKmPassager] = createSignal(0);
+  const [totalStudents, setTotalStudents] = createSignal(0);
+
   onMount(() => {
     setDisplayStops(getStops());
     setDisplaySchools(getSchools());
     setDisplayTrips(getTrips());
+    calcTotalDistance();
+    calcTotalKmPassager();
+    calcTotalStudent();
   });
 
-  return <div />;
+  function calcTotalKmPassager() {
+    setTotalKmPassager(0);
+    getTrips().forEach((trip) => {
+      setTotalKmPassager(totalKmPassager() + Number(trip.metrics?.kmPassager));
+    });
+    setTotalKmPassager(roundDecimal(totalKmPassager(), 2));
+  }
+
+  function calcTotalDistance() {
+    setTotalDistance(0);
+    getTrips().forEach((trip) => {
+      setTotalDistance(totalDistance() + Number(trip.metrics?.distance));
+    });
+    setTotalDistance(roundDecimal(totalDistance() / 1000, 2));
+  }
+
+  function calcTotalStudent() {
+    const stopsId: number[] = [];
+    setTotalStudents(0);
+    getTrips().forEach((trip) => {
+      trip.tripPoints.forEach((point) => {
+        if (!stopsId.includes(point.id)) stopsId.push(point.id);
+      });
+    });
+
+    const allStops = getStops().filter((stop) => stopsId.includes(stop.id));
+    allStops.forEach((stop) => {
+      stop.associated.forEach((item) => {
+        setTotalStudents(totalStudents() + item.quantity);
+      });
+    });
+    setDisplayStops(allStops);
+  }
+
+  return (
+    <div>
+      <DashboardMetrics
+        distance={totalDistance()}
+        kmPassager={totalKmPassager()}
+        students={totalStudents()}
+      />
+    </div>
+  );
+}
+
+function roundDecimal(value: number, precision: number) {
+  const tmp = Math.pow(10, precision);
+  return Math.round(value * tmp) / tmp;
 }
--- src/views/content/dashboard/template/Dashboard.tsx
+++ src/views/content/dashboard/template/Dashboard.tsx
@@ -34,7 +34,7 @@ export function Dashboard() {
   return (
     <div class="px-10 w-full bg-white">
       <div class="h-[calc(100vh-60px)]">
-        <div class="pt-5 mb-3">Dashboard (TODO)</div>
+        <div class="pt-5 mb-3">Dashboard</div>
         <LabeledInputSelect
           defaultValue={selectedFilter()}
           label="Vue"
