d887d4f3bf0c8ce7ae4b6f778653d408294b6eb7
AdChiv <adrien@entropik.digital>
2024-04-03 16:19:55 +0400

    add click signal and refacto map container

--- src/views/content/_component/template/MapContainer.tsx
+++ src/views/content/_component/template/MapContainer.tsx
@@ -1,14 +1,14 @@
-import { Show, createSignal, onMount } from "solid-js";
+import { Show, createEffect, createSignal, onMount } from "solid-js";
 
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useStateGui } from "../../../../StateGui";
 import { ImportCsvCanvas } from "../../../../component/ImportCsvCanvas";
-import { addNewUserInformation, getLeafletMap } from "../../../../signaux";
-import { MessageLevelEnum, MessageTypeEnum } from "../../../../type";
+import { addNewUserInformation } from "../../../../signaux";
+import { MessageLevelEnum, MessageTypeEnum, TileId } from "../../../../type";
 import { onBoard } from "../../board/component/template/ContextManager";
 import { MapPanels } from "../../map/component/organism/MapPanels";
-import { buildMapL7 } from "../../map/l7MapBuilder";
+import { getTileById } from "../../map/tileUtils";
 import { SchoolPoints } from "../organisme/SchoolPoints";
 import { StopPoints } from "../organisme/StopPoints";
 import { Trips } from "../organisme/Trips";
@@ -16,15 +16,14 @@ import { Ways } from "../organisme/Ways";
 
 const [, { getActiveMapId }] = useStateGui();
 
-function buildMap(div: HTMLDivElement) {
-  const option = "l7";
-  switch (option) {
-    case "l7": {
-      buildMapL7(div);
-      break;
-    }
-  }
-}
+const defaultTileId = "OpenStreetMap_Mapnik";
+
+export const [leafletMap, setLeafletMap] = createSignal<L.Map>();
+
+export const [tileLayerId, setTileLayerId] = createSignal<TileId | undefined>();
+
+export const [mapOnClick, setMapOnClick] =
+  createSignal<(e: L.LeafletMouseEvent) => void | undefined>();
 
 let mapDiv: HTMLDivElement;
 
@@ -32,13 +31,45 @@ export function MapContainer() {
   const [displayImportCsvCanvas, setDisplayImportCsvCanvas] =
     createSignal(false);
 
-  onMount(async () => {
-    // Manage shortcut keyboard event
-    // for (const handler of listHandlerLMap) {
-    // document.body.addEventListener("keydown", handler);
-    // }
+  onMount(() => {
+    init();
+  });
 
-    //TODO utilité ?
+  /**
+   * Change tile layer
+   */
+  createEffect(() => {
+    let layerID: TileId;
+    if (tileLayerId()) {
+      layerID = tileLayerId() as TileId;
+    } else {
+      layerID = defaultTileId;
+    }
+    const tileLayer: L.TileLayer = getTileById(layerID).tileContent;
+
+    (leafletMap() as L.Map).eachLayer(function (layer) {
+      if (layer instanceof L.TileLayer) {
+        layer.remove();
+      }
+    });
+    tileLayer.remove();
+    tileLayer.addTo(leafletMap() as L.Map);
+  });
+
+  /**
+   * Change map click
+   */
+  createEffect(() => {
+    let callBack: (e: L.LeafletMouseEvent) => void = (e) => {};
+    if (typeof mapOnClick() == "function") {
+      callBack = mapOnClick() as (e: L.LeafletMouseEvent) => void;
+    }
+    (leafletMap() as L.Map)
+      .removeEventListener("click")
+      .addEventListener("click", callBack);
+  });
+
+  function init() {
     if (getActiveMapId()) {
       mapDiv.addEventListener(
         "dragenter",
@@ -48,9 +79,20 @@ export function MapContainer() {
         },
         false
       );
-      buildMap(mapDiv);
+      buildMap();
     }
-  });
+  }
+  function buildMap() {
+    const leafletMap = new L.Map(mapDiv, {
+      zoomControl: false,
+      zoomSnap: 0.1,
+      zoomDelta: 0.1,
+      wheelPxPerZoomLevel: 200,
+      tap: false,
+    }).setView([-20.930746, 55.527503], 13);
+
+    setLeafletMap(leafletMap);
+  }
 
   return (
     <Show when={getActiveMapId()}>
@@ -87,15 +129,10 @@ export function MapContainer() {
         }}
       />
 
-      <SchoolPoints map={getLeafletMap() as L.Map} />
-      <StopPoints map={getLeafletMap() as L.Map} />
-      <Trips map={getLeafletMap() as L.Map} />
-      <Ways map={getLeafletMap() as L.Map} />
-
-      {/* 
-      <BusLines busLines={getLines()} />
-      <Trips map={getLeafletMap() as L.Map} />
-      <Paths map={getLeafletMap() as L.Map} /> */}
+      <SchoolPoints map={leafletMap() as L.Map} />
+      <StopPoints map={leafletMap() as L.Map} />
+      <Trips map={leafletMap() as L.Map} />
+      <Ways map={leafletMap() as L.Map} />
 
       {/* <ConfirmStopAddTrip /> */}
 
