83bf363cb3c68f4c6fed59aadfe84ece348dc2a5
GryphusOneSeven <gael.edmond@epitech.eu>
2024-04-15 13:23:49 +0400

    fixed css, created sub components

--- src/views/content/organization/organism/OrganizationMapWrapper.tsx
+++ src/views/content/organization/organism/OrganizationMapWrapper.tsx
@@ -2,6 +2,7 @@ import L from "leaflet";
 import { Setter, Show, onMount } from "solid-js";
 import { OrganizationMapBoundType } from "../../../../_entities/organization.entity";
 import { OrganizationMapEdit } from "../molecule/OrganizationMapEdit";
+import { OrganizationMapInfo } from "../molecule/OrganizationMapInfo";
 import "./OrganizationMapWrapper.css";
 
 interface OrganizationMapWrapperProps {
@@ -16,7 +17,7 @@ export function OrganizationMapWrapper(props: OrganizationMapWrapperProps) {
   });
 
   function buildMap() {
-    const tmpleafletMap = new L.Map("sub-map", {
+    const leafletMap = new L.Map("sub-map", {
       zoomControl: false,
       zoomSnap: 0.1,
       zoomDelta: 0.1,
@@ -26,7 +27,7 @@ export function OrganizationMapWrapper(props: OrganizationMapWrapperProps) {
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
-    }).addTo(tmpleafletMap);
+    }).addTo(leafletMap);
 
     const corner1 = L.latLng(
       props.mapBounds.corner1.lat as number,
@@ -37,26 +38,26 @@ export function OrganizationMapWrapper(props: OrganizationMapWrapperProps) {
       props.mapBounds.corner2.lng as number
     );
     const bounds = L.latLngBounds(corner1, corner2);
-    tmpleafletMap.fitBounds(bounds);
+    leafletMap.fitBounds(bounds);
 
     if (!props.editing) {
-      tmpleafletMap.dragging.disable();
-      tmpleafletMap.doubleClickZoom.disable();
-      tmpleafletMap.scrollWheelZoom.disable();
-      tmpleafletMap.keyboard.disable();
+      leafletMap.dragging.disable();
+      leafletMap.doubleClickZoom.disable();
+      leafletMap.scrollWheelZoom.disable();
+      leafletMap.keyboard.disable();
     }
 
     // eslint-disable-next-line solid/reactivity
-    tmpleafletMap.on("move", () => {
-      const corner1 = tmpleafletMap.getBounds().getNorthEast();
-      const corner2 = tmpleafletMap.getBounds().getSouthWest();
+    leafletMap.on("move", () => {
+      const corner1 = leafletMap.getBounds().getNorthEast();
+      const corner2 = leafletMap.getBounds().getSouthWest();
       const newBounds = { corner1, corner2 };
       if (props.mapBoundssetter) props.mapBoundssetter(newBounds);
     });
   }
 
   return (
-    <div class="flex gap-6">
+    <div class="organization-map-wrapper">
       <div>
         <Show
           when={!props.editing}
@@ -67,20 +68,7 @@ export function OrganizationMapWrapper(props: OrganizationMapWrapperProps) {
             />
           }
         >
-          <p class="font-bold">Map Bounds</p>
-          <p class="pl-4">
-            Lattitude coin Nord-Est : {props.mapBounds.corner1.lat}
-          </p>
-          <p class="pl-4">
-            Longitude coin Nord-Est : {props.mapBounds.corner1.lng}
-          </p>
-          <br />
-          <p class="pl-4">
-            Lattitude coin Sud-Ouest : {props.mapBounds.corner2.lat}
-          </p>
-          <p class="pl-4">
-            Longitude coin Sud-Ouest : {props.mapBounds.corner2.lng}
-          </p>
+          <OrganizationMapInfo mapBounds={props.mapBounds} />
         </Show>
       </div>
       <div id="sub-map" />
