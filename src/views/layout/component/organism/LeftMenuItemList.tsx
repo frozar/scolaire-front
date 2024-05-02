63765614ee481df9cbd25c11ecb300a8ab17b6d0
AdChiv <adrien@entropik.digital>
2024-05-02 14:07:11 +0400

    reorder and regroup the items of the left menu

--- src/views/layout/component/organism/LeftMenuItemList.tsx
+++ src/views/layout/component/organism/LeftMenuItemList.tsx
@@ -102,7 +102,7 @@ export default function (props: {
                   classList={{
                     disable: effectiveIsDisabled(),
                   }}
-                ></div>
+                />
               </Show>
 
               <LeftMenuItem
